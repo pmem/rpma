71d2e52b8b351e12ddd2660145a167131999a9d8
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-13 10:44:35 +0100

    Initial commit of rpma

---
+++ cmake/functions.cmake
@@ -0,0 +1,177 @@
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2018-2020, Intel Corporation
+#
+
+#
+# functions.cmake - helper functions for CMakeLists.txt
+#
+
+function(join SEP OUT VALUES)
+	string(REPLACE ";" "${SEP}" JOIN_TMP "${VALUES}")
+	set(${OUT} "${JOIN_TMP}" PARENT_SCOPE)
+endfunction()
+
+# prepends prefix to list of strings
+function(prepend var prefix)
+	set(listVar "")
+	foreach(f ${ARGN})
+		list(APPEND listVar "${prefix}/${f}")
+	endforeach(f)
+	set(${var} "${listVar}" PARENT_SCOPE)
+endfunction()
+
+# Checks whether flag is supported by current C compiler and appends
+# it to the relevant cmake variable.
+# 1st argument is a flag
+# 2nd (optional) argument is a build type (debug, release)
+macro(add_flag flag)
+	string(REPLACE - _ flag2 ${flag})
+	string(REPLACE " " _ flag2 ${flag2})
+	string(REPLACE = "_" flag2 ${flag2})
+	set(check_name "C_HAS_${flag2}")
+
+	check_c_compiler_flag(${flag} ${check_name})
+
+	if (${${check_name}})
+		if (${ARGC} EQUAL 1)
+			set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}")
+		else()
+			set(CMAKE_C_FLAGS_${ARGV1} "${CMAKE_C_FLAGS_${ARGV1}} ${flag}")
+		endif()
+	endif()
+endmacro()
+
+macro(add_sanitizer_flag flag)
+	set(SAVED_CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES})
+	set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} -fsanitize=${flag}")
+
+	check_c_compiler_flag("-fsanitize=${flag}" C_HAS_ASAN_UBSAN)
+	if(C_HAS_ASAN_UBSAN)
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${flag}")
+		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=${flag}")
+	else()
+		message("${flag} sanitizer not supported")
+	endif()
+
+	set(CMAKE_REQUIRED_LIBRARIES ${SAVED_CMAKE_REQUIRED_LIBRARIES})
+endmacro()
+
+# Generates cppstyle-$name and cppformat-$name targets and attaches them
+# as dependencies of global "cppformat" target.
+# cppstyle-$name target verifies C++ style of files in current source dir.
+# cppformat-$name target reformats files in current source dir.
+# If more arguments are used, then they are used as files to be checked
+# instead.
+# ${name} must be unique.
+function(add_cppstyle name)
+	if(NOT CLANG_FORMAT)
+		return()
+	endif()
+
+	if(${ARGC} EQUAL 1)
+		add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/cppstyle-${name}-status
+			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/*.c
+				${CMAKE_CURRENT_SOURCE_DIR}/*.h
+			COMMAND ${PERL_EXECUTABLE}
+				${CMAKE_SOURCE_DIR}/utils/cppstyle
+				${CLANG_FORMAT}
+				check
+				${CMAKE_CURRENT_SOURCE_DIR}/*.c
+				${CMAKE_CURRENT_SOURCE_DIR}/*.h
+			COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/cppstyle-${name}-status
+			)
+
+		add_custom_target(cppformat-${name}
+			COMMAND ${PERL_EXECUTABLE}
+				${CMAKE_SOURCE_DIR}/utils/cppstyle
+				${CLANG_FORMAT}
+				format
+				${CMAKE_CURRENT_SOURCE_DIR}/*.c
+				${CMAKE_CURRENT_SOURCE_DIR}/*.h
+			)
+	else()
+		add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/cppstyle-${name}-status
+			DEPENDS ${ARGN}
+			COMMAND ${PERL_EXECUTABLE}
+				${CMAKE_SOURCE_DIR}/utils/cppstyle
+				${CLANG_FORMAT}
+				check
+				${ARGN}
+			COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/cppstyle-${name}-status
+			)
+
+		add_custom_target(cppformat-${name}
+			COMMAND ${PERL_EXECUTABLE}
+				${CMAKE_SOURCE_DIR}/utils/cppstyle
+				${CLANG_FORMAT}
+				format
+				${ARGN}
+			)
+	endif()
+
+	add_custom_target(cppstyle-${name}
+			DEPENDS ${CMAKE_BINARY_DIR}/cppstyle-${name}-status)
+
+	add_dependencies(cppstyle cppstyle-${name})
+	add_dependencies(cppformat cppformat-${name})
+endfunction()
+
+# Generates check-whitespace-$name target and attaches it as a dependency
+# of global "check-whitespace" target.
+# ${name} must be unique.
+function(add_check_whitespace name)
+	if(${ARGC} EQUAL 1)
+		add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/check-whitespace-${name}-status
+			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/*.c
+				${CMAKE_CURRENT_SOURCE_DIR}/*.h
+			COMMAND ${PERL_EXECUTABLE}
+				${CMAKE_SOURCE_DIR}/utils/check_whitespace
+				${CMAKE_CURRENT_SOURCE_DIR}/*.c
+				${CMAKE_CURRENT_SOURCE_DIR}/*.h
+			COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/check-whitespace-${name}-status)
+	else()
+		add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/check-whitespace-${name}-status
+			DEPENDS ${ARGN}
+			COMMAND ${PERL_EXECUTABLE}
+				${CMAKE_SOURCE_DIR}/utils/check_whitespace ${ARGN}
+			COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/check-whitespace-${name}-status)
+	endif()
+
+	add_custom_target(check-whitespace-${name}
+			DEPENDS ${CMAKE_BINARY_DIR}/check-whitespace-${name}-status)
+	add_dependencies(check-whitespace check-whitespace-${name})
+endfunction()
+
+# Sets ${ret} to version of program specified by ${name} in major.minor format
+function(get_program_version_major_minor name ret)
+	execute_process(COMMAND ${name} --version
+		OUTPUT_VARIABLE cmd_ret
+		ERROR_QUIET)
+	STRING(REGEX MATCH "([0-9]+.)([0-9]+)" VERSION ${cmd_ret})
+	SET(${ret} ${VERSION} PARENT_SCOPE)
+endfunction()
+
+function(find_pmemcheck)
+	set(ENV{PATH} ${VALGRIND_PREFIX}/bin:$ENV{PATH})
+	execute_process(COMMAND valgrind --tool=pmemcheck --help
+			RESULT_VARIABLE VALGRIND_PMEMCHECK_RET
+			OUTPUT_QUIET
+			ERROR_QUIET)
+	if(VALGRIND_PMEMCHECK_RET)
+		set(VALGRIND_PMEMCHECK_FOUND 0 CACHE INTERNAL "")
+	else()
+		set(VALGRIND_PMEMCHECK_FOUND 1 CACHE INTERNAL "")
+	endif()
+
+	if(VALGRIND_PMEMCHECK_FOUND)
+		execute_process(COMMAND valgrind --tool=pmemcheck true
+				ERROR_VARIABLE PMEMCHECK_OUT
+				OUTPUT_QUIET)
+
+		string(REGEX MATCH ".*pmemcheck-([0-9.]*),.*" PMEMCHECK_OUT "${PMEMCHECK_OUT}")
+		set(PMEMCHECK_VERSION ${CMAKE_MATCH_1} CACHE INTERNAL "")
+	else()
+		message(WARNING "Valgrind pmemcheck NOT found.")
+	endif()
+endfunction()
---
+++ examples/template-example/template-example.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * template-example.c -- template example
+ */
+
+int
+main(int argc, char *argv[])
+{
+	return 0;
+}
