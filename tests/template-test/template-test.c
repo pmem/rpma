0d806c937d83468fd45c7d6ab72dcfe34390d9e3
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-15 09:33:34 +0100

    Initial commit of rpma

---
+++ tests/template-test/template-test.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * template-test.c -- a template test
+ */
+
+int
+main(int argc, char *argv[])
+{
+	return 0;
+}
