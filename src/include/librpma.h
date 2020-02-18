1545104e841685bbb07ae9c0270d7f76f91ee71d
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-18 08:37:21 +0100

    common: build system

---
+++ src/include/librpma.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2019-2020, Intel Corporation
+ */
+
+/*
+ * librpma.h -- definitions of librpma entry points (EXPERIMENTAL)
+ *
+ * This library provides low-level support for remote access to persistent
+ * memory utilizing RDMA-capable RNICs.
+ *
+ * See librpma(7) for details.
+ */
+
+#ifndef LIBRPMA_H
+#define LIBRPMA_H 1
+
+const char *rpma_errormsg(void);
+
+#endif /* librpma.h */
