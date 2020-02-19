f8605cfe0d067710600d3d483097d23fbaeb1495
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-19 09:50:48 +0100

    common: build system

---
+++ utils/docker/run-build.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2016-2020, Intel Corporation
+#
+
+#
+# run-build.sh - is called inside a Docker container,
+#                starts rpma build with tests.
+#
+
+set -e
+
+./prepare-for-build.sh
+
+EXAMPLE_TEST_DIR="/tmp/build_example"
+PREFIX=/usr
+TEST_DIR=${RPMA_TEST_DIR:-${DEFAULT_TEST_DIR}}
+CHECK_CSTYLE=${CHECK_CSTYLE:-ON}
+
+function sudo_password() {
+	echo $USERPASS | sudo -Sk $*
+}
+
+function cleanup() {
+	find . -name ".coverage" -exec rm {} \;
+	find . -name "coverage.xml" -exec rm {} \;
+	find . -name "*.gcov" -exec rm {} \;
+	find . -name "*.gcda" -exec rm {} \;
+}
+
+function upload_codecov() {
+	clang_used=$(cmake -LA -N . | grep CMAKE_C_COMPILER | grep clang | wc -c)
+
+	if [[ $clang_used > 0 ]]; then
+		gcovexe="llvm-cov gcov"
+	else
+		gcovexe="gcov"
+	fi
+
+	# the output is redundant in this case, i.e. we rely on parsed report from codecov on github
+	bash <(curl -s https://codecov.io/bash) -c -F $1 -x "$gcovexe"
+	cleanup
+}
+
+function compile_example_standalone() {
+	rm -rf $EXAMPLE_TEST_DIR
+	mkdir $EXAMPLE_TEST_DIR
+	cd $EXAMPLE_TEST_DIR
+
+	cmake $WORKDIR/examples/$1
+
+	# exit on error
+	if [[ $? != 0 ]]; then
+		cd -
+		return 1
+	fi
+
+	make -j$(nproc)
+	cd -
+}
+
+function run_example_standalone() {
+	cd $EXAMPLE_TEST_DIR
+
+	./$1 $2
+
+	# exit on error
+	if [[ $? != 0 ]]; then
+		cd -
+		return 1
+	fi
+
+	rm -f $2
+
+	cd -
+}
+
+echo
+echo "##############################################################"
+echo "### Verify build and install (in dir: ${PREFIX})"
+echo "##############################################################"
+
+mkdir $WORKDIR/build
+cd $WORKDIR/build
+
+cmake .. -DCMAKE_BUILD_TYPE=Debug \
+	-DTEST_DIR=$TEST_DIR \
+	-DCMAKE_INSTALL_PREFIX=$PREFIX \
+	-DCOVERAGE=$COVERAGE \
+	-DCHECK_CSTYLE=${CHECK_CSTYLE} \
+	-DDEVELOPER_MODE=1
+
+make -j$(nproc)
+make -j$(nproc) doc
+ctest --output-on-failure
+sudo_password -S make -j$(nproc) install
+
+if [ "$COVERAGE" == "1" ]; then
+	upload_codecov tests
+fi
+
+# Uninstall libraries
+cd $WORKDIR/build
+sudo_password -S make uninstall
+
+cd $WORKDIR
+rm -rf $WORKDIR/build
