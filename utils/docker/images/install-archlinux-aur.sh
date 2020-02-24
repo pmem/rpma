e1c32d76081ca1320821004a0450771a85f654e7
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-24 13:42:17 +0100

    common: add Docker images for other Linux distros
    Add Docker images for other Linux distros:
    1) Arch Linux
    2) CentOS latest
    3) Debian latest
    4) Debian testing
    5) Fedora rawhide
    6) openSUSE Leap
    7) openSUSE Tumbleweed
    8) Ubuntu rolling

---
+++ utils/docker/images/install-archlinux-aur.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+#
+
+PKG=$1
+git clone https://aur.archlinux.org/$PKG.git
+chown -R user.user ./$PKG
+cd $PKG
+sudo -u user makepkg -si --noconfirm --skippgpcheck
+cd ..
+rm -r $PKG
