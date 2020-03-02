bc55499ab45331c5e03200dcfd5fb048bc9a0309
Jan Michalski <jan.m.michalski@intel.com>
2020-03-02 09:29:07 +0100

    rpma: add device source code

---
+++ src/rpma_utils.h
@@ -0,0 +1,21 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * rpma_utils.h -- librpma utilities functions
+ */
+
+#ifndef RPMA_UTILS_H
+#define RPMA_UTILS_H 1
+
+#include <errno.h>
+
+#include "out.h"
+
+#define RPMA_E_ERRNO (-errno)
+
+#define RPMA_DEFAULT_TIMEOUT 1000
+
+#endif /* RPMA_UTILS_H */
