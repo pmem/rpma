3bb28dc6292a187039ff27e4c565650040410607
Lukasz Dorau <lukasz.dorau@intel.com>
2020-05-27 08:15:41 +0200

    common: install cmocka in Docker images

---
+++ utils/docker/images/install-cmocka.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+#
+
+#
+# install-cmocka.sh - installs cmocka.org
+#
+
+set -e
+
+git clone https://git.cryptomilk.org/projects/cmocka.git
+mkdir cmocka/build
+cd cmocka/build
+
+# cmocka-1.1.5-26-g672c5ce - pull latest fixes
+git checkout 672c5cee79eb412025c3dd8b034e611c1f119055
+
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo
+make -j$(nproc)
+sudo make -j$(nproc) install
+cd ../..
+rm -r cmocka
