3e214c8f035f7a5b7a96550ba6105a97e222a181
Jan M Michalski <jan.m.michalski@intel.com>
2020-06-02 23:02:59 +0200

    test: rpma_info_new/_delete unit tests

---
+++ tests/info-test/info-test_0.cmake
@@ -0,0 +1,12 @@
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+#
+
+include(${SRC_DIR}/../cmake/helpers.cmake)
+
+setup()
+
+execute(${TEST_EXECUTABLE})
+
+finish()
