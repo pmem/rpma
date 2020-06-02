34689d337bbaed55e8af8f2845f3851260973365
Jan M Michalski <jan.m.michalski@intel.com>
2020-06-02 13:50:59 +0200

    rpma: implement info module

--- src/info.c
+++ src/info.c
@@ -7,8 +7,15 @@
  * info.c -- librpma info-related implementations
  */
 
-#include "librpma.h"
+#include <errno.h>
+
+#include "cmocka_alloc.h"
+#include "conn_req.h"
 #include "info.h"
+#include "rpma_err.h"
+#include "out.h"
+
+#include "librpma.h"
 
 struct rpma_info {
 	/* either active or passive side of the connection */
@@ -19,40 +26,101 @@ struct rpma_info {
 /* internal librpma API */
 
 /*
- * rpma_info_new -- XXX use addr, service and side for resolving the rpma_info
- * instance; uses rdma_getaddrinfo
+ * rpma_info_new -- create an address translation cache aka the info object
  */
 int
 rpma_info_new(const char *addr, const char *service, enum rpma_info_side side,
-		struct rpma_info **info)
+		struct rpma_info **info_ptr)
 {
-	return RPMA_E_NOSUPP;
+	if (addr == NULL || service == NULL || info_ptr == NULL)
+		return RPMA_E_INVAL;
+
+	/* prepare hints */
+	struct rdma_addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	if (side == RPMA_INFO_PASSIVE)
+		hints.ai_flags |= RAI_PASSIVE;
+	hints.ai_qp_type = IBV_QPT_RC;
+	hints.ai_port_space = RDMA_PS_TCP;
+
+	/* query */
+	struct rdma_addrinfo *rai = NULL;
+	int ret = rdma_getaddrinfo(addr, service, &hints, &rai);
+	if (ret) {
+		Rpma_provider_error = errno;
+		return RPMA_E_PROVIDER;
+	}
+
+	struct rpma_info *info = Malloc(sizeof(*info));
+	if (info == NULL) {
+		ASSERTeq(errno, ENOMEM);
+		ret = RPMA_E_NOMEM;
+		goto err_freeaddrinfo;
+	}
+
+	info->side = side;
+	info->rai = rai;
+	*info_ptr = info;
+
+	return 0;
+
+err_freeaddrinfo:
+	rdma_freeaddrinfo(rai);
+	return ret;
 }
 
 /*
- * rpma_info_delete -- XXX relase cached info and delete the rpma_info object;
- * uses rdma_freeaddrinfo
+ * rpma_info_delete -- release the address translation cache and delete the
+ * info object
  */
 int
-rpma_info_delete(struct rpma_info **info)
+rpma_info_delete(struct rpma_info **info_ptr)
 {
-	return RPMA_E_NOSUPP;
+	if (info_ptr == NULL)
+		return RPMA_E_INVAL;
+
+	struct rpma_info *info = *info_ptr;
+	if (info == NULL)
+		return 0;
+
+	rdma_freeaddrinfo(info->rai);
+	Free(info);
+	*info_ptr = NULL;
+
+	return 0;
 }
 
 /*
- * rpma_info_resolve_addr -- XXX uses dma_resolve_addr
+ * rpma_info_resolve_addr -- resolve the ID destination address
  */
 int
-rpma_info_resolve_addr(struct rpma_info *info, struct rdma_cm_id *id)
+rpma_info_resolve_addr(struct rdma_cm_id *id, struct rpma_info *info)
 {
-	return RPMA_E_NOSUPP;
+	ASSERTeq(info->side, RPMA_INFO_PASSIVE);
+
+	int ret = rdma_resolve_addr(id, info->rai->ai_src_addr,
+			info->rai->ai_dst_addr, RPMA_DEFAULT_TIMEOUT);
+	if (ret) {
+		Rpma_provider_error = errno;
+		return RPMA_E_PROVIDER;
+	}
+
+	return 0;
 }
 
 /*
- * rdma_info_bind_addr -- XXX uses rdma_bind_addr
+ * rdma_info_bind_addr -- bind the ID to address
  */
 int
-rdma_info_bind_addr(struct rpma_info *info)
+rdma_info_bind_addr(struct rdma_cm_id *id, struct rpma_info *info)
 {
-	return RPMA_E_NOSUPP;
+	ASSERTeq(info->side, RPMA_INFO_PASSIVE);
+
+	int ret = rdma_bind_addr(id, info->rai->ai_src_addr);
+	if (ret) {
+		Rpma_provider_error = errno;
+		return RPMA_E_PROVIDER;
+	}
+
+	return 0;
 }
