9fac27e8f2725e76ebc0d0f0f5e1dd00e987221f
Lukasz Dorau <lukasz.dorau@intel.com>
2020-06-04 10:26:27 +0200

    test: add test for rpma_peer_create_qp()

--- tests/peer-test/peer-test.c
+++ tests/peer-test/peer-test.c
@@ -10,6 +10,7 @@
 #include "cmocka_headers.h"
 
 #include "librpma.h"
+#include "peer.h"
 
 #include <infiniband/verbs.h>
 
@@ -403,6 +404,15 @@ peer_delete_test_dealloc_pd_fail(void **peer_ptr)
 	assert_int_equal(rpma_err_get_provider_error(), EBUSY);
 }
 
+/*
+ * peer_create_qp_test -- rpma_peer_create_qp is not supported yet
+ */
+static void
+peer_create_qp_test(void **unused)
+{
+	assert_int_equal(rpma_peer_create_qp(NULL, NULL), RPMA_E_NOSUPP);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -423,6 +433,9 @@ main(int argc, char *argv[])
 		cmocka_unit_test_setup_teardown(
 				peer_delete_test_dealloc_pd_fail,
 				peer_setup, peer_teardown),
+
+		/* rpma_peer_create_qp() unit tests */
+		cmocka_unit_test(peer_create_qp_test),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
