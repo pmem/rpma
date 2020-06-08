16fa81afb32a2f63479825f4a12ea0116544a446
Jan M Michalski <jan.m.michalski@intel.com>
2020-06-08 11:06:03 +0200

    rpma: rpma_conn_req_from_cm_event()/_delete() implementation
    Additionally:
    - removal of struct rpma_conn_cfg (for now)

--- src/conn_req.h
+++ src/conn_req.h
@@ -16,7 +16,10 @@
 
 #define RPMA_DEFAULT_TIMEOUT 1000 /* ms */
 
-/* for the simplicity sake, it is assumed all CQ/SQ/RQ sizes are equal */
+/*
+ * For the simplicity sake, it is assumed all CQ/SQ/RQ sizes are equal.
+ * XXX Later on, it should be configurable on a per-connection basis.
+ */
 #define RPMA_DEFAULT_Q_SIZE 10
 
 /* the maximum number of scatter/gather elements in any Work Request */
@@ -25,7 +28,15 @@
 /* the maximum message size (in bytes) that can be posted inline */
 #define RPMA_MAX_INLINE_DATA 0
 
+/*
+ * ERRORS
+ * rpma_conn_req_from_cm_event() can fail with the following errors:
+ *
+ * - RPMA_E_INVAL - peer, event or req_ptr is NULL
+ * - RPMA_E_INVAL - event is not RDMA_CM_EVENT_CONNECT_REQUEST
+ * - RPMA_E_PROVIDER - ibv_create_cq(3) or rdma_create_qp(3) failed
+ */
 int rpma_conn_req_from_cm_event(struct rpma_peer *peer,
-		struct rdma_cm_event *event, struct rpma_conn_req **req);
+		struct rdma_cm_event *event, struct rpma_conn_req **req_ptr);
 
 #endif /* LIBRPMA_CONN_REQ_H */
