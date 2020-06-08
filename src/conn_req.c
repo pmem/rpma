7c54a57a13fc446bf1d2c12cc9f053cc2a31545e
Jan M Michalski <jan.m.michalski@intel.com>
2020-06-08 11:06:03 +0200

    rpma: rpma_conn_req_from_cm_event()/_delete() implementation
    Additionally:
    - removal of struct rpma_conn_cfg (for now)

--- src/conn_req.c
+++ src/conn_req.c
@@ -8,6 +8,7 @@
  */
 
 #include "cmocka_alloc.h"
+#include "conn.h"
 #include "conn_req.h"
 #include "out.h"
 #include "peer.h"
@@ -18,15 +19,22 @@ struct rpma_conn_req {
 	struct rdma_cm_event *edata;
 	/* CM ID of the connection request */
 	struct rdma_cm_id *id;
+	/* completion queue of the CM ID */
+	struct ibv_cq *cq;
 };
 
 /*
- * rpma_conn_req_from_id -- allocate a new conn_req object from CM ID
+ * rpma_conn_req_from_id -- allocate a new conn_req object from CM ID and equip
+ * the latter with QP and CQ
  */
 static int
 rpma_conn_req_from_id(struct rpma_peer *peer, struct rdma_cm_id *id,
 		struct rpma_conn_req **req)
 {
+	ASSERTne(peer, NULL);
+	ASSERTne(id, NULL);
+	ASSERTne(req, NULL);
+
 	/* create a CQ */
 	struct ibv_cq *cq = ibv_create_cq(id->verbs, RPMA_DEFAULT_Q_SIZE,
 				NULL /* cq_context */,
@@ -50,6 +58,7 @@ rpma_conn_req_from_id(struct rpma_peer *peer, struct rdma_cm_id *id,
 	}
 
 	(*req)->id = id;
+	(*req)->cq = cq;
 
 	return 0;
 
@@ -69,12 +78,24 @@ err_destroy_cq:
  * rpma_conn_req_from_id and add the event to conn_req
  */
 int
-rpma_conn_req_from_cm_event(struct rpma_peer *peer, struct rdma_cm_event *event,
-		struct rpma_conn_req **req)
+rpma_conn_req_from_cm_event(struct rpma_peer *peer, struct rdma_cm_event *edata,
+		struct rpma_conn_req **req_ptr)
 {
-	(void) rpma_conn_req_from_id;
+	if (peer == NULL || edata == NULL || req_ptr == NULL)
+		return RPMA_E_INVAL;
 
-	return RPMA_E_NOSUPP;
+	if (edata->event != RDMA_CM_EVENT_CONNECT_REQUEST)
+		return RPMA_E_INVAL;
+
+	struct rpma_conn_req *req = NULL;
+	int ret = rpma_conn_req_from_id(peer, edata->id, &req);
+	if (ret)
+		return ret;
+
+	req->edata = edata;
+	*req_ptr = req;
+
+	return 0;
 }
 
 /* public librpma API */
@@ -97,18 +118,82 @@ rpma_conn_req_new(struct rpma_peer *peer, const char *addr, const char *service,
  * or rdma_accept to transform rpma_conn_req into rpma_conn
  */
 int
-rpma_conn_req_connect(struct rpma_conn_req *req, struct rpma_conn_cfg *ccfg,
-		const void *private_data, uint8_t private_data_len,
-		struct rpma_conn **conn)
+rpma_conn_req_connect(struct rpma_conn_req **req_ptr, const void *private_data,
+		uint8_t private_data_len, struct rpma_conn **conn)
 {
 	return RPMA_E_NOSUPP;
 }
 
 /*
- * rpma_conn_req_delete -- XXX teardown of the conn_req object
+ * rpma_conn_req_delete -- destroy QP and CQ of the CM ID and reject the
+ * connection or XXX. Finally, free the connection request object.
  */
 int
-rpma_conn_req_delete(struct rpma_conn_req **req)
+rpma_conn_req_delete(struct rpma_conn_req **req_ptr)
 {
-	return RPMA_E_NOSUPP;
+	if (req_ptr == NULL)
+		return RPMA_E_INVAL;
+
+	struct rpma_conn_req *req = *req_ptr;
+	if (req == NULL)
+		return 0;
+
+	int ret = 0;
+
+	rdma_destroy_qp(req->id);
+
+	Rpma_provider_error = ibv_destroy_cq(req->cq);
+	if (Rpma_provider_error) {
+		ret = RPMA_E_PROVIDER;
+		goto err_reject_or_XXX;
+	}
+
+	if (req->edata) {
+		if (rdma_reject(req->id,
+				NULL /* private data */,
+				0 /* private data len */)) {
+			Rpma_provider_error = errno;
+			ret = RPMA_E_PROVIDER;
+			goto err_ack;
+		}
+
+		if (rdma_ack_cm_event(req->edata)) {
+			Rpma_provider_error = errno;
+			ret = RPMA_E_PROVIDER;
+			goto err_free;
+		}
+	} else {
+		/*
+		 * XXX to be implemented when rpma_conn_req_new() will be
+		 * ready
+		 */
+		ASSERT(0);
+	}
+
+	Free(req);
+	*req_ptr = NULL;
+
+	return 0;
+
+err_reject_or_XXX:
+	if (req->edata == NULL) {
+		/*
+		 * XXX to be implemented when rpma_conn_req_new() will be
+		 * ready
+		 */
+		ASSERT(0);
+	}
+
+/* error path when req->edata != NULL */
+err_reject:
+	(void) rdma_reject(req->id, NULL, 0);
+
+err_ack:
+	(void) rdma_ack_cm_event(req->edata);
+
+err_free:
+	Free(req);
+	*req_ptr = NULL;
+
+	return ret;
 }
