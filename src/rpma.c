dec2b6dd30ef185f3095b418bc59f79e849f26ba
Lukasz Dorau <lukasz.dorau@intel.com>
2020-06-08 13:14:04 +0200

    rpma: use info module in rpma_utils_get_ibv_context()

--- src/rpma.c
+++ src/rpma.c
@@ -11,6 +11,7 @@
 
 #include "librpma.h"
 #include "rpma_err.h"
+#include "info.h"
 #include "out.h"
 
 /* public librpma API */
@@ -21,23 +22,16 @@
 int
 rpma_utils_get_ibv_context(const char *addr, struct ibv_context **dev)
 {
-	struct rdma_addrinfo *rai;
-	struct rdma_addrinfo hints;
+	struct rpma_info *info;
 
 	if (addr == NULL || dev == NULL)
 		return RPMA_E_INVAL;
 
-	/* prepare hints for rdma_getaddrinfo() */
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_flags |= RAI_PASSIVE; /* XXX passive for now */
-	hints.ai_qp_type = IBV_QPT_RC;
-	hints.ai_port_space = RDMA_PS_TCP;
-
-	int ret = rdma_getaddrinfo(addr, NULL, &hints, &rai);
-	if (ret) {
-		Rpma_provider_error = errno;
-		return RPMA_E_PROVIDER;
-	}
+	/* XXX only passive for now */
+	int ret = rpma_info_new(addr, NULL /* service */,
+			RPMA_INFO_PASSIVE, &info);
+	if (ret)
+		return ret;
 
 	struct rdma_cm_id *temp_id;
 	ret = rdma_create_id(NULL, &temp_id, NULL, RDMA_PS_TCP);
@@ -47,15 +41,10 @@ rpma_utils_get_ibv_context(const char *addr, struct ibv_context **dev)
 		goto err_create_id;
 	}
 
-	ASSERTeq(rai->ai_flags & RAI_PASSIVE, RAI_PASSIVE);
-
 	/* bind the address */
-	ret = rdma_bind_addr(temp_id, rai->ai_src_addr);
-	if (ret) {
-		Rpma_provider_error = errno;
-		ret = RPMA_E_PROVIDER;
+	ret = rpma_info_bind_addr(info, temp_id);
+	if (ret)
 		goto err_bind_addr;
-	}
 
 	/* obtain the device */
 	*dev = temp_id->verbs;
@@ -64,7 +53,7 @@ err_bind_addr:
 	(void) rdma_destroy_id(temp_id);
 
 err_create_id:
-	rdma_freeaddrinfo(rai);
+	(void) rpma_info_delete(&info);
 	return ret;
 }
 
