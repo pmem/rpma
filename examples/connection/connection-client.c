22a19ee54b8756bdc1fe6c777c565536c1a776a1
Jan M Michalski <jan.m.michalski@intel.com>
2020-06-20 15:26:33 +0200

    examples: enrich the connection example

--- examples/connection/connection-client.c
+++ examples/connection/connection-client.c
@@ -13,57 +13,139 @@
 
 #include <librpma.h>
 
+static void
+print_error(const char *fname, const int ret)
+{
+	int result = 0;
+
+	if (ret == RPMA_E_PROVIDER) {
+		int errnum = rpma_err_get_provider_error();
+		const char *errstr = strerror(errnum);
+		result = fprintf(stderr, "%s failed: %s (%s)\n", fname,
+				rpma_err_2str(ret), errstr);
+	} else {
+		result = fprintf(stderr, "%s failed: %s\n", fname,
+				rpma_err_2str(ret));
+	}
+
+	if (result < 0)
+		exit(-1);
+}
+
 int
 main(int argc, char *argv[])
 {
-	if (argc < 4) {
-		fprintf(stderr, "usage: %s <addr_local> <addr> <service>\n",
+	if (argc < 3) {
+		fprintf(stderr, "usage: %s <addr> <service>\n",
 			argv[0]);
-		abort();
+		exit(-1);
 	}
 
 	/* parameters */
-	char *addr_local = argv[1];
-	char *addr = argv[2];
-	char *service = argv[3];
+	char *addr = argv[1];
+	char *service = argv[2];
 
 	/* resources */
-	struct ibv_context *dev;
-	struct rpma_peer *peer;
-	struct rpma_conn_req *req;
-	struct rpma_conn *conn;
-	enum rpma_conn_event conn_event;
-	int ret;
+	struct ibv_context *dev = NULL;
+	struct rpma_peer *peer = NULL;
+	struct rpma_conn_req *req = NULL;
+	struct rpma_conn *conn = NULL;
+	enum rpma_conn_event conn_event = RPMA_CONN_UNDEFINED;
+	int ret = 0;
 
-	/* request for a connection and wait for its establishment */
-	ret = rpma_utils_get_ibv_context(addr_local, &dev);
-	assert(ret == 0);
+	/* obtain an IBV context for a remote IP address */
+	ret = rpma_utils_get_ibv_context(addr, &dev);
+	if (ret) {
+		print_error("rpma_utils_get_ibv_context", ret);
+		return -1;
+	}
 
+	/* create a new peer object */
 	ret = rpma_peer_new(dev, &peer);
-	assert(ret == 0);
+	if (ret) {
+		print_error("rpma_peer_new", ret);
+		return -1;
+	}
 
+	/* create a connection request */
 	ret = rpma_conn_req_new(peer, addr, service, &req);
-	assert(ret == 0);
+	if (ret) {
+		print_error("rpma_conn_req_new", ret);
+		goto err_peer_delete;
+	}
+
+	/* connect the connection request and obtain the connection object */
 	ret = rpma_conn_req_connect(&req, NULL, &conn);
-	assert(ret == 0);
+	if (ret) {
+		print_error("rpma_conn_req_connect", ret);
+		goto err_req_delete;
+	}
 
 	/* wait for the connection to establish */
 	ret = rpma_conn_next_event(conn, &conn_event);
-	assert(ret == 0 && conn_event == RPMA_CONN_ESTABLISHED);
+	if (ret) {
+		print_error("rpma_conn_next_event", ret);
+		goto err_conn_delete;
+	} else if (conn_event != RPMA_CONN_ESTABLISHED) {
+		fprintf(stderr, "rpma_conn_next_event returned an unexptected "
+				"event: %s\n",
+				rpma_utils_conn_event_2str(conn_event));
+		goto err_conn_delete;
+	} else {
+		fprintf(stderr, "rpma_conn_next_event returned an event: %s\n",
+				rpma_utils_conn_event_2str(conn_event));
+	}
 
 	/* here you can use the newly established connection */
 
-	/* disconnect the connection */
+	/* wait for the connection to being closed */
 	ret = rpma_conn_next_event(conn, &conn_event);
-	assert(ret == 0 && conn_event == RPMA_CONN_CLOSED);
+	if (ret) {
+		print_error("rpma_conn_next_event", ret);
+		goto err_conn_disconnect;
+	} else if (conn_event != RPMA_CONN_CLOSED) {
+		fprintf(stderr, "rpma_conn_next_event returned an unexptected "
+				"event: %s\n",
+				rpma_utils_conn_event_2str(conn_event));
+		goto err_conn_disconnect;
+	} else {
+		fprintf(stderr, "rpma_conn_next_event returned an event: %s\n",
+				rpma_utils_conn_event_2str(conn_event));
+	}
+
+	/* disconnect the connection */
 	ret = rpma_conn_disconnect(conn);
-	assert(ret == 0);
+	if (ret) {
+		print_error("rpma_conn_disconnect", ret);
+		goto err_conn_delete;
+	}
 
+	/* delete the connection object */
 	ret = rpma_conn_delete(&conn);
-	assert(ret == 0);
+	if (ret) {
+		print_error("rpma_conn_delete", ret);
+		goto err_peer_delete;
+	}
 
+	/* delete the peer object */
 	ret = rpma_peer_delete(&peer);
-	assert(ret == 0);
+	if (ret) {
+		print_error("rpma_peer_delete", ret);
+		goto err_exit;
+	}
 
 	return 0;
+
+err_conn_disconnect:
+	(void) rpma_conn_disconnect(conn);
+err_conn_delete:
+	(void) rpma_conn_delete(&conn);
+err_req_delete:
+	if (req)
+		(void) rpma_conn_req_delete(&req);
+err_peer_delete:
+	(void) rpma_peer_delete(&peer);
+
+err_exit:
+	return -1;
 }
