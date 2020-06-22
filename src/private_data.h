ad87c8bc3702db528716a2ae9f5cfd7acb4bde27
Jan M Michalski <jan.m.michalski@intel.com>
2020-06-22 19:07:00 +0200

    rpma: introduce a private_data module

---
+++ src/private_data.h
@@ -0,0 +1,31 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * private_data.h -- a store for connections' private data (definitions)
+ */
+
+#ifndef LIBRPMA_PRIVATE_DATA_H
+#define LIBRPMA_PRIVATE_DATA_H
+
+#include <rdma/rdma_cma.h>
+
+#include "librpma.h"
+
+/*
+ * ERRORS
+ * rpma_private_data_store() can fail with the following error:
+ *
+ * - RPMA_E_NOMEM - out of memory
+ */
+int rpma_private_data_store(struct rdma_cm_event *edata,
+		struct rpma_conn_private_data *pdata);
+
+/*
+ * The function cannot fail.
+ */
+void rpma_private_data_discard(struct rpma_conn_private_data *pdata);
+
+#endif /* LIBRPMA_PRIVATE_DATA_H */
