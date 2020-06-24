191c1452447baaa790d4689b857d726c0e31f018
Jan M Michalski <jan.m.michalski@intel.com>
2020-06-24 14:30:42 +0200

    rpma: private_data_copy introduction

--- src/private_data.h
+++ src/private_data.h
@@ -24,6 +24,15 @@ int rpma_private_data_store(struct rdma_cm_event *edata,
 		struct rpma_conn_private_data *pdata);
 
 /*
+ * ERRORS
+ * rpma_private_data_copy() can fail with the following error:
+ *
+ * - RPMA_E_NOMEM - out of memory
+ */
+int rpma_private_data_copy(struct rpma_conn_private_data *src,
+		struct rpma_conn_private_data *dst);
+
+/*
  * The function cannot fail.
  */
 void rpma_private_data_discard(struct rpma_conn_private_data *pdata);
