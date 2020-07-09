bfffe8a00849d5a5615d37abf9bae0e9a04d904a
Jan M Michalski <jan.m.michalski@intel.com>
2020-07-09 09:54:31 +0200

    test: group conn and mr unit tests

---
+++ tests/conn-test/conn-test.c
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * conn-test.c -- the conn unit tests hub
+ */
+
+#include "conn-test-common.h"
+
+extern const struct CMUnitTest tests_disconnect[];
+extern const struct CMUnitTest tests_new[];
+extern const struct CMUnitTest tests_next_completion[];
+extern const struct CMUnitTest tests_next_event[];
+extern const struct CMUnitTest tests_private_data[];
+extern const struct CMUnitTest tests_read[];
+
+struct rpma_test_group groups[] = {
+	RPMA_TEST_GROUP(tests_disconnect, NULL),
+	RPMA_TEST_GROUP(tests_new, NULL),
+	RPMA_TEST_GROUP(tests_next_completion, group_setup_next_completion),
+	RPMA_TEST_GROUP(tests_next_event, NULL),
+	RPMA_TEST_GROUP(tests_private_data, NULL),
+	RPMA_TEST_GROUP(tests_read, group_setup_read),
+	RPMA_TEST_GROUP(NULL, NULL),
+};
+
+int
+main(int argc, char *argv[])
+{
+	return rpma_test_groups_run(groups);
+}
