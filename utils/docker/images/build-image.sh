3c0fb84fdb263ef17984fe0a91b15d8a0338b52b
Jan M Michalski <jan.m.michalski@intel.com>
2020-07-14 19:27:43 +0200

    common: allow building images with PMDK included

--- utils/docker/images/build-image.sh
+++ utils/docker/images/build-image.sh
@@ -17,7 +17,7 @@ set -e
 
 function usage {
 	echo "Usage:"
-	echo "    build-image.sh <DOCKERHUB_REPO> <OS-VER>"
+	echo "    build-image.sh <DOCKERHUB_REPO> <OS-VER> [<SKIP_PMDK_BUILD>]"
 	echo "where <OS-VER>, for example, can be 'fedora-30', provided " \
 		"a Dockerfile named 'Dockerfile.fedora-30' exists in the " \
 		"current directory."
@@ -36,8 +36,14 @@ if [[ ! -f "Dockerfile.$2" ]]; then
 	exit 1
 fi
 
+# Check if SKIP_PMDK_BUILD is set to "y"
+if [ "x$3" == "xy" ]; then
+	SKIP_PMDK_BUILD_ARG="--build-arg SKIP_PMDK_BUILD=y"
+fi
+
 # Build a Docker image tagged with ${DOCKERHUB_REPO}:0.1-OS-VER
 docker build -t $1:0.1-$2 \
 	--build-arg http_proxy=$http_proxy \
 	--build-arg https_proxy=$https_proxy \
+	$SKIP_PMDK_BUILD_ARG \
 	-f Dockerfile.$2 .
