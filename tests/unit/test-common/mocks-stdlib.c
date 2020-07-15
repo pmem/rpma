63e6081ab01435615b0e50e1c76ce35ef4238b13
Jan M Michalski <jan.m.michalski@intel.com>
2020-07-15 12:44:24 +0200

    test: extract conn_req mocks to common
    This step extracts all mocks from conn_req. Other mocks are planned to
    be extracted and unified iteratively.

---
+++ tests/unit/test-common/mocks-stdlib.c
@@ -0,0 +1,29 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * mocks-stdlib.c -- stdlib mocks
+ */
+
+#include <errno.h>
+#include <stddef.h>
+
+#include "cmocka_headers.h"
+
+void *__real__test_malloc(size_t size);
+
+/*
+ * __wrap__test_malloc -- malloc() mock
+ */
+void *
+__wrap__test_malloc(size_t size)
+{
+	errno = mock_type(int);
+
+	if (errno)
+		return NULL;
+
+	return __real__test_malloc(size);
+}
