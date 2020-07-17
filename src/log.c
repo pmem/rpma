b56d9f6cf9ee4f322242622d98716da962718ead
Tomasz Gromadzki <grom72@o2.pl>
2020-07-17 18:40:46 +0200

    rpma: minor fixes in log implementation

--- src/log.c
+++ src/log.c
@@ -69,7 +69,7 @@ static rpma_log_level Rpma_log_stderr_threshold = RPMA_LOG_DISABLED;
  * get_timestamp_prefix -- provide actual time in a readable string
  *
  * ASSUMPTIONS:
- * - buf != NULL
+ * - buf != NULL && buf_size >= 16
  */
 static void
 get_timestamp_prefix(char *buf, size_t buf_size)
@@ -79,20 +79,26 @@ get_timestamp_prefix(char *buf, size_t buf_size)
 	struct timespec ts;
 	long usec;
 
+	/*
+	 * to ensure that we have enough space for fixed messages
+	 */
+	if (buf_size < 16)
+		return;
+
 	if (clock_gettime(CLOCK_REALTIME, &ts) ||
 	    (NULL == (info = localtime(&ts.tv_sec)))) {
-		snprintf(buf, buf_size, "[unknown time] ");
+		(void) snprintf(buf, buf_size, "[unknown time] ");
 		return;
 	}
 
 	usec = ts.tv_nsec / 1000;
 	if (!strftime(date, sizeof(date), "%Y-%m-%d %H:%M:%S", info)) {
-		snprintf(buf, buf_size, "[unknown time] ");
+		(void) snprintf(buf, buf_size, "[unknown time] ");
 		return;
 	}
 
 	if (snprintf(buf, buf_size, "[%s.%06ld] ", date, usec) < 0) {
-		*buf = '\0';
+		(void) snprintf(buf, buf_size, "[unknown time] ");
 		return;
 	}
 }
@@ -144,7 +150,9 @@ rpma_log_function(rpma_log_level level, const char *file_name,
 				rpma_log_level_names[level]) < 0)
 			strcpy(prefix, "[error prefix]: ");
 	} else {
-		prefix[0] = '\0';
+		if (snprintf(prefix, sizeof(prefix), "*%s*: ",
+				rpma_log_level_names[level]) < 0)
+			strcpy(prefix, "[error prefix]: ");
 	}
 
 	if (level <= Rpma_log_stderr_threshold) {
@@ -154,8 +162,8 @@ rpma_log_function(rpma_log_level level, const char *file_name,
 
 	if (level <= Rpma_log_syslog_threshold) {
 		if (level != RPMA_LOG_DISABLED) {
-			int severity = rpma_log_level2syslog_severity(level);
-			syslog(severity, "%s%s", prefix, message);
+			syslog(rpma_log_level2syslog_severity(level),
+				"%s%s", prefix, message);
 		}
 	}
 }
