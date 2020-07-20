49bbe06589d82dd0fe65aa9d665ace28a2738023
Tomasz Gromadzki <grom72@o2.pl>
2020-07-20 06:36:23 +0200

    rpma: ut: logging to custom function minor fixes

--- tests/unit/log-test/log-test-to-custom-function.c
+++ tests/unit/log-test/log-test-to-custom-function.c
@@ -5,7 +5,7 @@
 
 /*
  * log-test-to-custom-function.c -- unit tests of the log module with custom
- * log function provided in rpma_log_init() function
+ * log function set in rpma_log_init() function
  */
 
 #include <stdlib.h>
@@ -14,34 +14,34 @@
 #include "log-test-to-syslog.h"
 
 /*
- * custom_log_function -- use-defined custom log function mock. It is used
- * instead of writing to syslog/stderr.
+ * custom_log_function -- user-defined custom log function mock. It is used
+ * instead of the default function that writes to syslog/stderr.
  */
 static void
 custom_log_function(rpma_log_level level, const char *file_name,
 		const int line_no, const char *function_name,
-		const char *format, va_list args)
+		const char *message_format, va_list args)
 {
 	check_expected(level);
-	if (file_name)
-		check_expected(file_name);
+	check_expected(file_name);
 	check_expected(line_no);
-	if (function_name)
-		check_expected(function_name);
-	check_expected(format);
-
-	char custom_log_function_output [256];
-	int ret = vsnprintf(custom_log_function_output,
-		sizeof(custom_log_function_output), format, args);
-	assert_true(ret > 0);
-	if (ret > 0)
-		check_expected_ptr(custom_log_function_output);
+	check_expected(function_name);
+	check_expected(message_format);
+	char *arg = va_arg(args, char *);
+	char *message = mock_ptr_type(char *);
+	assert_string_equal(arg, message);
 }
 
+/*
+ * lrpma_log_input -- test condition selection enum
+ */
 enum rpma_log_input {
-	rpma_log_input_non_null,
-	rpma_log_input_file_name_null,
-	rpma_log_input_file_name_function_name_null
+	/* file name and function name is given */
+	rpma_log_input__no_nulls,
+	/* only function name is given */
+	rpma_log_input__file_NULL,
+	/* file name and function name are NULL */
+	rpma_log_input__file_function_NULL
 };
 
 /*
@@ -58,12 +58,12 @@ log__to_user_function(void **config_ptr)
 	rpma_log_fini();
 
 	/*
-	 * enable syslog mocks to monitor if no output is written to syslog()
+	 * enable syslog mocks to monitor there is no calls related to syslog()
 	 */
 	syslog_mock_enable();
 
 	/*
-	 * start log with custom function
+	 * start log with a custom function
 	 */
 	assert_int_equal(0, rpma_log_init(custom_log_function));
 	for (rpma_log_level level = RPMA_LOG_DISABLED;
@@ -73,25 +73,29 @@ log__to_user_function(void **config_ptr)
 		 */
 		expect_value(custom_log_function, level, level);
 
-		if (rpma_log_input_non_null == *config) {
+		if (rpma_log_input__no_nulls == *config) {
 			expect_string(custom_log_function, file_name,
 					TEST_FILE_NAME);
+		} else {
+			expect_value(custom_log_function, file_name, NULL);
 		}
 		expect_value(custom_log_function, line_no, TEST_LINE_NO);
-		if (rpma_log_input_file_name_function_name_null != *config) {
+		if (rpma_log_input__file_function_NULL != *config) {
 			expect_string(custom_log_function, function_name,
 				TEST_FUNCTION_NAME);
+		} else {
+			expect_value(custom_log_function, function_name, NULL);
 		}
-		expect_string(custom_log_function, format, "%s");
-		expect_string(custom_log_function, custom_log_function_output,
-				TEST_MESSAGE);
+
+		expect_string(custom_log_function, message_format, "%s");
+		will_return(custom_log_function, TEST_MESSAGE);
 		/*
 		 * run test
 		 */
 		rpma_log(level,
-			rpma_log_input_non_null == *config?TEST_FILE_NAME:NULL,
+			rpma_log_input__no_nulls == *config?TEST_FILE_NAME:NULL,
 			TEST_LINE_NO,
-			rpma_log_input_file_name_function_name_null != *config?
+			rpma_log_input__file_function_NULL != *config?
 				TEST_FUNCTION_NAME: NULL,
 			"%s", TEST_MESSAGE);
 	}
@@ -106,9 +110,9 @@ log__to_user_function(void **config_ptr)
 		 * run test
 		 */
 		rpma_log(level,
-			rpma_log_input_non_null == *config?TEST_FILE_NAME:NULL,
+			rpma_log_input__no_nulls == *config?TEST_FILE_NAME:NULL,
 			TEST_LINE_NO,
-			rpma_log_input_file_name_function_name_null != *config?
+			rpma_log_input__file_function_NULL != *config?
 				TEST_FUNCTION_NAME: NULL,
 			"%s", TEST_MESSAGE);
 	}
@@ -117,16 +121,16 @@ log__to_user_function(void **config_ptr)
 /*
  * for tests with file name and function name provided
  */
-static enum rpma_log_input non_null = rpma_log_input_non_null;
+static enum rpma_log_input non_null = rpma_log_input__no_nulls;
 /*
  * for tests with only function name provided
  */
-static enum rpma_log_input file_name__null = rpma_log_input_file_name_null;
+static enum rpma_log_input file_name__null = rpma_log_input__file_NULL;
 /*
  * for tests without file name and function name provided
  */
 static enum rpma_log_input file_name_function_name_null =
-				rpma_log_input_file_name_function_name_null;
+				rpma_log_input__file_function_NULL;
 
 const struct CMUnitTest log_test_to_custom_function[] = {
 	/*
