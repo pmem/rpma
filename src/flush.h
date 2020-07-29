dd9f754763b95a8ca6ad1be2adae699a0b52f057
osalyk <oksana.salyk@intel.com>
2020-07-29 10:21:01 +0200

    rpma: add flush func and fix tests

--- src/flush.h
+++ src/flush.h
@@ -26,7 +26,8 @@ struct rpma_flush {
  * ERRORS
  * rpma_flush_new() can fail with the following errors:
  *
- * - XXX
+ * - RPMA_E_NOMEM - out of memory
+ * - RPMA_E_PROVIDER - sysconf() or ibv_reg_mr() failed
  */
 int rpma_flush_new(struct rpma_peer *peer, struct rpma_flush **flush_ptr);
 
@@ -34,7 +35,7 @@ int rpma_flush_new(struct rpma_peer *peer, struct rpma_flush **flush_ptr);
  * ERRORS
  * rpma_flush_delete() can fail with the following errors:
  *
- * - XXX
+ * - RPMA_E_PROVIDER ibv_dereg_mr() failed
  */
 int rpma_flush_delete(struct rpma_flush **flush_ptr);
 
