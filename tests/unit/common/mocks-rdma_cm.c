1456170a63a47c13ad4214ff537870ae08856c0f
Lukasz Dorau <lukasz.dorau@intel.com>
2020-08-07 13:54:34 +0200

    test: reorganization of the 'info' unit test

--- tests/unit/common/mocks-rdma_cm.c
+++ tests/unit/common/mocks-rdma_cm.c
@@ -13,9 +13,12 @@
 #include "conn_req.h"
 #include "cmocka_headers.h"
 #include "mocks-rdma_cm.h"
+#include "test-common.h"
 
 /* mock control entity */
 int Mock_ctrl_defer_destruction = MOCK_CTRL_NO_DEFER;
+
+const struct rdma_cm_id Cmid_zero = {0};
 
 /*
  * rdma_destroy_qp -- rdma_destroy_qp() mock
@@ -168,6 +171,80 @@ int
 rdma_disconnect(struct rdma_cm_id *id)
 {
 	check_expected_ptr(id);
+
+	errno = mock_type(int);
+	if (errno)
+		return -1;
+
+	return 0;
+}
+
+/*
+ * rdma_getaddrinfo -- rdma_getaddrinfo() mock
+ */
+int
+rdma_getaddrinfo(const char *node, const char *service,
+		const struct rdma_addrinfo *hints, struct rdma_addrinfo **res)
+{
+	struct rdma_addrinfo_args *args =
+				mock_type(struct rdma_addrinfo_args *);
+	if (args->validate_params == MOCK_VALIDATE) {
+		assert_string_equal(node, MOCK_IP_ADDRESS);
+		assert_string_equal(service, MOCK_SERVICE);
+		check_expected(hints->ai_flags);
+	}
+
+	*res = args->res;
+
+	if (*res != NULL)
+		return 0;
+
+	errno = mock_type(int);
+
+	return -1;
+}
+
+/*
+ * rdma_freeaddrinfo -- rdma_freeaddrinfo() mock
+ */
+void
+rdma_freeaddrinfo(struct rdma_addrinfo *res)
+{
+	struct rdma_addrinfo_args *args =
+				mock_type(struct rdma_addrinfo_args *);
+	if (args->validate_params == MOCK_VALIDATE)
+		assert_ptr_equal(res, args->res);
+}
+
+/*
+ * rdma_resolve_addr -- rdma_resolve_addr() mock
+ * Note: CM ID is not modified.
+ */
+int
+rdma_resolve_addr(struct rdma_cm_id *id, struct sockaddr *src_addr,
+		struct sockaddr *dst_addr, int timeout_ms)
+{
+	check_expected_ptr(id);
+	check_expected_ptr(src_addr);
+	check_expected_ptr(dst_addr);
+	check_expected(timeout_ms);
+
+	errno = mock_type(int);
+	if (errno)
+		return -1;
+
+	return 0;
+}
+
+/*
+ * rdma_bind_addr -- rdma_bind_addr() mock
+ * Note: CM ID is not modified.
+ */
+int
+rdma_bind_addr(struct rdma_cm_id *id, struct sockaddr *addr)
+{
+	check_expected_ptr(id);
+	check_expected_ptr(addr);
 
 	errno = mock_type(int);
 	if (errno)
---
+++ tests/unit/info/info-resolve_addr.c
@@ -0,0 +1,134 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * info-resolve_addr.c -- unit tests of the info module
+ *
+ * API covered:
+ * - rpma_info_resolve_addr()
+ */
+
+#include <stdlib.h>
+
+#include "cmocka_headers.h"
+#include "conn_req.h"
+#include "info.h"
+#include "librpma.h"
+#include "info-common.h"
+#include "mocks-rdma_cm.h"
+
+#include <infiniband/verbs.h>
+
+/*
+ * resolve_addr__id_NULL -- NULL id is invalid
+ */
+static void
+resolve_addr__id_NULL(void **info_state_ptr)
+{
+	struct info_state *istate = *info_state_ptr;
+
+	/* run test */
+	int ret = rpma_info_resolve_addr(istate->info, NULL);
+
+	/* verify the result */
+	assert_int_equal(ret, RPMA_E_INVAL);
+}
+
+/*
+ * resolve_addr__info_NULL -- NULL info is invalid
+ */
+static void
+resolve_addr__info_NULL(void **unused)
+{
+	/* run test */
+	struct rdma_cm_id cmid = {0};
+	int ret = rpma_info_resolve_addr(NULL, &cmid);
+
+	/* verify the result */
+	assert_int_equal(ret, RPMA_E_INVAL);
+	assert_int_equal(memcmp(&cmid, &Cmid_zero, sizeof(cmid)), 0);
+}
+
+/*
+ * resolve_addr__id_info_NULL -- NULL id and info are invalid
+ */
+static void
+resolve_addr__id_info_NULL(void **unused)
+{
+	/* run test */
+	int ret = rpma_info_resolve_addr(NULL, NULL);
+
+	/* verify the result */
+	assert_int_equal(ret, RPMA_E_INVAL);
+}
+
+/*
+ * resolve_addr__resolve_addr_EAGAIN -- rdma_resolve_addr() fails
+ * with EAGAIN
+ */
+static void
+resolve_addr__resolve_addr_EAGAIN(void **info_state_ptr)
+{
+	struct info_state *istate = *info_state_ptr;
+
+	/* configure mocks */
+	struct rdma_cm_id cmid = {0};
+	expect_value(rdma_resolve_addr, id, &cmid);
+	expect_value(rdma_resolve_addr, src_addr, MOCK_SRC_ADDR);
+	expect_value(rdma_resolve_addr, dst_addr, MOCK_DST_ADDR);
+	expect_value(rdma_resolve_addr, timeout_ms, RPMA_DEFAULT_TIMEOUT);
+	will_return(rdma_resolve_addr, EAGAIN);
+
+	/* run test */
+	int ret = rpma_info_resolve_addr(istate->info, &cmid);
+
+	/* verify the result */
+	assert_int_equal(ret, RPMA_E_PROVIDER);
+	assert_int_equal(rpma_err_get_provider_error(), EAGAIN);
+	assert_int_equal(memcmp(&cmid, &Cmid_zero, sizeof(cmid)), 0);
+}
+
+/*
+ * resolve_addr__success -- happy day scenario
+ */
+static void
+resolve_addr__success(void **info_state_ptr)
+{
+	struct info_state *istate = *info_state_ptr;
+
+	/* configure mocks */
+	struct rdma_cm_id cmid = {0};
+	expect_value(rdma_resolve_addr, id, &cmid);
+	expect_value(rdma_resolve_addr, src_addr, MOCK_SRC_ADDR);
+	expect_value(rdma_resolve_addr, dst_addr, MOCK_DST_ADDR);
+	expect_value(rdma_resolve_addr, timeout_ms, RPMA_DEFAULT_TIMEOUT);
+	will_return(rdma_resolve_addr, MOCK_OK);
+
+	/* run test */
+	int ret = rpma_info_resolve_addr(istate->info, &cmid);
+
+	/* verify the result */
+	assert_int_equal(ret, MOCK_OK);
+	assert_int_equal(memcmp(&cmid, &Cmid_zero, sizeof(cmid)), 0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	const struct CMUnitTest tests[] = {
+		/* rpma_info_resolve_addr() unit tests */
+		cmocka_unit_test_setup_teardown(resolve_addr__id_NULL,
+				setup__new_active, teardown__delete),
+		cmocka_unit_test(resolve_addr__info_NULL),
+		cmocka_unit_test(resolve_addr__id_info_NULL),
+		cmocka_unit_test_setup_teardown(
+				resolve_addr__resolve_addr_EAGAIN,
+				setup__new_active, teardown__delete),
+		cmocka_unit_test_setup_teardown(resolve_addr__success,
+				setup__new_active, teardown__delete),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
