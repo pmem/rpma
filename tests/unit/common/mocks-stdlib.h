7a92f2870096733b6bfc47f1554480c06525e627
Lukasz Dorau <lukasz.dorau@intel.com>
2020-08-07 13:59:29 +0200

    test: reorganization of the 'flush' unit test

---
+++ tests/unit/common/mocks-stdlib.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020, Intel Corporation
+ */
+
+/*
+ * mocks-stdlib.h -- the stdlib mocks' header
+ */
+
+#ifndef MOCKS_STDLIB_H
+#define MOCKS_STDLIB_H
+
+struct posix_memalign_args {
+	void *ptr;
+};
+
+#endif /* MOCKS_STDLIB_H */
