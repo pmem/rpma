2ef381ad202edf73230f48d3ef2dc457d179fef3
Changcheng Liu <changcheng.liu@aliyun.com>
2020-08-11 15:49:25 +0800

    example/write-to-persist: get/ack CQ event before polling cq
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- examples/04-write-to-persistent/client.c
+++ examples/04-write-to-persistent/client.c
@@ -243,6 +243,12 @@ main(int argc, char *argv[])
 	if (ret)
 		goto err_conn_disconnect;
 
+	ret = rpma_conn_prepare_completions(conn);
+	if (ret && ret != RPMA_E_NO_COMPLETION) {
+		print_error_ex("rpma_conn_prepare_completions", ret);
+		goto err_conn_disconnect;
+	}
+
 	ret = rpma_conn_next_completion(conn, &cmpl);
 	if (ret) {
 		print_error_ex("rpma_conn_next_completion", ret);
