becdb965167c485feff6c2af526ccaed0b2d69dd
Lukasz Dorau <lukasz.dorau@intel.com>
2020-08-17 14:21:11 +0200

    test: reorganization of the 'flush' unit test

---
+++ tests/unit/flush/flush-common.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * flush-common.h -- header of the common part of unit tests
+ * of the flush module
+ */
+
+#ifndef FLUSH_COMMON_H
+#define FLUSH_COMMON_H 1
+
+#define MOCK_RPMA_MR_REMOTE	(struct rpma_mr_remote *)0xC412
+#define MOCK_RPMA_MR_LOCAL	(struct rpma_mr_local *)0xC411
+#define MOCK_REMOTE_OFFSET	(size_t)0xC414
+#define MOCK_LEN		(size_t)0xC415
+#define MOCK_FLAGS		(int)0xC416
+#define MOCK_OP_CONTEXT		(void *)0xC417
+#define MOCK_RAW_LEN		8
+
+/*
+ * All the resources used between setup__flush_new and teardown__flush_delete.
+ */
+struct flush_test_state {
+	struct rpma_flush *flush;
+};
+
+int setup__flush_new(void **fstate_ptr);
+int teardown__flush_delete(void **fstate_ptr);
+
+#endif /* FLUSH_COMMON_H */
---
+++ tests/unit/flush/flush-new.c
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * flush-new.c -- unit tests of the flush module
+ *
+ * APIs covered:
+ * - rpma_flush_new
+ * - rpma_flush_delete
+ */
+
+#include "cmocka_headers.h"
+#include "flush.h"
+#include "flush-common.h"
+#include "mocks-stdlib.h"
+#include "test-common.h"
+
+/*
+ * new__malloc_ENOMEM -- malloc() fail with ENOMEM
+ */
+static void
+new__malloc_ENOMEM(void **unused)
+{
+	/* configure mocks */
+	will_return(__wrap__test_malloc, ENOMEM);
+
+	/* run test */
+	struct rpma_flush *flush = NULL;
+	int ret = rpma_flush_new(MOCK_PEER, &flush);
+
+	/* verify the results */
+	assert_int_equal(ret, RPMA_E_NOMEM);
+	assert_null(flush);
+}
+
+/*
+ * new__apm_sysconf_EINVAL -- sysconf() fail with EINVAL
+ */
+static void
+new__apm_sysconf_EINVAL(void **unused)
+{
+	/* configure mocks */
+	will_return_always(__wrap__test_malloc, MOCK_OK);
+	will_return(__wrap_sysconf, EINVAL);
+
+	/* run test */
+	struct rpma_flush *flush = NULL;
+	int ret = rpma_flush_new(MOCK_PEER, &flush);
+
+	/* verify the results */
+	assert_int_equal(ret, RPMA_E_PROVIDER);
+	assert_int_equal(rpma_err_get_provider_error(), EINVAL);
+	assert_null(flush);
+}
+
+/*
+ * new__apm_posix_memalign_ENOMEM -- malloc() fail with ENOMEM
+ */
+static void
+new__apm_posix_memalign_ENOMEM(void **unused)
+{
+	/* configure mocks */
+	will_return_always(__wrap__test_malloc, MOCK_OK);
+	will_return(__wrap_sysconf, MOCK_OK);
+	will_return(__wrap_posix_memalign, ENOMEM);
+
+	/* run test */
+	struct rpma_flush *flush = NULL;
+	int ret = rpma_flush_new(MOCK_PEER, &flush);
+
+	/* verify the results */
+	assert_int_equal(ret, RPMA_E_NOMEM);
+	assert_null(flush);
+}
+
+/*
+ * new__apm_mr_reg_ENOMEM -- malloc() fail with ENOMEM
+ */
+static void
+new__apm_mr_reg_RPMA_E_NOMEM(void **unused)
+{
+	/* configure mocks */
+	will_return_always(__wrap__test_malloc, MOCK_OK);
+	will_return(__wrap_sysconf, MOCK_OK);
+
+	will_return(__wrap_posix_memalign, MOCK_OK);
+	struct posix_memalign_args allocated_raw = {0};
+	will_return(__wrap_posix_memalign, &allocated_raw);
+	expect_value(rpma_mr_reg, peer, MOCK_PEER);
+	expect_value(rpma_mr_reg, size, 8);
+	expect_value(rpma_mr_reg, usage, RPMA_MR_USAGE_READ_DST);
+	will_return(rpma_mr_reg, &allocated_raw.ptr);
+	will_return(rpma_mr_reg, NULL);
+	will_return(rpma_mr_reg, RPMA_E_NOMEM);
+
+	/* run test */
+	struct rpma_flush *flush = NULL;
+	int ret = rpma_flush_new(MOCK_PEER, &flush);
+
+	/* verify the results */
+	assert_int_equal(ret, RPMA_E_NOMEM);
+	assert_null(flush);
+}
+
+/*
+ * new__apm_success -- happy day scenario
+ */
+static void
+new__apm_success(void **unused)
+{
+	/*
+	 * The thing is done by setup__flush_new()
+	 * and teardown__flush_delete().
+	 */
+}
+
+int
+main(int argc, char *argv[])
+{
+	const struct CMUnitTest tests[] = {
+		/* rpma__new() unit tests */
+		cmocka_unit_test(new__malloc_ENOMEM),
+		cmocka_unit_test(new__apm_sysconf_EINVAL),
+		cmocka_unit_test(new__apm_posix_memalign_ENOMEM),
+		cmocka_unit_test(new__apm_mr_reg_RPMA_E_NOMEM),
+		cmocka_unit_test_setup_teardown(new__apm_success,
+			setup__flush_new, teardown__flush_delete),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
