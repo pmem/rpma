5ece62737ef889068fbf68c5324a06e027c9f0d5
osalyk <oksana.salyk@intel.com>
2020-08-27 15:56:07 +0200

    examples: add atomic_write example

---
+++ examples/07-atomic-write/README.md
@@ -0,0 +1,44 @@
+Example of performing an RPMA atomic write to a persistent memory region
+===
+
+The atomic write example shows a simple remote log manipulation where two
+parts of the process are implemented:
+- The server, if provided (and capable of), prepares a local persistent memory.
+The memory is filled with an initial log state. The simplest log structure
+looks as follow:
+```
+struct log {
+	char signature[LOG_SIGNATURE_SIZE];
+	/* last written data (note the field is aligned to RPMA_ATOMIC_WRITE_ALIGNMENT) */
+	uint64_t used;
+	char data[LOG_DATA_SIZE];
+};
+```
+The client along with the exposed memory descriptor receives an offset of
+the used field. Which is enough to read the current state of the log and
+perform an append. After the connection is established the server waits for
+the client to disconnect.
+- The client, after the connection is established, receives the server's
+memory descriptor and the used offset. The client reads the used value.
+Having that, the client writes and flushes the new data at the end of the log.
+Without waiting for both operations completions the client can perform
+an atomic write of the new used value and flush it. The client has to wait
+for the last flush completion before appending another entry to the log.
+When the client is done it simply disconnects from the server.
+
+**Note**: If server does not have a pmem path (or it is not
+capable to use pmem at all) it uses DRAM instead.
+
+**Note**: For the sake of this example, the memory region being write from is
+transferred via connection's private data. In general, it can be transferred via
+an out-of-band or the in-band channel.
+
+## Usage
+
+```bash
+[user@server]$ ./server $server_address $port [<pmem-path>]
+```
+
+```bash
+[user@client]$ ./client $server_address $port $word1 [<word2>] [<...>]
+```
---
+++ examples/07-atomic-write/server.c
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * server.c -- a server of the atomic-write example
+ *
+ * Please see README.md for a detailed description of this example.
+ */
+
+#include <inttypes.h>
+#include <librpma.h>
+#include <librpma_log.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef USE_LIBPMEM
+#include <libpmem.h>
+#define USAGE_STR "usage: %s <server_address> <port> [<pmem-path>]\n"
+#else
+#define USAGE_STR "usage: %s <server_address> <port>\n"
+#endif /* USE_LIBPMEM */
+
+#include "common-conn.h"
+
+#define LOG_HDR_SIGNATURE "LOG"
+#define LOG_SIGNATURE_SIZE 8
+#define LOG_DATA_SIZE 100000 /* 100k */
+
+/* defined log structure */
+struct log {
+	char signature[LOG_SIGNATURE_SIZE];
+	/* last written data (aligned to RPMA_ATOMIC_WRITE_ALIGNMENT) */
+	uint64_t used;
+	char data[LOG_DATA_SIZE];
+};
+
+int
+main(int argc, char *argv[])
+{
+	/* validate parameters */
+	if (argc < 3) {
+		fprintf(stderr, USAGE_STR, argv[0]);
+		return -1;
+	}
+
+	/* configure logging thresholds to see more details */
+	rpma_log_set_threshold(RPMA_LOG_THRESHOLD, RPMA_LOG_LEVEL_INFO);
+	rpma_log_set_threshold(RPMA_LOG_THRESHOLD_AUX, RPMA_LOG_LEVEL_INFO);
+
+	/* read common parameters */
+	char *addr = argv[1];
+	char *port = argv[2];
+	int ret;
+
+	/* resources - memory region */
+	void *mr_ptr = NULL;
+	size_t mr_size = 0;
+	enum rpma_mr_plt mr_plt = RPMA_MR_PLT_VOLATILE;
+	struct rpma_mr_local *mr = NULL;
+
+	struct log *log;
+
+#ifdef USE_LIBPMEM
+	if (argc == 4) {
+		char *path = argv[3];
+		int is_pmem;
+
+		/* map the file */
+		mr_ptr = pmem_map_file(path, 0 /* len */, 0 /* flags */,
+				0 /* mode */, &mr_size, &is_pmem);
+		if (mr_ptr == NULL)
+			return -1;
+
+		/* pmem is expected */
+		if (!is_pmem) {
+			(void) pmem_unmap(mr_ptr, mr_size);
+			return -1;
+		}
+
+		/*
+		 * At the beginning of the persistent memory, a signature is
+		 * stored which marks its content as valid. So the length
+		 * of the mapped memory has to be at least of the length of
+		 * the signature to convey any meaningful content and be usable
+		 * as a persistent store.
+		 */
+		if (mr_size < LOG_SIGNATURE_SIZE) {
+			(void) fprintf(stderr, "%s too small (%zu < %u)\n",
+					path, mr_size, LOG_SIGNATURE_SIZE);
+			(void) pmem_unmap(mr_ptr, mr_size);
+			return -1;
+		}
+
+		/*
+		 * All of the space under the offset is intended for
+		 * the string contents. Space is assumed to be at least 1 KiB.
+		 */
+		if (mr_size - LOG_SIGNATURE_SIZE < KILOBYTE) {
+			fprintf(stderr, "%s too small (%zu < %u)\n",
+					path, mr_size,
+					KILOBYTE + LOG_SIGNATURE_SIZE);
+			(void) pmem_unmap(mr_ptr, mr_size);
+			return -1;
+		}
+
+		log = mr_ptr;
+
+		/*
+		 * If the signature is not in place the persistent content has
+		 * to be initialized and persisted.
+		 */
+		if (strncmp(mr_ptr, SIGNATURE_STR, SIGNATURE_LEN) != 0) {
+			/* initialize used value and persist it */
+			log->used = offsetof(struct log, data);
+			pmem_persist(&log->used, sizeof(uint64_t));
+			/* write the signature to mark the content as valid */
+			strncpy(mr_ptr, LOG_HDR_SIGNATURE, LOG_SIGNATURE_SIZE);
+			pmem_persist(mr_ptr, LOG_SIGNATURE_SIZE);
+		}
+
+		mr_plt = RPMA_MR_PLT_PERSISTENT;
+	}
+#endif
+
+	/* if no pmem support or it is not provided */
+	if (mr_ptr == NULL) {
+		size_t log_size = (sizeof(struct log) +
+				(sizeof(char) * LOG_DATA_SIZE));
+		log = malloc_aligned(log_size);
+		if (log == NULL)
+			return -1;
+
+		log->used = offsetof(struct log, data);
+
+		mr_ptr = (void *)log;
+		mr_size = log_size;
+		mr_plt = RPMA_MR_PLT_VOLATILE;
+	}
+
+	/* RPMA resources */
+	struct rpma_peer *peer = NULL;
+	struct rpma_ep *ep = NULL;
+	struct rpma_conn *conn = NULL;
+
+	/*
+	 * lookup an ibv_context via the address and create a new peer using it
+	 */
+	if ((ret = server_peer_via_address(addr, &peer)))
+		goto err_free;
+
+	/* start a listening endpoint at addr:port */
+	if ((ret = rpma_ep_listen(peer, addr, port, &ep)))
+		goto err_peer_delete;
+
+	/* register the memory */
+	if ((ret = rpma_mr_reg(peer, mr_ptr, mr_size,
+			RPMA_MR_USAGE_WRITE_DST | RPMA_MR_USAGE_READ_SRC,
+			mr_plt, &mr)))
+		goto err_ep_shutdown;
+
+	/* calculate data for the client write */
+	struct common_data data;
+	data.data_offset = offsetof(struct log, used);
+
+	/* get the memory region's descriptor */
+	if ((ret = rpma_mr_get_descriptor(mr, &data.desc)))
+		goto err_mr_dereg;
+
+	/*
+	 * Wait for an incoming connection request, accept it and wait for its
+	 * establishment.
+	 */
+	struct rpma_conn_private_data pdata;
+	pdata.ptr = &data;
+	pdata.len = sizeof(struct common_data);
+	if ((ret = server_accept_connection(ep, &pdata, &conn)))
+		goto err_mr_dereg;
+
+	/*
+	 * Wait for RPMA_CONN_CLOSED, disconnect and delete the connection
+	 * structure.
+	 */
+	if ((ret = common_wait_for_conn_close_and_disconnect(&conn)))
+		goto err_mr_dereg;
+
+	/* print the saved data */
+	char *ptr = log->data;
+	char *log_data_tail = (char *)log + log->used;
+	while (ptr < log_data_tail) {
+		if (*ptr)
+			putc(*ptr, stdout);
+		else
+			putc('\n', stdout);
+		ptr++;
+	}
+
+err_mr_dereg:
+	/* deregister the memory region */
+	(void) rpma_mr_dereg(&mr);
+
+err_ep_shutdown:
+	/* shutdown the endpoint */
+	(void) rpma_ep_shutdown(&ep);
+
+err_peer_delete:
+	/* delete the peer object */
+	(void) rpma_peer_delete(&peer);
+
+err_free:
+#ifdef USE_LIBPMEM
+	if (mr_plt == RPMA_MR_PLT_PERSISTENT) {
+		pmem_unmap(mr_ptr, mr_size);
+		mr_ptr = NULL;
+	}
+#endif
+
+	if (mr_plt == RPMA_MR_PLT_VOLATILE)
+		free(log);
+
+	return ret ? -2 : 0;
+}
