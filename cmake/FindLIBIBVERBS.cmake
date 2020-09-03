b4837adefbdb24084e645bdd303d8e35a686ea0d
Lukasz Dorau <lukasz.dorau@intel.com>
2020-09-03 11:05:11 +0200

    common: find libibverbs and librdmacm w/o PkgConfig
    Make it possible to find the libibverbs and librdmacm libraries
    without PkgConfig (e.g. when those libraries do not provide
    a package-config file (*.pc) what happens in case of older OS releases.
    
    Ref: #289

---
+++ cmake/FindLIBIBVERBS.cmake
@@ -0,0 +1,20 @@
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+#
+
+message(STATUS "Checking for module 'libibverbs' w/o PkgConfig")
+
+find_library(LIBIBVERBS_LIBRARY NAMES libibverbs.so libibverbs ibverbs)
+set(LIBIBVERBS_LIBRARIES ${LIBIBVERBS_LIBRARY})
+
+if(LIBIBVERBS_LIBRARY)
+	message(STATUS "  Found libibverbs w/o PkgConfig")
+else()
+	set(MSG_NOT_FOUND "libibverbs NOT found (set CMAKE_PREFIX_PATH to point the location)")
+	if(LIBIBVERBS_FIND_REQUIRED)
+		message(FATAL_ERROR ${MSG_NOT_FOUND})
+	else()
+		message(WARNING ${MSG_NOT_FOUND})
+	endif()
+endif()
---
+++ cmake/FindLIBRDMACM.cmake
@@ -0,0 +1,20 @@
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+#
+
+message(STATUS "Checking for module 'librdmacm' w/o PkgConfig")
+
+find_library(LIBRDMACM_LIBRARY NAMES librdmacm.so librdmacm rdmacm)
+set(LIBRDMACM_LIBRARIES ${LIBRDMACM_LIBRARY})
+
+if(LIBRDMACM_LIBRARY)
+	message(STATUS "  Found librdmacm w/o PkgConfig")
+else()
+	set(MSG_NOT_FOUND "librdmacm NOT found (set CMAKE_PREFIX_PATH to point the location)")
+	if(LIBRDMACM_FIND_REQUIRED)
+		message(FATAL_ERROR ${MSG_NOT_FOUND})
+	else()
+		message(WARNING ${MSG_NOT_FOUND})
+	endif()
+endif()
