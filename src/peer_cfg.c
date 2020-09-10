3de3da535c0bf3a1256f0b7d9b6be5547ff1a543
Lukasz Dorau <lukasz.dorau@intel.com>
2020-09-10 12:26:45 +0200

    rpma: implement rpma_peer_cfg

--- src/peer_cfg.c
+++ src/peer_cfg.c
@@ -9,13 +9,21 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
+#include "librpma.h"
 #include "log_internal.h"
 #include "peer_cfg.h"
 #include "rpma_err.h"
+#include "stdbool.h"
 
 #ifdef TEST_MOCK_ALLOC
 #include "cmocka_alloc.h"
 #endif
+
+#define SUPPORTED2STR(var) ((var) ? "supported" : "unsupported")
+
+struct rpma_peer_cfg {
+	bool direct_write_to_pmem;
+};
 
 /* internal librpma API */
 
@@ -23,47 +31,63 @@
  * rpma_peer_cfg_new -- create a new peer configuration object
  */
 int
-rpma_peer_cfg_new(struct rpma_peer_cfg **pcfg)
+rpma_peer_cfg_new(struct rpma_peer_cfg **pcfg_ptr)
 {
-	return RPMA_E_NOSUPP;
+	if (pcfg_ptr == NULL)
+		return RPMA_E_INVAL;
+
+	struct rpma_peer_cfg *cfg = malloc(sizeof(struct rpma_peer_cfg));
+	if (cfg == NULL)
+		return RPMA_E_NOMEM;
+
+	/* set default values */
+	cfg->direct_write_to_pmem = false;
+	*pcfg_ptr = cfg;
+	return 0;
 }
 
 /*
  * rpma_peer_cfg_delete -- delete the peer configuration object
  */
 int
-rpma_peer_cfg_delete(struct rpma_peer_cfg **pcfg)
+rpma_peer_cfg_delete(struct rpma_peer_cfg **pcfg_ptr)
 {
-	return RPMA_E_NOSUPP;
+	if (pcfg_ptr == NULL)
+		return RPMA_E_INVAL;
+
+	free(*pcfg_ptr);
+	*pcfg_ptr = NULL;
+	return 0;
 }
 
 /*
- * rpma_peer_cfg_set_ddio -- declare the DDIO state
+ * rpma_peer_cfg_set_direct_write_to_pmem -- declare if direct write
+ * to PMEM is supported
  */
 int
-rpma_peer_cfg_set_ddio(struct rpma_peer_cfg *pcfg, enum rpma_on_off_type state)
+rpma_peer_cfg_set_direct_write_to_pmem(struct rpma_peer_cfg *pcfg,
+		bool supported)
 {
-	return RPMA_E_NOSUPP;
+	if (pcfg == NULL)
+		return RPMA_E_INVAL;
+
+	pcfg->direct_write_to_pmem = supported;
+	return 0;
 }
 
 /*
- * rpma_peer_cfg_set_auto_flush -- declare the auto flush state
+ * rpma_peer_cfg_get_direct_write_to_pmem -- check if direct write
+ * to PMEM is supported
  */
 int
-rpma_peer_cfg_set_auto_flush(struct rpma_peer_cfg *pcfg,
-		enum rpma_on_off_type state)
+rpma_peer_cfg_get_direct_write_to_pmem(struct rpma_peer_cfg *pcfg,
+		bool *supported)
 {
-	return RPMA_E_NOSUPP;
-}
+	if (pcfg == NULL || supported == NULL)
+		return RPMA_E_INVAL;
 
-/*
- * rpma_peer_cfg_get_persistent_flush_supported -- XXX
- */
-int
-rpma_peer_cfg_get_persistent_flush_supported(struct rpma_peer_cfg *pcfg,
-		enum rpma_on_off_type *state)
-{
-	return RPMA_E_NOSUPP;
+	*supported = pcfg->direct_write_to_pmem;
+	return 0;
 }
 
 /*
@@ -73,7 +97,14 @@ int
 rpma_peer_cfg_get_descriptor(struct rpma_peer_cfg *pcfg,
 		rpma_peer_cfg_descriptor *desc)
 {
-	return RPMA_E_NOSUPP;
+	if (pcfg == NULL || desc == NULL)
+		return RPMA_E_INVAL;
+
+	char *buff = (char *)desc;
+
+	*((uint8_t *)buff) = (uint8_t)pcfg->direct_write_to_pmem;
+
+	return 0;
 }
 
 /*
@@ -82,7 +113,24 @@ rpma_peer_cfg_get_descriptor(struct rpma_peer_cfg *pcfg,
  */
 int
 rpma_peer_cfg_from_descriptor(rpma_peer_cfg_descriptor *desc,
-		struct rpma_peer_cfg **pcfg)
+		struct rpma_peer_cfg **pcfg_ptr)
 {
-	return RPMA_E_NOSUPP;
+	if (desc == NULL || pcfg_ptr == NULL)
+		return RPMA_E_INVAL;
+
+	char *buff = (char *)desc;
+
+	uint8_t direct_write_to_pmem = *(uint8_t *)buff;
+
+	struct rpma_peer_cfg *cfg = malloc(sizeof(struct rpma_peer_cfg));
+	if (cfg == NULL)
+		return RPMA_E_NOMEM;
+
+	cfg->direct_write_to_pmem = direct_write_to_pmem;
+	*pcfg_ptr = cfg;
+
+	RPMA_LOG_INFO("new rpma_peer_cfg(direct_write_to_pmem=%s)",
+			SUPPORTED2STR(direct_write_to_pmem));
+
+	return 0;
 }
