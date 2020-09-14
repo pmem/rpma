78de26e0fda77860abc48cf9185c793ff5a09ad2
Lukasz Dorau <lukasz.dorau@intel.com>
2020-09-14 10:20:03 +0200

    rpma: implement rpma_conn_apply_remote_peer_cfg()

---
+++ tests/unit/common/mocks-rpma-peer_cfg.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * mocks-rpma-peer_cfg.h -- a rpma-peer_cfg mocks header
+ */
+
+#ifndef MOCKS_RPMA_PEER_CFG_H
+#define MOCKS_RPMA_PEER_CFG_H
+
+#endif /* MOCKS_RPMA_PEER_CFG_H */
