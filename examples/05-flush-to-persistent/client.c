df62dc84e15a3f98eb7107343bcf795cef3bf61a
Lukasz Dorau <lukasz.dorau@intel.com>
2020-09-15 12:30:32 +0200

    rpma: remove rpma_peer_cfg_descriptor

--- examples/05-flush-to-persistent/client.c
+++ examples/05-flush-to-persistent/client.c
@@ -201,16 +201,24 @@ main(int argc, char *argv[])
 	 * descriptor and apply it to the current connection.
 	 */
 	struct common_data *dst_data = pdata.ptr;
-	ret = rpma_peer_cfg_from_descriptor(&dst_data->pcfg_desc, &pcfg);
+	ret = rpma_peer_cfg_get_descriptor_size(pcfg,
+			&dst_data->pcfg_desc_size);
 	if (ret)
 		goto err_mr_dereg;
+	dst_data->pcfg_desc = malloc(dst_data->pcfg_desc_size);
+	if (dst_data->pcfg_desc == NULL)
+		goto err_mr_dereg;
+	ret = rpma_peer_cfg_from_descriptor(dst_data->pcfg_desc,
+			dst_data->pcfg_desc_size, &pcfg);
+	if (ret)
+		goto err_free_pcfg_desc;
 	ret = rpma_peer_cfg_get_direct_write_to_pmem(pcfg,
 			&direct_write_to_pmem);
 	ret |= rpma_conn_apply_remote_peer_cfg(conn, pcfg);
 	(void) rpma_peer_cfg_delete(&pcfg);
 	/* either get or apply failed */
 	if (ret)
-		goto err_mr_dereg;
+		goto err_free_pcfg_desc;
 
 	/*
 	 * Create a remote memory registration structure from the received
@@ -219,7 +227,7 @@ main(int argc, char *argv[])
 	dst_offset = dst_data->data_offset;
 	ret = rpma_mr_remote_from_descriptor(&dst_data->mr_desc, &dst_mr);
 	if (ret)
-		goto err_mr_dereg;
+		goto err_free_pcfg_desc;
 
 	/* get the remote memory region size */
 	ret = rpma_mr_remote_get_size(dst_mr, &dst_size);
@@ -288,6 +296,9 @@ main(int argc, char *argv[])
 err_mr_remote_delete:
 	/* delete the remote memory region's structure */
 	(void) rpma_mr_remote_delete(&dst_mr);
+
+err_free_pcfg_desc:
+	free(dst_data->pcfg_desc);
 
 err_mr_dereg:
 	/* deregister the memory region */
--- src/peer_cfg.c
+++ src/peer_cfg.c
@@ -94,15 +94,27 @@ rpma_peer_cfg_get_direct_write_to_pmem(struct rpma_peer_cfg *pcfg,
  * rpma_peer_cfg_get_descriptor -- get a descriptor of a peer configuration
  */
 int
-rpma_peer_cfg_get_descriptor(struct rpma_peer_cfg *pcfg,
-		rpma_peer_cfg_descriptor *desc)
+rpma_peer_cfg_get_descriptor(struct rpma_peer_cfg *pcfg, void *desc)
 {
 	if (pcfg == NULL || desc == NULL)
 		return RPMA_E_INVAL;
 
-	char *buff = (char *)desc;
+	*((uint8_t *)desc) = (uint8_t)pcfg->direct_write_to_pmem;
 
-	*((uint8_t *)buff) = (uint8_t)pcfg->direct_write_to_pmem;
+	return 0;
+}
+
+/*
+ * rpma_peer_cfg_get_descriptor_size -- get size of the peer configuration
+ * descriptor
+ */
+int
+rpma_peer_cfg_get_descriptor_size(struct rpma_peer_cfg *pcfg, size_t *desc_size)
+{
+	if (pcfg == NULL || desc_size == NULL)
+		return RPMA_E_INVAL;
+
+	*desc_size = sizeof(uint8_t);
 
 	return 0;
 }
@@ -112,19 +124,25 @@ rpma_peer_cfg_get_descriptor(struct rpma_peer_cfg *pcfg,
  * from a descriptor
  */
 int
-rpma_peer_cfg_from_descriptor(rpma_peer_cfg_descriptor *desc,
+rpma_peer_cfg_from_descriptor(void *desc, size_t desc_size,
 		struct rpma_peer_cfg **pcfg_ptr)
 {
 	if (desc == NULL || pcfg_ptr == NULL)
 		return RPMA_E_INVAL;
 
-	char *buff = (char *)desc;
-
-	uint8_t direct_write_to_pmem = *(uint8_t *)buff;
+	size_t size = sizeof(uint8_t);
+	if (desc_size < size) {
+		RPMA_LOG_ERROR(
+			"incorrect size of the descriptor: %i bytes (should be at least %i bytes)",
+			desc_size, size);
+		return RPMA_E_INVAL;
+	}
 
 	struct rpma_peer_cfg *cfg = malloc(sizeof(struct rpma_peer_cfg));
 	if (cfg == NULL)
 		return RPMA_E_NOMEM;
+
+	uint8_t direct_write_to_pmem = *(uint8_t *)desc;
 
 	cfg->direct_write_to_pmem = direct_write_to_pmem;
 	*pcfg_ptr = cfg;
