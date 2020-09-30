d2800f63c09a0af8099650fd840f6a4b788c2dbb
Lukasz Dorau <lukasz.dorau@intel.com>
2020-09-30 11:07:31 +0200

    rpma: write 'MEMORY MANAGEMENT' section of manual

--- src/include/librpma.h
+++ src/include/librpma.h
@@ -96,10 +96,22 @@
  *
  * MEMORY MANAGEMENT
  *
- * - rpma_mr_reg - XXX
- * - rpma_mr_dereg - XXX
- * - rpma_mr_get_descriptor - XXX
- * - rpma_mr_remote_from_descriptor - XXX
+ * Every piece of RAM must be registered and its memory protection attributes
+ * must be specified in order to be used in an RDMA operation. This can be done
+ * using the following memory management librpma functions:
+ * - rpma_mr_reg() which registers a memory region and creates a local memory
+ * registration object and
+ * - rpma_mr_dereg() which deregisters the memory region and deletes
+ * the local memory registration object.
+ *
+ * A description of the registered memory region sometimes has to be
+ * transferred via network to the other side of the connection.
+ * In order to do that a network-transferable description
+ * of the provided memory region (called 'descriptor') has to be created using
+ * rpma_mr_get_descriptor(). Once the descriptor is transferred
+ * to the other side it should be decoded by rpma_mr_remote_from_descriptor()
+ * in order to create a remote memory region's structure
+ * which allows transferring data between the peers.
  *
  * MESSAGING
  *
@@ -673,11 +685,11 @@ int rpma_mr_dereg(struct rpma_mr_local **mr_ptr);
  *	int rpma_mr_get_descriptor(const struct rpma_mr_local *mr, void *desc);
  *
  * DESCRIPTION
- * rpma_mr_get_descriptor() writes a network-transferable description of
- * the provided local memory region. Once the descriptor is transferred to
- * the other side it can be consumed by rpma_mr_remote_from_descriptor() to
- * create a remote memory region's structure which allows transferring data
- * between the peers.
+ * rpma_mr_get_descriptor() writes a network-transferable description
+ * of the provided local memory region (called 'descriptor').
+ * Once the descriptor is transferred to the other side it should be decoded
+ * by rpma_mr_remote_from_descriptor() to create a remote memory region's
+ * structure which allows transferring data between the peers.
  *
  * RETURN VALUE
  * The rpma_mr_get_descriptor() function returns 0 on success or a negative
