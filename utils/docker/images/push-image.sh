0bd6e8a4b54677a0ecee078f4d0226be54b636a2
Lukasz Dorau <lukasz.dorau@intel.com>
2020-10-12 10:21:32 +0200

    common: migrate from Docker Hub to GitHub Container Registry

--- utils/docker/images/push-image.sh
+++ utils/docker/images/push-image.sh
@@ -5,11 +5,11 @@
 #
 
 #
-# push-image.sh - pushes the Docker image to the Docker Hub.
+# push-image.sh - pushes the Docker image to GitHub Container Registry.
 #
-# The script utilizes $DOCKERHUB_USER and $DOCKERHUB_PASSWORD variables
-# to log in to Docker Hub. The variables can be set in the CI project's
-# configuration for automated builds.
+# The script utilizes $GH_CR_USER and $GH_CR_PAT variables
+# to log in to GitHub Container Registry. The variables can be set
+# in the CI project's configuration for automated builds.
 #
 
 set -e
@@ -41,8 +41,8 @@ then
 	exit 1
 fi
 
-# Log in to the Docker Hub
-docker login -u="$DOCKERHUB_USER" -p="$DOCKERHUB_PASSWORD"
+# Log in to GitHub Container Registry
+docker login https://ghcr.io -u="$GH_CR_USER" -p="$GH_CR_PAT"
 
-# Push the image to the repository
+# Push the image to GitHub Container Registry
 docker push ${DOCKER_REPO}:${TAG}
