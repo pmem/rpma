53761f9e61d56bbdf2ee2eb3deaeca59e4efd43d
Wang,Long <long1.wang@intel.com>
2020-10-23 01:03:23 +0800

    test: integration test for 04-example-write-to-persistent
    Signed-off-by: Wang,Long <long1.wang@intel.com>

---
+++ examples/04-write-to-persistent/hello.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * hello.h -- a hello message structure declarations
+ */
+
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define HELLO_STR_SIZE 1024
+
+enum lang_t {en, es};
+
+struct hello_t {
+	enum lang_t lang;
+	char str[HELLO_STR_SIZE];
+};
+#endif
