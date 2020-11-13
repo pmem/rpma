fc6e813143e1f8c9efa326f2221c266df201b0e8
Wang,Long <long1.wang@intel.com>
2020-11-13 16:09:15 +0800

    test: valgrind test for all examples

--- examples/run-all-on-SoftRoCE.sh
+++ examples/run-all-on-SoftRoCE.sh
@@ -3,14 +3,46 @@
 # Copyright 2020, Intel Corporation
 
 #
-# run-all-on-SoftRoCE.sh - run all examples on SoftRoCE
+# run-all-on-SoftRoCE.sh - run all examples on SoftRoCE (optionally under valgrind)
 #
-# Usage: run-all-on-SoftRoCE.sh <binary-examples-directory> [IP_address] [port]
+# Usage: run-all-on-SoftRoCE.sh <binary-examples-directory> [IP_address] [port] [--valgrind]
 #
 
 BIN_DIR=$1
-IP_ADDRESS=$2
-PORT=$3
+IP_ADDRESS=
+PORT=
+if [ x"$2" == x"--valgrind" ]; then
+	VLD_TEST=$2
+elif [ x"$3" == x"--valgrind" ]; then
+	IP_ADDRESS=$2
+	VLD_TEST=$3
+elif [ x"$4" == x"--valgrind" ]; then
+	IP_ADDRESS=$2
+	PORT=$3
+	VLD_TEST=$4
+else
+	IP_ADDRESS=$2
+	PORT=$3
+fi
+
+if [ x"$VLD_TEST" == x"--valgrind" ]; then
+	VLD_CMD="valgrind --leak-check=yes"
+	VLD_SUPP_PATH=$(dirname $0)
+	VLD_SUPP="--suppressions=${VLD_SUPP_PATH}/memcheck-librdmacm.supp"
+	VLD_SUPP="${VLD_SUPP} --suppressions=${VLD_SUPP_PATH}/memcheck-libnl.supp"
+	# prepare the server command
+	VLD_SLOG_FILE="${BIN_DIR}/valgrind-server.log"
+	VLD_SLOG="--log-file=${VLD_SLOG_FILE}"
+	VLD_SCMD="${VLD_CMD} ${VLD_SUPP} ${VLD_SLOG}"
+	# prepare the client command
+	VLD_CLOG_FILE="${BIN_DIR}/valgrind-client.log"
+	VLD_CLOG="--log-file=${VLD_CLOG_FILE}"
+	VLD_CCMD="${VLD_CMD} ${VLD_SUPP} ${VLD_CLOG}"
+	VLD_MSG="(under Valgrind)"
+	echo "Running examples with Valgrind is tuned for Fedora 32 and rdma-core v33.0 debug build (EXPERIMENTAL)"
+	echo "It may fail for any other OS, OS version, rdma-core version and for the release build."
+	echo
+fi
 
 MODULE="rdma_rxe"
 STATE_OK="state ACTIVE physical_state LINK_UP"
@@ -37,10 +69,10 @@ function run_example() {
 	DIR=$1
 	EXAMPLE=$(basename $DIR)
 
-	echo "*** Running example: $EXAMPLE"
+	echo "*** Running example: $EXAMPLE $VLD_MSG"
 
 	echo "Starting the server ..."
-	$DIR/server $IP_ADDRESS $PORT &
+	$VLD_SCMD $DIR/server $IP_ADDRESS $PORT &
 	sleep 1
 
 	RV=0
@@ -48,26 +80,26 @@ function run_example() {
 	06-multiple-connections)
 		for SEED in 8 9 11 12; do
 			echo "Starting the client ..."
-			$DIR/client $IP_ADDRESS $PORT $SEED
+			$VLD_CCMD $DIR/client $IP_ADDRESS $PORT $SEED
 			RV=$?
 			[ $RV -ne 0 ] && break
 		done
 		;;
 	07-atomic-write)
 		echo "Starting the client ..."
-		$DIR/client $IP_ADDRESS $PORT "1st_word" "2nd_word" "3rd_word"
+		$VLD_CCMD $DIR/client $IP_ADDRESS $PORT "1st_word" "2nd_word" "3rd_word"
 		RV=$?
 		;;
 	08-messages-ping-pong)
 		SEED=7
 		ROUNDS=3
 		echo "Starting the client ..."
-		$DIR/client $IP_ADDRESS $PORT $SEED $ROUNDS
+		$VLD_CCMD $DIR/client $IP_ADDRESS $PORT $SEED $ROUNDS
 		RV=$?
 		;;
 	*)
 		echo "Starting the client ..."
-		$DIR/client $IP_ADDRESS $PORT
+		$VLD_CCMD $DIR/client $IP_ADDRESS $PORT
 		RV=$?
 		;;
 	esac
@@ -76,6 +108,14 @@ function run_example() {
 		echo Error: example $EXAMPLE FAILED!
 		N_FAILED=$(($N_FAILED + 1))
 		FAILED="$FAILED$EXAMPLE\n"
+	elif [ x"$VLD_CCMD" != x"" ]; then
+		cerrno=$(grep "ERROR SUMMARY:" ${VLD_CLOG_FILE} | grep -Eoh "[0-9]+ errors" | awk '{print $1}')
+		if [ $cerrno -gt 0 ]; then
+			echo "Error: example $EXAMPLE client $VLD_MSG FAILED!"
+			N_CFAILED=$(($N_CFAILED + 1))
+			CFAILED="${CFAILED}${EXAMPLE}-client\n"
+			mv ${VLD_CLOG_FILE} ${BIN_DIR}/$EXAMPLE-valgrind-client.log
+		fi
 	fi
 
 	# make sure the server's process is finished
@@ -87,6 +127,16 @@ function run_example() {
 		kill -9 $PID 2>/dev/null
 	fi
 
+	if [ x"$VLD_SCMD" != x"" ]; then
+		serrno=$(grep "ERROR SUMMARY:" ${VLD_SLOG_FILE} | grep -Eoh "[0-9]+ errors" | awk '{print $1}')
+		if [ $serrno -gt 0 ]; then
+			echo "Error: example $EXAMPLE server $VLD_MSG FAILED!"
+			N_SFAILED=$(($N_SFAILED + 1))
+			SFAILED="${SFAILED}${EXAMPLE}-server\n"
+			mv ${VLD_SLOG_FILE} ${BIN_DIR}/$EXAMPLE-valgrind-server.log
+		fi
+	fi
+
 	echo
 }
 
@@ -94,7 +144,8 @@ function run_example() {
 
 if [ "$BIN_DIR" == "" ]; then
 	echo "Error: missing required argument"
-	echo "Usage: run-all-on-SoftRoCE.sh <binary-examples-directory> [IP_address] [port]"
+	echo "Usage: run-all-on-SoftRoCE.sh <binary-examples-directory> [IP_address] [port] [--valgrind]"
+
 	exit 1
 fi
 
@@ -110,6 +161,11 @@ fi
 
 N_FAILED=0
 FAILED=""
+N_SFAILED=0
+SFAILED=""
+N_CFAILED=0
+CFAILED=""
+
 for srv in $(find $BIN_DIR -name server | sort); do
 	DIR=$(dirname $srv)
 	run_example $DIR
@@ -118,6 +174,22 @@ done
 if [ $N_FAILED -gt 0 ]; then
 	echo "$N_FAILED example(s) failed:"
 	echo -e "$FAILED"
+	err=1
+fi
+
+if [ $N_SFAILED -gt 0 ]; then
+	echo "$N_SFAILED example(s) server $VLD_MSG failed:"
+	echo -e "$SFAILED"
+	err=1
+fi
+
+if [ $N_CFAILED -gt 0 ]; then
+	echo "$N_CFAILED example(s) client $VLD_MSG failed:"
+	echo -e "$CFAILED"
+	err=1
+fi
+
+if [[ $err == 1 ]]; then
 	exit 1
 fi
 
