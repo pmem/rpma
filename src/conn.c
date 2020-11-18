ac15f541827c446ef161103515f0b212ade3b93b
Xiao Yang <yangx.jy@cn.fujitsu.com>
2020-11-18 13:35:05 +0800

    rpma: make rpma_flush() support visibility on remote pmem
    rpma_flush() needs to support RPMA_FLUSH_TYPE_VISIBILITY
    even if direct-pmem-write parameter is false on remote
    persistent memory.
    
    This change avoids the following error on example-05:
    --------------------------------------------
    server:
    ./server 192.168.122.189 8765 /dev/dax0.0
    ...
    
    client:
    ./client 192.168.122.189 8765 /dev/dax1.0
    ...
    RPMA_FLUSH_TYPE_PERSISTENT is NOT supported
    Nov 18 13:08:55.792994 [21682] *ERROR* conn.c: 380: \
    rpma_flush: The remote memory region does not support \
    flushing to global visibility
    ...
    --------------------------------------------
    
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>

--- src/conn.c
+++ src/conn.c
@@ -375,13 +375,6 @@ rpma_flush(struct rpma_conn *conn,
 		return RPMA_E_NOSUPP;
 	}
 
-	if (type == RPMA_FLUSH_TYPE_VISIBILITY &&
-	    0 == (flush_type & RPMA_MR_USAGE_FLUSH_TYPE_VISIBILITY)) {
-		RPMA_LOG_ERROR(
-			"The remote memory region does not support flushing to global visibility");
-		return RPMA_E_NOSUPP;
-	}
-
 	rpma_flush_func flush = conn->flush->func;
 	return flush(conn->id->qp, conn->flush, dst, dst_offset,
 			len, type, flags, op_context);
