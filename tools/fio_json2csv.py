e96eaee58dc4a7c8249bff7842d190f4647a396b
Jan M Michalski <jan.m.michalski@intel.com>
2020-12-01 10:57:35 +0000

    common: tools: RPMA read latency automat

---
+++ tools/fio_json2csv.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+#
+
+#
+# fio-json2csv.py -- generate a CSV from FIO JSON (EXPERIMENTAL)
+#
+
+import argparse
+import csv
+import json
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generate a CSV from FIO JSON (EXPERIMENTAL)')
+    parser.add_argument('json_file', metavar='JSON_FILE',
+        help='a FIO json file to process')
+    parser.add_argument('--op', metavar='OP',
+        choices=['read', 'write'], default='read',
+        help='a FIO operation to extract')
+    parser.add_argument('--output_file', metavar='CSV_FILE',
+        default='output.csv', help='an output file')
+    args = parser.parse_args()
+
+    with open(args.json_file) as json_file, \
+            open(args.output_file, 'w') as csv_file:
+        # read JSON file
+        data = json.load(json_file)
+        job = data['jobs'][0]
+        options = job['job options']
+        op = job[args.op]
+        lat_ns = op['lat_ns']
+        # prepare data for writing
+        csv_columns = ['bs']
+        csv_data = {'bs': options['bs']}
+        for k,v in lat_ns.items():
+            if k == "percentile" or k == "bins":
+                continue
+            column = 'lat_ns_' + k
+            csv_columns.append(column)
+            csv_data[column] = v
+        # prepare percentiles for writing
+        for k, v in lat_ns['percentile'].items():
+            column = 'lat_ns_pctl_' + k
+            csv_columns.append(column)
+            csv_data[column] = v
+        # write CSV file
+        writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
+        writer.writeheader()
+        writer.writerow(csv_data)
+
+if __name__ == "__main__":
+    main()
