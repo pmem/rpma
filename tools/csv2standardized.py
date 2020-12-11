ffc030f451c63dd81329f3e5b6e3ebf40c98ceb4
Lukasz Dorau <lukasz.dorau@intel.com>
2020-12-11 15:38:34 +0100

    tools: add new workload bw-dp for changing iodepth/tx-depth

--- tools/csv2standardized.py
+++ tools/csv2standardized.py
@@ -61,10 +61,10 @@ ib_lat_output_names = [
     'lat_pctl_99.9']
 
 ib_bw_input_names = [
-    'threads', 'bs', 'ops', 'bw_peak', 'bw_avg', 'msg_rate']
+    'threads', 'iodepth', 'bs', 'ops', 'bw_peak', 'bw_avg', 'msg_rate']
 
 ib_bw_output_names = [
-    'threads', 'bs', 'ops', 'bw_avg']
+    'threads', 'iodepth', 'bs', 'ops', 'bw_avg']
 
 def main():
     parser = argparse.ArgumentParser(
