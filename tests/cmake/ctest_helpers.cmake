e1bb04daf7e874207c14b0a088592f7ca9cc8cf2
Wang,Long <long1.wang@intel.com>
2020-12-18 17:46:10 +0800

    test: add librmpa API's multithreaded test

--- tests/cmake/ctest_helpers.cmake
+++ tests/cmake/ctest_helpers.cmake
@@ -216,3 +216,30 @@ function(add_test_generic)
 		endforeach()
 	endif()
 endfunction()
+
+function(add_multithreaded)
+	set(options USE_LIBIBVERBS)
+	set(oneValueArgs NAME BIN)
+	set(multiValueArgs SRCS)
+	cmake_parse_arguments(MULTITHREADED
+		"${options}"
+		"${oneValueArgs}"
+		"${multiValueArgs}"
+		${ARGN})
+
+	set(target multithreaded-${MULTITHREADED_NAME}-${MULTITHREADED_BIN})
+
+	prepend(srcs ${CMAKE_CURRENT_SOURCE_DIR} ${srcs})
+	add_executable(${target} ${MULTITHREADED_SRCS})
+	target_include_directories(${target} PRIVATE
+		${LIBRPMA_INCLUDE_DIRS})
+	set_target_properties(${target} PROPERTIES
+		OUTPUT_NAME ${MULTITHREADED_BIN})
+	target_link_libraries(${target} ${LIBRPMA_LIBRARIES} pthread)
+
+	if(MULTITHREADED_USE_LIBIBVERBS)
+		target_include_directories(${target}
+			PRIVATE ${LIBIBVERBS_INCLUDE_DIRS})
+		target_link_libraries(${target} ${LIBIBVERBS_LIBRARIES})
+	endif()
+endfunction()
