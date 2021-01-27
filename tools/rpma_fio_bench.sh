26a1415598419bb5f4ce6b40fa536551a80f801c
Jan M Michalski <jan.m.michalski@intel.com>
2021-01-27 16:16:17 +0000

    tools: rpma_fio_bench.sh remove redundant assignments

--- tools/rpma_fio_bench.sh
+++ tools/rpma_fio_bench.sh
@@ -127,7 +127,6 @@ function benchmark_one() {
 
 	case $PERSIST_MODE in
 	apm)
-		PERSIST_MODE="apm"
 		REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=1
 		if [ -z "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" ] || \
 		   [ $FORCE_REMOTE_DIRECT_WRITE_TO_PMEM -eq $REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM ]; then
@@ -140,7 +139,6 @@ function benchmark_one() {
 		fi
 		;;
 	gpspm)
-		PERSIST_MODE="gpspm"
 		REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=0
 		if [ -z "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" ] || \
 		   [ $FORCE_REMOTE_DIRECT_WRITE_TO_PMEM -eq $REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM ]; then
