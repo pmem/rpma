0e940c16e19fc40bffc5597dd77dc9467c1d27f4
Hao Li <lihao2018.fnst@cn.fujitsu.com>
2021-01-27 14:08:20 +0800

    examples: Add a multiple connection example using SRQ
    - This example is based on the example-06
    
    Signed-off-by: Hao Li <lihao2018.fnst@cn.fujitsu.com>

---
+++ examples/12-shared-receive-queue/shared-receive-queue.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * shared-receive-queue.h -- a common declarations
+ * for the 12 example
+ */
+
+#ifndef EXAMPLES_SHARED_RECEIVE_QUEUE_COMMON
+#define EXAMPLES_SHARED_RECEIVE_QUEUE_COMMON
+
+#define MAX_MSG_SIZE 32
+
+#endif /* EXAMPLES_SHARED_RECEIVE_QUEUE */
