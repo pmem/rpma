51056d2d0fd790826d71d11d51481a9570014282
Jan M Michalski <jan.m.michalski@intel.com>
2021-02-02 19:20:58 +0000

    tools: introduce two separate execution paths: report and appendices

--- tools/create_report.py
+++ tools/create_report.py
@@ -5,7 +5,8 @@
 #
 
 #
-# create_report.py -- generate a performance report (EXPERIMENTAL)
+# create_report.py -- generate the performance report and appendices
+#                     (EXPERIMENTAL)
 #
 
 import argparse
@@ -43,18 +44,7 @@ def is_a_file(parser, arg):
     else:
         return arg
 
-def main():
-    parser = argparse.ArgumentParser( \
-        formatter_class=argparse.RawDescriptionHelpFormatter, \
-        description='Generate a performance report (EXPERIMENTAL) \n\n{}'.format(PREREQUISITE))
-    parser.add_argument('--report_dir', metavar='DIR', required=True, \
-        help='a figures directory')
-    parser.add_argument('--release', required=True, help='e.g. 0.00')
-    parser.add_argument('--test_date', required=True, help='e.g. December 2020')
-    parser.add_argument('--high_level_setup_figure', required=True, \
-        help='e.g. Figure_0.png', type=lambda x: is_a_file(parser, x))
-    args = vars(parser.parse_args())
-
+def report(args):
     contents = {}
 
     # copy the high-level setup figure
@@ -87,6 +77,37 @@ def main():
     output_file = os.path.join(args['report_dir'], 'report.html')
     with open(output_file, 'w') as f:
         f.write(report)
+
+def appendices(args):
+    print("XXX")
+
+def main():
+    # common arguments
+    parser = argparse.ArgumentParser( \
+        formatter_class=argparse.RawDescriptionHelpFormatter, \
+        description=( \
+            'Generate the performance report and appendices (EXPERIMENTAL) ' \
+            f'\n\n{PREREQUISITE}'))
+    parser.add_argument('--report_dir', metavar='DIR', required=True, \
+        help='a figures directory')
+    parser.add_argument('--release', required=True, help='e.g. 0.00')
+
+    subparsers = parser.add_subparsers(dest='command')
+
+    # report-specifc arguments
+    parser_r = subparsers.add_parser('report', help='generate the report')
+    parser_r.add_argument('--test_date', required=True, help='e.g. December 2020')
+    parser_r.add_argument('--high_level_setup_figure', required=True, \
+        help='e.g. Figure_0.png', type=lambda x: is_a_file(parser, x))
+
+    # appendices-specifc arguments
+    parser_a = subparsers.add_parser('appendices', help='generate the appendices')
+
+    # parse the command line
+    args = vars(parser.parse_args())
+
+    # call a command-specific function by its name
+    globals()[args['command']](args)
 
 if __name__ == "__main__":
     main()
