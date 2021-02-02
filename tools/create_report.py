d2437a406289d93459a49171be6d2e5290e6fafb
Jan M Michalski <jan.m.michalski@intel.com>
2021-02-02 22:35:44 +0000

    tools: intrudce appendices generation
    Includes:
    - creating a general part of rendering a document
    - making header a document specific
    - implement the appendices structure for the generator
    - a simple all-in-one appendices content structure

--- tools/create_report.py
+++ tools/create_report.py
@@ -19,17 +19,32 @@ from jinja2 import Environment, FileSystemLoader, Template
 PREREQUISITE = 'Before using this script generate report figures using ./create_report_figures.sh'
 SEARCHPATH = 'templates'
 
-# all used SEARCHPATH/*.md files
-CONTENTS = [ \
+# all SEARCHPATH/*.md files used for the report
+REPORT_CONTENTS_MD = [ \
+    'report_header', \
     'audience', 'authors', 'bios', 'configuration_common', \
-    'configuration_target', 'header', 'report', 'security', 'introduction', \
+    'configuration_target', 'report', 'security', 'introduction', \
     'tc_read_lat_config', 'tc_read_bw_config', 'tc1_read', \
     'tc_write_lat_config', 'tc_write_bw_config', 'tc2_write', \
     'tc_mix_lat_config', 'tc_mix_bw_config', 'tc3_mix' ]
 
+# all SEARCHPATH/*.html files used for the report
+REPORT_CONTENTS_HTML = {
+    'report_menu'
+}
+
+# all SEARCHPATH/*.md files used for the appendices
+APPENDICES_CONTENTS_MD = [ \
+    'appendices_header', 'appendices' ]
+
+# all SEARCHPATH/*.html files used for the appendices
+APPENDICES_CONTENTS_HTML = {
+    'appendices_menu'
+}
+
 # all variables on a per-content basis
 CONTENTS_VARIABLES = { \
-    'header': [ 'release'], \
+    'report_header': [ 'release'], \
     'introduction': [ 'hl_ext'], \
     'tc1_read': [ 'tc_read_lat_config', 'tc_read_bw_config' ], \
     'tc2_write': [ 'tc_write_lat_config', 'tc_write_bw_config' ], \
@@ -37,12 +52,8 @@ CONTENTS_VARIABLES = { \
     'report': [ \
         'test_date', 'audience', 'authors', 'bios', \
         'configuration_common', 'configuration_target', 'security', \
-        'introduction', 'tc1_read', 'tc2_write', 'tc3_mix' ] \
-}
-
-# all used SEARCHPATH/*.html files
-CONTENTS_HTML = {
-    'report_menu'
+        'introduction', 'tc1_read', 'tc2_write', 'tc3_mix' ], \
+    'appendices_header': [ 'release'], \
 }
 
 def is_a_file(parser, arg):
@@ -50,6 +61,43 @@ def is_a_file(parser, arg):
         parser.error(f"The file {arg} does not exist!")
     else:
         return arg
+
+def populate_contents(contents, parts_html, parts_md, args):
+    # load the HTML content parts
+    env = Environment(loader=FileSystemLoader(SEARCHPATH))
+    for name in parts_html:
+        tmpl = env.get_template(f'{name}.html')
+        contents[name] = tmpl.render()
+
+    # convert content parts from markdown to HTML
+    for name in parts_md:
+        if not os.path.isfile(f'{SEARCHPATH}/{name}.md'):
+            contents[name] = f'<i>no {SEARCHPATH}/{name}.md</i><br/>'
+            continue
+        contents[name] = markdown2.markdown_path(f'{SEARCHPATH}/{name}.md', \
+                extras=['tables'])
+
+    # replace the variables with appropriate values
+    for name, var_names in CONTENTS_VARIABLES.items():
+        if name not in contents.keys():
+            continue
+        tmpl = Template(contents[name])
+        # get variable values from either args or contents
+        tmpl_vars = {k: args.get(k, contents.get(k, 'no data')) for k in var_names}
+        contents[name] = tmpl.render(tmpl_vars)
+
+    return contents
+
+def render(contents, args, output):
+    # render the report with the complete layout
+    env = Environment(loader=FileSystemLoader(SEARCHPATH))
+    layout_tmpl = env.get_template('layout.html')
+    report = layout_tmpl.render(contents)
+
+    # prepare output file and write the result
+    output_file = os.path.join(args['report_dir'], f'{output}.html')
+    with open(output_file, 'w') as f:
+        f.write(report)
 
 def report(args):
     contents = {}
@@ -60,41 +108,26 @@ def report(args):
     shutil.copyfile(args['high_level_setup_figure'], hl_figure)
     contents['hl_ext'] = ext
 
-    # load the HTML content parts
-    env = Environment(loader=FileSystemLoader(SEARCHPATH))
-    for name in CONTENTS_HTML:
-        tmpl = env.get_template(f'{name}.html')
-        contents[name] = tmpl.render()
+    contents = populate_contents(contents, REPORT_CONTENTS_HTML, \
+        REPORT_CONTENTS_MD, args)
 
-    # convert content parts from markdown to HTML
-    for name in CONTENTS:
-        if not os.path.isfile(f'{SEARCHPATH}/{name}.md'):
-            contents[name] = f'<i>no {SEARCHPATH}/{name}.md</i><br/>'
-            continue
-        contents[name] = markdown2.markdown_path(f'{SEARCHPATH}/{name}.md', \
-                extras=['tables'])
-
-    # replace the variables with appropriate values
-    for name, var_names in CONTENTS_VARIABLES.items():
-        tmpl = Template(contents[name])
-        # get variable values from either args or contents
-        tmpl_vars = {k: args.get(k, contents.get(k, 'no data')) for k in var_names}
-        contents[name] = tmpl.render(tmpl_vars)
-
-    # attach the correct menu for the report
+    # attach the correct header, menu and content for the report
+    contents['header'] = contents['report_header']
     contents['menu'] = contents['report_menu']
+    contents['content'] = contents['report']
 
-    # render the report with the complete layout
-    layout_tmpl = env.get_template('layout.html')
-    report = layout_tmpl.render(contents)
-
-    # prepare output file and write the result
-    output_file = os.path.join(args['report_dir'], 'report.html')
-    with open(output_file, 'w') as f:
-        f.write(report)
+    render(contents, args, 'report')
 
 def appendices(args):
-    print("XXX")
+    contents = populate_contents({}, APPENDICES_CONTENTS_HTML, \
+        APPENDICES_CONTENTS_MD, args)
+
+    # attach the correct header, menu and content for the appendices
+    contents['header'] = contents['appendices_header']
+    contents['menu'] = contents['appendices_menu']
+    contents['content'] = contents['appendices']
+
+    render(contents, args, 'appendices')
 
 def main():
     # common arguments
