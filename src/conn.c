df03762d90b890ca4e65e5070445bf5fd5e2bbd4
osalyk <oksana.salyk@intel.com>
2021-02-12 06:58:23 +0100

    rpma: rpma_read 0B message

--- src/conn.c
+++ src/conn.c
@@ -301,7 +301,10 @@ rpma_read(struct rpma_conn *conn,
 	const struct rpma_mr_remote *src,  size_t src_offset,
 	size_t len, int flags, const void *op_context)
 {
-	if (conn == NULL || dst == NULL || src == NULL || flags == 0)
+	if (conn == NULL || flags == 0 ||
+	    ((src == NULL || dst == NULL) &&
+	    (src != NULL || dst != NULL || dst_offset != 0 || src_offset != 0 ||
+	    len != 0)))
 		return RPMA_E_INVAL;
 
 	return rpma_mr_read(conn->id->qp,
