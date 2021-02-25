e9ccbb031b9de1f401c17fed906235e24c10ea30
Wang, Long <long1.wang@intel.com>
2021-02-25 18:01:25 +0800

    test: add rpma_conn_req_get_private_data() unit test

--- tests/unit/conn_req/conn_req-connect.c
+++ tests/unit/conn_req/conn_req-connect.c
@@ -400,6 +400,50 @@ connect_via_accept__success_incoming(void **unused)
 }
 
 /*
+ * connect_via_accept_wi_pdata__success_incoming -- rpma_conn_req_connect()
+ * success (using an incoming connection request w/i private data)
+ */
+static void
+connect_via_accept_wi_pdata__success_incoming(void **unused)
+{
+	/* WA for cmocka/issues#47 */
+	struct conn_req_test_state *cstate = NULL;
+	assert_int_equal(setup__conn_req_from_cm_event((void **)&cstate), 0);
+	assert_non_null(cstate);
+
+	/* configure mocks */
+	expect_value(rdma_accept, id, &cstate->id);
+	will_return(rdma_accept, MOCK_OK);
+	expect_value(rdma_ack_cm_event, event, &cstate->event);
+	will_return(rdma_ack_cm_event, MOCK_OK);
+	expect_value(rpma_conn_new, id, &cstate->id);
+	will_return(rpma_conn_new, MOCK_CONN);
+	expect_value(rpma_conn_set_private_data, conn, MOCK_CONN);
+	expect_value(rpma_conn_set_private_data, pdata->ptr, MOCK_PRIVATE_DATA);
+	expect_value(rpma_conn_set_private_data, pdata->len, MOCK_PDATA_LEN);
+	will_return(rpma_conn_set_private_data, 0);
+	expect_function_call(rpma_private_data_discard);
+
+	/* get private data */
+	struct rpma_conn_private_data data;
+	int ret = rpma_conn_req_get_private_data(cstate->req, &data);
+
+	/* verify the results */
+	assert_int_equal(ret, MOCK_OK);
+	assert_ptr_equal(data.ptr, MOCK_PRIVATE_DATA);
+	assert_int_equal(data.len, MOCK_PDATA_LEN);
+
+	/* run test */
+	struct rpma_conn *conn = NULL;
+	ret = rpma_conn_req_connect(&cstate->req, NULL, &conn);
+
+	/* verify the results */
+	assert_int_equal(ret, MOCK_OK);
+	assert_null(cstate->req);
+	assert_int_equal(conn, MOCK_CONN);
+}
+
+/*
  * connect_via_connect__connect_EAGAIN -- rdma_connect() fails with EAGAIN
  */
 static void
@@ -597,6 +641,7 @@ static const struct CMUnitTest test_connect[] = {
 		connect_via_accept__conn_new_EAGAIN_subsequent_EIO),
 	cmocka_unit_test(connect_via_accept__set_private_data_ENOMEM),
 	cmocka_unit_test(connect_via_accept__success_incoming),
+	cmocka_unit_test(connect_via_accept_wi_pdata__success_incoming),
 	/* connect via rdma_connect() */
 	cmocka_unit_test(connect_via_connect__connect_EAGAIN),
 	cmocka_unit_test(
