9ba5c3807e4ee239afde2dde284254da58a415b6
osalyk <oksana.salyk@intel.com>
2021-04-01 18:47:40 +0200

    tools: separate the common part of the scripts for benchmarking

--- tools/perf/ib_read.sh
+++ tools/perf/ib_read.sh
@@ -12,6 +12,8 @@
 # the resutls.
 #
 
+source ./bench_common.sh
+
 HEADER_LAT="#bytes #iterations    t_min[usec]    t_max[usec]  t_typical[usec]    t_avg[usec]    t_stdev[usec]   99% percentile[usec]   99.9% percentile[usec]"
 HEADER_BW="#threads #bytes     #iterations    BW_peak[Gb/sec]    BW_average[Gb/sec]   MsgRate[Mpps]"
 
@@ -19,76 +21,17 @@ TIMESTAMP=$(date +%y-%m-%d-%H%M%S)
 
 echo "This tool is EXPERIMENTAL"
 
-function usage()
-{
-	echo "Error: $1"
-	echo
-	echo "usage: $0 <server_ip> <all|bw-bs|bw-dp-exp|bw-dp-lin|bw-th|lat>"
-	echo
-	echo "export JOB_NUMA=0"
-	echo "export AUX_PARAMS='-d mlx5_0 -R'"
-	echo "export IB_PATH=/custom/ib tool/path/"
-	echo
-	echo "export REMOTE_USER=user"
-	echo "export REMOTE_PASS=pass"
-	echo "export REMOTE_JOB_NUMA=0"
-	echo "export REMOTE_AUX_PARAMS='-d mlx5_0 -R'"
-	echo "export REMOTE_IB_PATH=/custom/ib tool/path/"
-	echo "export REMOTE_SUDO_NOPASSWD=0/1"
-	echo "export REMOTE_RNIC_PCIE_ROOT_PORT=<pcie_root_port>"
-	echo "export REMOTE_DIRECT_WRITE_TO_PMEM=0/1 (https://pmem.io/rpma/documentation/basic-direct-write-to-pmem.html)"
-	echo "export FORCE_REMOTE_DIRECT_WRITE_TO_PMEM=0/1 (forces setting REMOTE_DIRECT_WRITE_TO_PMEM to this value)"
-	echo
-	echo "export REMOTE_ANOTHER_NUMA=1"
-	echo "export REMOTE_RESULTS_DIR=/tmp/"
-	echo "export EVENT_LIST=/path/to/edp/events/list"
-	echo "export REMOTE_CMD_PRE='source /opt/intel/sep/sep_vars.sh; numactl -N \${REMOTE_ANOTHER_NUMA} emon -i \${EVENT_LIST} > \${REMOTE_RESULTS_DIR}\${RUN_NAME}_emon.dat'"
-	echo "export REMOTE_CMD_POST='sleep 10; source /opt/intel/sep/sep_vars.sh; emon -stop'"
-	echo
-	echo "Note:"
-	echo "The 'REMOTE_CMD_PRE' and 'REMOTE_CMD_POST' environment variables"
-	echo "can use the 'RUN_NAME' environment variable internally,"
-	echo "which contains a unique name of each run."
-	echo
-	echo "Debug:"
-	echo "export SHORT_RUNTIME=1 (adequate for functional verification only)"
-	echo "export DO_NOTHING=1 (create empty output files; do not run the actual execution)"
-	echo "export DUMP_CMDS=1 (dump all commands that would be executed; do not run the actual execution)"
-	exit 1
-}
+if [ "$1" == "--env" ]; then
+	show_environment
+	exit 0
+fi
 
 if [ "$#" -lt 2 ]; then
-	usage "Too few arguments"
-elif [ -z "$JOB_NUMA" ]; then
-	usage "JOB_NUMA not set"
-elif [ -z "$REMOTE_USER" ]; then
-	usage "REMOTE_USER not set"
-elif [ -z "$REMOTE_PASS" ]; then
-	usage "REMOTE_PASS not set"
-elif [ -z "$REMOTE_JOB_NUMA" ]; then
-	usage "REMOTE_JOB_NUMA not set"
-elif [ -z "$REMOTE_RNIC_PCIE_ROOT_PORT" -a "$REMOTE_SUDO_NOPASSWD" == "1" ]; then
-	usage "REMOTE_RNIC_PCIE_ROOT_PORT not set"
-elif [ -z "$REMOTE_DIRECT_WRITE_TO_PMEM" -a "$REMOTE_SUDO_NOPASSWD" != "1" ]; then
-	usage "REMOTE_DIRECT_WRITE_TO_PMEM not set"
+	ib_usage "Too few arguments"
+	exit 1
 fi
 
-if [ "$REMOTE_SUDO_NOPASSWD" != "1" ]; then
-	echo "WARNING: sudo (called on the remote side) will prompt for password!"
-	echo "         Toggling DDIO will be skipped!"
-	echo
-	echo "         In order to change it:"
-	echo "           1) set permissions of sudo to NOPASSWD in '/etc/sudoers' and"
-	echo "           2) set REMOTE_SUDO_NOPASSWD=1"
-	echo
-	if [ -n "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" -a "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" != "$REMOTE_DIRECT_WRITE_TO_PMEM" ]; then
-		echo "Error: FORCE_REMOTE_DIRECT_WRITE_TO_PMEM != REMOTE_DIRECT_WRITE_TO_PMEM ($FORCE_REMOTE_DIRECT_WRITE_TO_PMEM != $REMOTE_DIRECT_WRITE_TO_PMEM),"
-		echo "       and REMOTE_SUDO_NOPASSWD does not equal 1."
-		echo "       Change sudo permissions in order to force setting REMOTE_DIRECT_WRITE_TO_PMEM."
-		echo "Exiting..."
-		exit 1
-	fi
-fi
+check_parameters
 
 function verify_block_size()
 {
@@ -119,16 +62,7 @@ function benchmark_one() {
 	SERVER_IP=$1
 	MODE=$2
 
-	REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=1
-	if [ -z "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" ] || \
-	   [ $FORCE_REMOTE_DIRECT_WRITE_TO_PMEM -eq $REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM ]; then
-		DDIO_MODE="disable"
-		DDIO_QUERY=0
-	else
-		DDIO_MODE="enable"
-		DDIO_QUERY=1
-		REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM
-	fi
+	ddio_off
 
 	case $MODE in
 	bw-bs)
@@ -198,7 +132,8 @@ function benchmark_one() {
 		verify_block_size
 		;;
 	*)
-		usage "Wrong mode: $MODE"
+		ib_usage "Wrong mode: $MODE"
+		exit 1
 		;;
 	esac
 
@@ -217,32 +152,7 @@ function benchmark_one() {
 	fi
 	echo
 
-	if [ "$REMOTE_SUDO_NOPASSWD" == "1" ]; then
-		if [ "$DO_RUN" == "1" ]; then
-			# copy the ddio.sh script to the server
-			sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no \
-				../ddio.sh $REMOTE_USER@$SERVER_IP:$DIR 2>>$LOG_ERR
-			# set DDIO on the server
-			sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-				$REMOTE_USER@$SERVER_IP \
-				"sudo $DIR/ddio.sh -d $REMOTE_RNIC_PCIE_ROOT_PORT -s $DDIO_MODE \
-				> $LOG_ERR 2>&1" 2>>$LOG_ERR
-			# query DDIO on the server
-			sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-				$REMOTE_USER@$SERVER_IP \
-				"sudo $DIR/ddio.sh -d $REMOTE_RNIC_PCIE_ROOT_PORT -q \
-				> $LOG_ERR 2>&1" 2>>$LOG_ERR
-			if [ $? -ne $DDIO_QUERY ]; then
-				echo "Error: setting DDIO to '$DDIO_MODE' failed"
-				exit 1
-			fi
-		fi
-		REMOTE_DIRECT_WRITE_TO_PMEM=$REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM
-	elif [ $REMOTE_DIRECT_WRITE_TO_PMEM -ne $REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM ]; then
-		echo "Error: REMOTE_DIRECT_WRITE_TO_PMEM does not have the required value ($REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM)"
-		echo "Skipping..."
-		return
-	fi
+	set_ddio
 
 	if [ "$DUMP_CMDS" == "1" ]; then
 		echo "REMOTE_DIRECT_WRITE_TO_PMEM=$REMOTE_DIRECT_WRITE_TO_PMEM" >> $SERVER_DUMP
@@ -312,11 +222,7 @@ function benchmark_one() {
 			IT_OPT="--iters $IT"
 		fi
 
-		export RUN_NAME=${NAME}_${ITER}
-		echo "Name of this run: ${RUN_NAME}"
-
-		REMOTE_CMD_PRE_SUBST=$(echo "$REMOTE_CMD_PRE" | envsubst)
-		REMOTE_CMD_POST_SUBST=$(echo "$REMOTE_CMD_POST" | envsubst)
+		prepare_variables
 
 		# run the server
 		CMD="numactl -N $REMOTE_JOB_NUMA ${IB_PATH}${IB_TOOL} $BS_OPT $QP_OPT \
@@ -324,9 +230,7 @@ function benchmark_one() {
 
 		if [ "$DO_RUN" == "1" ]; then
 			if [ "x$REMOTE_CMD_PRE_SUBST" != "x" ]; then
-				echo "$REMOTE_CMD_PRE_SUBST"
-				sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-					$REMOTE_USER@$SERVER_IP "$REMOTE_CMD_PRE_SUBST" 2>>$LOG_ERR &
+				collect_statistics --start
 			fi
 
 			sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
@@ -345,9 +249,7 @@ function benchmark_one() {
 				| sed 's/[ ]*$//' | sed -r 's/[[:blank:]]+/,/g' >> $OUTPUT
 
 			if [ "x$REMOTE_CMD_POST_SUBST" != "x" ]; then
-				echo "$REMOTE_CMD_POST_SUBST"
-				sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-					$REMOTE_USER@$SERVER_IP "$REMOTE_CMD_POST_SUBST" 2>>$LOG_ERR
+				collect_statistics --stop
 			fi
 
 		elif [ "$DUMP_CMDS" == "1" ]; then
@@ -382,7 +284,8 @@ all)
 	MODES="bw-bs bw-dp-exp bw-dp-lin bw-th lat"
 	;;
 *)
-	usage "Wrong mode: $MODES"
+	ib_usage "Wrong mode: $MODES"
+	exit 1
 	;;
 esac
 
--- tools/perf/rpma_fio_bench.sh
+++ tools/perf/rpma_fio_bench.sh
@@ -11,127 +11,35 @@
 # sizes (1KiB, 4KiB, 64KiB) and generates a single CSV file with all results.
 #
 
+source ./bench_common.sh
+
 TIMESTAMP=$(date +%y-%m-%d-%H%M%S)
-
-function usage()
-{
-	echo "Error: $1"
-	echo
-	echo "Usage: $0 <server_ip> all|apm|gpspm|aof_sw|aof_hw [all|read|randread|write|randwrite|rw|randrw] [all|bw-bs|bw-dp-exp|bw-dp-lin|bw-th|bw-cpu|bw-cpu-mt|lat|lat-cpu]"
-	echo "       $0 --env - show environment variables used by the script"
-	echo
-	echo "Notes:"
-	echo " - 'all' is the default value for missing arguments"
-	echo " - the 'gpspm' mode does not support the 'read' operation for now."
-	echo " - the 'aof_*' modes do not support the 'read', 'randread', 'randwrite', 'rw' and 'randrw' operations."
-	echo
-	echo "export JOB_NUMA=0"
-	echo "export FIO_PATH=/custom/fio/path/"
-	echo "export COMMENT=any_text_to_be_added_to_every_file_name"
-	echo
-	echo "export REMOTE_USER=user"
-	echo "export REMOTE_PASS=pass"
-	echo "export REMOTE_JOB_NUMA=0"
-	echo "export REMOTE_SUDO_NOPASSWD=0/1"
-	echo "export REMOTE_RNIC_PCIE_ROOT_PORT=<pcie_root_port>"
-	echo "export REMOTE_DIRECT_WRITE_TO_PMEM=0/1 (https://pmem.io/rpma/documentation/basic-direct-write-to-pmem.html)"
-	echo "export FORCE_REMOTE_DIRECT_WRITE_TO_PMEM=0/1 (forces setting REMOTE_DIRECT_WRITE_TO_PMEM to this value)"
-	echo "export REMOTE_FIO_PATH=/custom/fio/path/"
-	echo "export REMOTE_JOB_PATH=/custom/jobs/path"
-	echo "export REMOTE_JOB_MEM_PATH=/path/to/mem"
-	echo "export BUSY_WAIT_POLLING=0/1"
-	echo
-	echo "export REMOTE_ANOTHER_NUMA=1"
-	echo "export REMOTE_RESULTS_DIR=/tmp/"
-	echo
-	echo "In case you would like to collect sar data:"
-	echo "export REMOTE_CMD_PRE='rm -f \${REMOTE_RESULTS_DIR}sar.dat; numactl -N \${REMOTE_ANOTHER_NUMA} sar -u -P \${REMOTE_JOB_NUMA_CPULIST} -o \${REMOTE_RESULTS_DIR}sar.dat 5 > /dev/null'"
-	echo "export REMOTE_CMD_POST='sleep 10; killall -9 sar; sadf -d -- -u -P \${REMOTE_JOB_NUMA_CPULIST} \${REMOTE_RESULTS_DIR}sar.dat > \${REMOTE_RESULTS_DIR}sar_\${RUN_NAME}.csv'"
-	echo
-	echo "In case you would like to collect emon data:"
-	echo "export EVENT_LIST=/path/to/edp/events/list"
-	echo "export REMOTE_CMD_PRE='source /opt/intel/sep/sep_vars.sh; numactl -N \${REMOTE_ANOTHER_NUMA} emon -i \${EVENT_LIST} > \${REMOTE_RESULTS_DIR}\${RUN_NAME}_emon.dat'"
-	echo "export REMOTE_CMD_POST='sleep 10; source /opt/intel/sep/sep_vars.sh; emon -stop'"
-	echo
-	echo "Note:"
-	echo "The 'REMOTE_CMD_PRE' and 'REMOTE_CMD_POST' environment variables"
-	echo "can use the 'RUN_NAME' environment variable internally,"
-	echo "which contains a unique name of each run."
-	echo
-	echo "Debug:"
-	echo "export SHORT_RUNTIME=0 (adequate for functional verification only)"
-	echo "export TRACER='gdbserver localhost:2345'"
-	echo "export REMOTE_TRACER='gdbserver localhost:2345'"
-	echo "export DO_NOTHING=1 (create empty output files; do not run the actual execution)"
-	echo "export DUMP_CMDS=1 (dump all commands that would be executed; do not run the actual execution)"
-	echo
-	exit 1
-}
-
-function show_environment() {
-	echo
-	echo "Environment variables used by the script:"
-	echo
-	echo "export JOB_NUMA=$JOB_NUMA"
-	echo "export FIO_PATH=$FIO_PATH"
-	echo "export COMMENT=$COMMENT"
-	echo
-	echo "export REMOTE_USER=$REMOTE_USER"
-	echo "export REMOTE_PASS=$REMOTE_PASS"
-	echo "export REMOTE_JOB_NUMA=$REMOTE_JOB_NUMA"
-	echo "export REMOTE_SUDO_NOPASSWD=$REMOTE_SUDO_NOPASSWD"
-	echo "export REMOTE_RNIC_PCIE_ROOT_PORT=$REMOTE_RNIC_PCIE_ROOT_PORT"
-	echo "export REMOTE_DIRECT_WRITE_TO_PMEM=$REMOTE_DIRECT_WRITE_TO_PMEM"
-	echo "export FORCE_REMOTE_DIRECT_WRITE_TO_PMEM=$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM"
-	echo "export REMOTE_FIO_PATH=$REMOTE_FIO_PATH"
-	echo "export REMOTE_JOB_PATH=$REMOTE_JOB_PATH"
-	echo "export REMOTE_JOB_MEM_PATH=$REMOTE_JOB_MEM_PATH"
-	echo "export BUSY_WAIT_POLLING=$BUSY_WAIT_POLLING"
-	echo
-	echo "export REMOTE_ANOTHER_NUMA=$REMOTE_ANOTHER_NUMA"
-	echo "export REMOTE_CMD_PRE='$REMOTE_CMD_PRE'"
-	echo "export REMOTE_CMD_POST='$REMOTE_CMD_POST'"
-	echo
-	echo "Debug:"
-	echo "export SHORT_RUNTIME=$SHORT_RUNTIME"
-	echo "export TRACER=$TRACER"
-	echo "export REMOTE_TRACER=$REMOTE_TRACER"
-	echo "export DO_NOTHING=$DO_NOTHING"
-	echo "export DUMP_CMDS=$DUMP_CMDS"
-	exit 0
-}
 
 if [ "$1" == "--env" ]; then
 	show_environment
+	exit 0
 fi
 
 if [ "$#" -lt 2 ] || [ "$#" -eq 2 -a "$2" != "all" ]; then
-	usage "Too few arguments"
-elif [ -z "$JOB_NUMA" ]; then
-	usage "JOB_NUMA not set"
-elif [ -z "$REMOTE_USER" ]; then
-	usage "REMOTE_USER not set"
-elif [ -z "$REMOTE_PASS" ]; then
-	usage "REMOTE_PASS not set"
-elif [ -z "$REMOTE_JOB_NUMA" ]; then
-	usage "REMOTE_JOB_NUMA not set"
-elif [ -z "$REMOTE_RNIC_PCIE_ROOT_PORT" -a "$REMOTE_SUDO_NOPASSWD" == "1" ]; then
-	usage "REMOTE_RNIC_PCIE_ROOT_PORT not set"
-elif [ -z "$REMOTE_DIRECT_WRITE_TO_PMEM" -a "$REMOTE_SUDO_NOPASSWD" != "1" ]; then
-	usage "REMOTE_DIRECT_WRITE_TO_PMEM not set"
+	rpma_usage "Too few arguments"
+	exit 1
 elif [ "$2" == "gpspm" ]; then
 	case "$3" in
 	read|randread|rw|randrw)
-		usage "The 'gpspm' mode does not support the '$3' operation for now."
+		rpma_usage "The 'gpspm' mode does not support the '$3' operation for now."
+		exit 1
 		;;
 	esac
 elif [ "$2" == "aof_sw" -o "$2" == "aof_hw" ]; then
 	case "$3" in
 	read|randread|randwrite|rw|randrw)
-		usage "The '$2' mode does not support the '$3' operation."
+		rpma_usage "The '$2' mode does not support the '$3' operation."
+		exit 1
 		;;
 	esac
 fi
+
+check_parameters
 
 if [ -z "$BUSY_WAIT_POLLING" ]; then
 	BUSY_WAIT_POLLING=1
@@ -141,23 +49,6 @@ if [ "$BUSY_WAIT_POLLING" == "1" ]; then
 	POLLING="busy-wait"
 else
 	POLLING="no-busy-wait"
-fi
-
-if [ "$REMOTE_SUDO_NOPASSWD" != "1" ]; then
-	echo "WARNING: sudo (called on the remote side) will prompt for password!"
-	echo "         Toggling DDIO will be skipped!"
-	echo
-	echo "         In order to change it:"
-	echo "           1) set permissions of sudo to NOPASSWD in '/etc/sudoers' and"
-	echo "           2) set REMOTE_SUDO_NOPASSWD=1"
-	echo
-	if [ -n "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" -a "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" != "$REMOTE_DIRECT_WRITE_TO_PMEM" ]; then
-		echo "Error: FORCE_REMOTE_DIRECT_WRITE_TO_PMEM != REMOTE_DIRECT_WRITE_TO_PMEM ($FORCE_REMOTE_DIRECT_WRITE_TO_PMEM != $REMOTE_DIRECT_WRITE_TO_PMEM),"
-		echo "       and REMOTE_SUDO_NOPASSWD does not equal 1."
-		echo "       Change sudo permissions in order to force setting REMOTE_DIRECT_WRITE_TO_PMEM."
-		echo "Exiting..."
-		exit 1
-	fi
 fi
 
 function benchmark_one() {
@@ -172,28 +63,10 @@ function benchmark_one() {
 
 	case $PERSIST_MODE in
 	apm|aof_hw)
-		REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=1
-		if [ -z "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" ] || \
-		   [ $FORCE_REMOTE_DIRECT_WRITE_TO_PMEM -eq $REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM ]; then
-			DDIO_MODE="disable"
-			DDIO_QUERY=0
-		else
-			DDIO_MODE="enable"
-			DDIO_QUERY=1
-			REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM
-		fi
+		ddio_off
 		;;
 	gpspm|aof_sw)
-		REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=0
-		if [ -z "$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM" ] || \
-		   [ $FORCE_REMOTE_DIRECT_WRITE_TO_PMEM -eq $REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM ]; then
-			DDIO_MODE="enable"
-			DDIO_QUERY=1
-		else
-			DDIO_MODE="disable"
-			DDIO_QUERY=0
-			REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM=$FORCE_REMOTE_DIRECT_WRITE_TO_PMEM
-		fi
+		ddio_on
 		;;
 	esac
 
@@ -328,32 +201,7 @@ function benchmark_one() {
 		fi
 	fi
 
-	if [ "$REMOTE_SUDO_NOPASSWD" == "1" ]; then
-		if [ "$DO_RUN" == "1" ]; then
-			# copy the ddio.sh script to the server
-			sshpass -p "$REMOTE_PASS" scp -o StrictHostKeyChecking=no \
-				../ddio.sh $REMOTE_USER@$SERVER_IP:$DIR 2>>$LOG_ERR
-			# set DDIO on the server
-			sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-				$REMOTE_USER@$SERVER_IP \
-				"sudo $DIR/ddio.sh -d $REMOTE_RNIC_PCIE_ROOT_PORT -s $DDIO_MODE \
-				> $LOG_ERR 2>&1" 2>>$LOG_ERR
-			# query DDIO on the server
-			sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-				$REMOTE_USER@$SERVER_IP \
-				"sudo $DIR/ddio.sh -d $REMOTE_RNIC_PCIE_ROOT_PORT -q \
-				> $LOG_ERR 2>&1" 2>>$LOG_ERR
-			if [ $? -ne $DDIO_QUERY ]; then
-				echo "Error: setting DDIO to '$DDIO_MODE' failed"
-				exit 1
-			fi
-		fi
-		REMOTE_DIRECT_WRITE_TO_PMEM=$REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM
-	elif [ $REMOTE_DIRECT_WRITE_TO_PMEM -ne $REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM ]; then
-		echo "Error: REMOTE_DIRECT_WRITE_TO_PMEM does not have the required value ($REQUIRED_REMOTE_DIRECT_WRITE_TO_PMEM)"
-		echo "Skipping..."
-		return
-	fi
+	set_ddio
 
 	if [ "$DUMP_CMDS" == "1" ]; then
 		echo "REMOTE_DIRECT_WRITE_TO_PMEM=$REMOTE_DIRECT_WRITE_TO_PMEM" >> $SERVER_DUMP
@@ -412,11 +260,7 @@ function benchmark_one() {
 			FILTER="" # no section filtering so CPU load will be included
 		fi
 
-		export RUN_NAME=${NAME}_${ITER}
-		echo "Name of this run: ${RUN_NAME}"
-
-		REMOTE_CMD_PRE_SUBST=$(echo "$REMOTE_CMD_PRE" | envsubst)
-		REMOTE_CMD_POST_SUBST=$(echo "$REMOTE_CMD_POST" | envsubst)
+		prepare_variables
 
 		ENV="serverip=$SERVER_IP numjobs=${TH} iodepth=${DP} \
 			filename=${REMOTE_JOB_DEST} \
@@ -425,9 +269,7 @@ function benchmark_one() {
 			cores_per_socket=${CORES_PER_SOCKET}"
 		if [ "$DO_RUN" == "1" ]; then
 			if [ "x$REMOTE_CMD_PRE_SUBST" != "x" ]; then
-				echo "$REMOTE_CMD_PRE_SUBST"
-				sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-					$REMOTE_USER@$SERVER_IP "$REMOTE_CMD_PRE_SUBST" 2>>$LOG_ERR &
+				collect_statistics --start
 			fi
 
 			# copy config to the server
@@ -462,9 +304,7 @@ function benchmark_one() {
 			fi
 
 			if [ "x$REMOTE_CMD_POST_SUBST" != "x" ]; then
-				echo "$REMOTE_CMD_POST_SUBST"
-				sshpass -p "$REMOTE_PASS" -v ssh -o StrictHostKeyChecking=no \
-					$REMOTE_USER@$SERVER_IP "$REMOTE_CMD_POST_SUBST" 2>>$LOG_ERR
+				collect_statistics --stop
 			fi
 
 			for i in $INDS; do
@@ -534,7 +374,8 @@ all)
 	P_MODES="apm gpspm aof_sw aof_hw"
 	;;
 *)
-	usage "Wrong persistency mode: $P_MODES"
+	rpma_usage "Wrong persistency mode: $P_MODES"
+	exit 1
 	;;
 esac
 
@@ -545,7 +386,8 @@ all)
 	OPS="read randread write randwrite rw randrw"
 	;;
 *)
-	usage "Wrong operation: $OPS"
+	rpma_usage "Wrong operation: $OPS"
+	exit 1
 	;;
 esac
 
@@ -556,7 +398,8 @@ all)
 	MODES="lat lat-cpu bw-bs bw-cpu bw-cpu-mt bw-th bw-dp-lin bw-dp-exp"
 	;;
 *)
-	usage "Wrong mode: $MODES"
+	rpma_usage "Wrong mode: $MODES"
+	exit 1
 	;;
 esac
 
