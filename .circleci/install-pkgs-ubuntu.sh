4bd870bef33b67cf0825b2e93dab1c6c05a23fdc
Lukasz Dorau <lukasz.dorau@intel.com>
2021-04-09 12:00:19 +0200

    common: CircleCI: install packages with debug symbols

--- .circleci/install-pkgs-ubuntu.sh
+++ .circleci/install-pkgs-ubuntu.sh
@@ -31,13 +31,21 @@ RPMA_DEPS="\
 	groff \
 	graphviz \
 	libibverbs-dev \
+	libibverbs1-dbgsym \
 	librdmacm-dev \
+	librdmacm1-dbgsym \
 	libunwind-dev \
 	linux-modules-extra-$(uname -r) \
 	pandoc"
 
-# Install all required packages
+# Update existing packages
 sudo apt-get update
+
+# Enable repositories with debug symbols packages (-dbgsym)
+sudo apt-get install ubuntu-dbgsym-keyring
+sudo cp .circleci/ddebs.list /etc/apt/sources.list.d/ddebs.list
+sudo apt-get update
+
 sudo apt-get install --assume-yes --no-install-recommends \
 	$BASE_DEPS \
 	$EXAMPLES_DEPS \
