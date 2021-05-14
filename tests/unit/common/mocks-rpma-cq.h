04a2b16bd3b03c5c2dd3ce90445e4f90364a4e98
Xiao Yang <yangx.jy@fujitsu.com>
2021-05-14 18:54:22 +0800

    test: add and refactor unit tests
    Signed-off-by: Xiao Yang <yangx.jy@fujitsu.com>

---
+++ tests/unit/common/mocks-rpma-cq.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2021, Fujitsu */
+
+/*
+ * mocks-rpma-cq.h -- librpma cq.c module mocks
+ */
+
+#ifndef MOCKS_RPMA_CQ_H
+#define MOCKS_RPMA_CQ_H
+
+#include "cq.h"
+
+#define MOCK_RPMA_CQ		(struct rpma_cq *)0xD418
+
+#endif /* MOCKS_RPMA_CQ_H */
