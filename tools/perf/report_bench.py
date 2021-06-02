6cb298346090bfc863027a4de8192348b796b957
Jan M Michalski <jan.m.michalski@intel.com>
2021-06-02 07:26:34 -0400

    tools: perf: adjust report_*.py files to pylint requirements

--- tools/perf/report_bench.py
+++ tools/perf/report_bench.py
@@ -4,44 +4,44 @@
 # Copyright 2021, Intel Corporation
 #
 
-#
-# report_bench.py -- run report-specific benchmarks (EXPERIMENTAL)
-#
+"""report_bench.py -- run report-specific benchmarks (EXPERIMENTAL)"""
 
 import argparse
-import json
-import os
 
-from lib.common import *
-from lib.Bench import *
+from lib.common import json_from_file, dir_path
+from lib.Bench import Bench
 
-Parser = argparse.ArgumentParser(
+PARSER = argparse.ArgumentParser(
     description='Run report-specific benchmarks (EXPERIMENTAL)')
-subparsers = Parser.add_subparsers(dest='command')
+SUBPARSERS = PARSER.add_subparsers(dest='command')
 # Python >= 3.7 accepts 'required' kwarg. For older versions, it is validated
 # manually.
-subparsers.required = True
-parser_f = subparsers.add_parser('run', help='start a new benchmark')
-parser_f.add_argument('--config', type=json_from_file, required=True,
-    help='a platform configuration file')
+SUBPARSERS.required = True
+PARSER_R = SUBPARSERS.add_parser('run', help='start a new benchmark')
+PARSER_R.add_argument('--config', type=json_from_file, required=True,
+                      help='a platform configuration file')
 # XXX it should be possible to provide multiple files or an experiment.json
 # file providing a set of benchmarks to be run
-parser_f.add_argument('--figure', type=json_from_file, required=True,
-    help='a figure file describing the benchmark')
-parser_f.add_argument('--result_dir', type=dir_path, required=True,
-    help='an output directory')
-parser_c = subparsers.add_parser('continue', help='continue the benchmark')
-parser_c.add_argument('--bench', type=json_from_file, required=True,
-    help='a bench.json file of an interrupted benchmark')
+PARSER_R.add_argument('--figure', type=json_from_file, required=True,
+                      help='a figure file describing the benchmark')
+PARSER_R.add_argument('--result_dir', type=dir_path, required=True,
+                      help='an output directory')
+PARSER_C = SUBPARSERS.add_parser('continue', help='continue the benchmark')
+PARSER_C.add_argument('--bench', type=json_from_file, required=True,
+                      help='a bench.json file of an interrupted benchmark')
 
 def main():
-    args = Parser.parse_args()
+    """
+    Create new or restore the Bench object and try to run all defined
+    benchmarks.
+    """
+    args = PARSER.parse_args()
     if args.command == "run":
         bench = Bench.new(args.config, args.figure, args.result_dir)
     elif args.command == "continue":
         bench = Bench.carry_on(args.bench)
     else:
-        raise ValueError(f"Unsupported command: {args.command}") 
+        raise ValueError(f"Unsupported command: {args.command}")
     bench.run()
     print('Done.')
 
