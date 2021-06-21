67a45d044a6d7dd6c212a7a57e7c7fb658cc7e73
Wang, Long <long1.wang@intel.com>
2021-06-21 11:48:16 +0800

    test: add rpma_conn_cfg_set_cq_size multithreaded test

---
+++ tests/multithreaded/conn/rpma_conn_cfg_common.c
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * rpma_conn_cfg_common.c -- common part for rpma_conn_cfg_* multithreaded tests
+ */
+
+#include <stdlib.h>
+#include <librpma.h>
+
+#include "mtt.h"
+#include "rpma_conn_cfg_common.h"
+
+/*
+ * rpma_conn_cfg_common_init -- allocate state and create a new connection
+ * configuration object
+ */
+void rpma_conn_cfg_common_init(unsigned id, void *prestate, void **state_ptr,
+		struct mtt_result *tr)
+{
+	struct rpma_conn_cfg_common_state *st =
+		(struct rpma_conn_cfg_common_state *)
+		calloc(1, sizeof(struct rpma_conn_cfg_common_state));
+	int ret;
+
+	if (!st) {
+		MTT_ERR(tr, "calloc", errno);
+		return;
+	}
+
+	if ((ret = rpma_conn_cfg_new(&st->cfg_ptr))) {
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_new", ret);
+		return;
+	}
+
+	*state_ptr = st;
+}
+
+/*
+ * rpma_conn_cfg_common_fini -- free the connection configuration object
+ * and free the state
+ */
+void rpma_conn_cfg_common_fini(unsigned id, void *prestate, void **state_ptr,
+		struct mtt_result *tr)
+{
+	struct rpma_conn_cfg_common_state *st =
+		(struct rpma_conn_cfg_common_state *)*state_ptr;
+	int ret;
+
+	if ((ret = rpma_conn_cfg_delete(&st->cfg_ptr)))
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_delete", ret);
+
+	free(st);
+	*state_ptr = NULL;
+}
