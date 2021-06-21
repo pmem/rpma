53379f9e5007b3aec1af646a6a2b1fed1a39ed60
Wang, Long <long1.wang@intel.com>
2021-06-21 11:54:57 +0800

    test: add rpma_conn_cfg_set_sq_size multithreaded test

---
+++ tests/multithreaded/conn/rpma_conn_cfg_set_sq_size.c
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * rpma_conn_cfg_set_sq_size.c -- rpma_conn_cfg_set_sq_size multithreaded test
+ */
+
+#include <stdlib.h>
+#include <librpma.h>
+
+#include "mtt.h"
+
+#define Q_SIZE_EXP 20
+
+struct state {
+	struct rpma_conn_cfg *cfg_ptr;
+};
+
+/*
+ * init -- allocate state and create a new connection configuration object
+ */
+void
+init(unsigned id, void *prestate, void **state_ptr, struct mtt_result *tr)
+{
+	struct state *st = (struct state *)calloc(1, sizeof(struct state));
+	int ret;
+
+	if (!st) {
+		MTT_ERR(tr, "calloc", errno);
+		return;
+	}
+
+	if ((ret = rpma_conn_cfg_new(&st->cfg_ptr))) {
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_new", ret);
+		return;
+	}
+
+	*state_ptr = st;
+}
+
+/*
+ * thread -- set connection establishment sq size and check if its value is
+ * as expected
+ */
+static void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *tr)
+{
+	struct state *st = (struct state *)state;
+	uint32_t sq_size;
+	int ret;
+
+	if ((ret = rpma_conn_cfg_set_sq_size(st->cfg_ptr, Q_SIZE_EXP))) {
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_sq_size", ret);
+		return;
+	}
+
+	if ((ret = rpma_conn_cfg_get_sq_size(st->cfg_ptr, &sq_size))) {
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_get_sq_size", ret);
+		return;
+	}
+
+	if (sq_size != Q_SIZE_EXP)
+		MTT_ERR(tr, "sq_size != Q_SIZE_EXP", EINVAL);
+}
+
+/*
+ * fini -- free the connection configuration object and free the state
+ */
+static void
+fini(unsigned id, void *prestate, void **state_ptr,
+		struct mtt_result *tr)
+{
+	struct state *st = (struct state *)*state_ptr;
+	int ret;
+
+	if ((ret = rpma_conn_cfg_delete(&st->cfg_ptr)))
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_delete", ret);
+
+	free(st);
+	*state_ptr = NULL;
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct mtt_args args = {0};
+
+	if (mtt_parse_args(argc, argv, &args))
+		return -1;
+
+	struct mtt_test test = {
+			NULL,
+			NULL,
+			NULL,
+			init,
+			thread,
+			fini,
+			NULL,
+			NULL
+	};
+
+	return mtt_run(&test, args.threads_num);
+}
