bc1ae26f5c42e18283ddd953753e89d01d45afed
Lukasz Dorau <lukasz.dorau@intel.com>
2021-07-05 07:16:25 +0200

    tools: kvtable.lines2str(): check if 'lines' is iterable

--- tools/perf/lib/kvtable.py
+++ tools/perf/lib/kvtable.py
@@ -10,7 +10,12 @@
 
 def lines2str(lines, sep = "\n"):
     """Merge a list of lines into a single string"""
-    return sep.join(lines)
+    if isinstance(lines, str):
+        return lines
+    if hasattr(lines, '__iter__'):
+        # merge a list of lines into a single string
+        return sep.join(lines)
+    return str(lines)
 
 def dict2kvtable(obj, env):
     """Generate an HTML table from a dictionary"""
