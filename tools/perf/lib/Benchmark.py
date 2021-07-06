399d38b90d0a5a4c66f7164ed0f0e98b7d40ba36
Lukasz Dorau <lukasz.dorau@intel.com>
2021-07-06 13:32:20 +0200

    tools: add checks to Benchmark.run()

--- tools/perf/lib/Benchmark.py
+++ tools/perf/lib/Benchmark.py
@@ -95,6 +95,10 @@ class Benchmark:
         return output
 
     def _benchmark_args(self, env):
+        if 'tool' not in self.oneseries or \
+           'mode' not in self.oneseries or \
+           'server_ip' not in env:
+            raise ValueError
         args = ['./' + self.oneseries['tool'], env['server_ip']]
         if 'tool_mode' in self.oneseries.keys():
             args.append(self.oneseries['tool_mode'])
@@ -106,6 +110,26 @@ class Benchmark:
     def run(self, config, result_dir):
         args = self._benchmark_args(config)
         env = self._get_env(config, result_dir)
+
+        if 'filetype' not in self.oneseries or 'id' not in self.oneseries:
+            raise ValueError
+
+        if self.oneseries['filetype'] == 'malloc':
+            env['REMOTE_JOB_MEM_PATH'] = 'malloc'
+        elif self.oneseries['filetype'] == 'pmem':
+            if 'REMOTE_JOB_MEM_PATH' not in env or env['REMOTE_JOB_MEM_PATH'] == 'malloc':
+                raise ValueError
+
+        if 'tool_mode' in self.oneseries.keys() and self.oneseries['tool_mode'] == 'gpspm':
+            if 'busy_wait_polling' not in self.oneseries:
+                raise ValueError
+
+        if 'busy_wait_polling' in self.oneseries:
+            if self.oneseries['busy_wait_polling']:
+                env['BUSY_WAIT_POLLING'] = '1'
+            else:
+                env['BUSY_WAIT_POLLING'] = '0'
+
         process = subprocess.run(args, env=env)
         process.check_returncode()
         self.oneseries['done'] = True
