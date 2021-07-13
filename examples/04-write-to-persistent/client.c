28b5093b8f788d5a5b82dfc506976e6b86c25873
Li Zhijian <lizhijian@cn.fujitsu.com>
2021-07-13 16:19:20 +0800

    examples/04-write-to-persistent: correct the return value
    Generally, program should exit with non-zero if there is something wrong
    
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>

--- examples/04-write-to-persistent/client.c
+++ examples/04-write-to-persistent/client.c
@@ -215,6 +215,7 @@ main(int argc, char *argv[])
 	if (ret) {
 		goto err_mr_dereg;
 	} else if (dst_size < KILOBYTE) {
+		ret = -1;
 		fprintf(stderr,
 				"Remote memory region size too small for writing the data of the assumed size (%zu < %d)\n",
 				dst_size, KILOBYTE);
@@ -244,12 +245,14 @@ main(int argc, char *argv[])
 		goto err_mr_remote_delete;
 
 	if (cmpl.op != RPMA_OP_READ) {
+		ret = -1;
 		(void) fprintf(stderr,
 				"unexpected cmpl.op value (%d != %d)\n",
 				cmpl.op, RPMA_OP_READ);
 		goto err_mr_remote_delete;
 	}
 	if (cmpl.op_status != IBV_WC_SUCCESS) {
+		ret = -1;
 		(void) fprintf(stderr, "rpma_read failed with %d\n",
 				cmpl.op_status);
 		goto err_mr_remote_delete;
