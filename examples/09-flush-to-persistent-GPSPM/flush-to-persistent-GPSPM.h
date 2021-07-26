d991461e17e7eb93f7282831d111141202c8fade
Xiao Yang <yangx.jy@fujitsu.com>
2021-07-26 22:56:58 +0800

    examples: make example-09 use seperate receive CQ
    Signed-off-by: Xiao Yang <yangx.jy@fujitsu.com>

--- examples/09-flush-to-persistent-GPSPM/flush-to-persistent-GPSPM.h
+++ examples/09-flush-to-persistent-GPSPM/flush-to-persistent-GPSPM.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* Copyright 2020, Intel Corporation */
+/* Copyright 2021, Fujitsu */
 
 /*
  * flush-to-persistent-GSPSPM.h -- a common declarations for the 09 example
@@ -12,5 +13,7 @@
 
 #define SEND_OFFSET	0
 #define RECV_OFFSET	(SEND_OFFSET + MSG_SIZE_MAX)
+
+#define RCQ_SIZE	1
 
 #endif /* EXAMPLES_FLUSH_2_PMEM_GPSPM */
