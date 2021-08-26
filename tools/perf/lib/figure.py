82643fcdd0722f7f8e922acae7be68f0f6c0b27c
Jan M Michalski <jan.m.michalski@intel.com>
2021-08-26 13:30:56 -0400

    tools: perf: add lib.figure to pylint

--- tools/perf/lib/figure.py
+++ tools/perf/lib/figure.py
@@ -4,22 +4,23 @@
 # Copyright 2021, Intel Corporation
 #
 
-#
-# Figure.py -- generate figure-related products (EXPERIMENTAL)
-#
+"""figure.py -- generate figure-related products (EXPERIMENTAL)"""
 
-import matplotlib.pyplot as plt
+import json
 import os.path
-
 from textwrap import wrap
-from .common import *
-from .flat import *
-from .Benchmark import *
+import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
 
-SKIP_NO_AXIS_MSG = "SKIP: Axis '{}' is not provided by the series of id={}. Available keys are: {}"
+from lib.common import json_from_file, str2key
+from lib.flat import make_flat, process_fstrings
+
+# XXX allow not breaking long strings
+SKIP_NO_AXIS_MSG = "SKIP: Axis '{}' is not provided by the series of id={}. " \
+    "Available keys are: {}"
 
 class Figure:
+    """A single figure"""
 
     _figure_kwargs = {'figsize': [6.4, 4.8], 'dpi': 200, \
         'tight_layout': {'pad': 6}}
@@ -27,14 +28,14 @@ class Figure:
     def _series_file(self, result_dir):
         return os.path.join(result_dir, self.file + '.json')
 
-    def __init__(self, f, result_dir = ""):
+    def __init__(self, f, result_dir=""):
         self.output = f['output']
         self.output['done'] = self.output.get('done', False)
         # copies for convenience
         self.title = self.output['title']
         self.file = self.output['file']
-        self.x = self.output['x']
-        self.y = self.output['y']
+        self.argx = self.output['x']
+        self.argy = self.output['y']
         self.key = self.output['key']
         self.xscale = self.output.get('xscale', 'log')
         self.result_dir = result_dir
@@ -50,6 +51,7 @@ class Figure:
         return {'output': self.output, 'series': self.series}
 
     def is_done(self):
+        """Are all steps completed?"""
         return self.output['done']
 
     @staticmethod
@@ -67,7 +69,9 @@ class Figure:
         """Generate all derived variables of a series"""
         output = {}
         if 'rw' in oneseries.keys():
-            output['rw_order'] = 'rand' if oneseries['rw'] in ['randread', 'randwrite'] else 'seq'
+            output['rw_order'] = \
+                'rand' if oneseries['rw'] in ['randread', 'randwrite'] \
+                else 'seq'
         if 'x' in oneseries.keys():
             output['x_key'] = str2key(oneseries['x'])
         if 'y' in oneseries.keys():
@@ -79,15 +83,17 @@ class Figure:
         """Flatten the figures list"""
         figures = make_flat(figures, cls.get_figure_desc)
         figures = process_fstrings(figures, cls.get_figure_desc,
-            cls.oneseries_derivatives)
+                                   cls.oneseries_derivatives)
         output = []
-        for f in figures:
+        for figure in figures:
             # flatten series
-            common = f.get('series_common', {})
-            f['series'] = make_flat(f['series'], cls.get_oneseries_desc, common)
-            f['series'] = process_fstrings(f['series'], cls.get_oneseries_desc,
+            common = figure.get('series_common', {})
+            figure['series'] = make_flat(
+                figure['series'], cls.get_oneseries_desc, common)
+            figure['series'] = process_fstrings(
+                figure['series'], cls.get_oneseries_desc,
                 cls.oneseries_derivatives)
-            output.append(cls(f))
+            output.append(cls(figure))
         return output
 
     def prepare_series(self, result_dir):
@@ -97,12 +103,12 @@ class Figure:
         """
         output = {}
         output['title'] = self.title
-        output['x'] = self.x
-        output['y'] = self.y
+        output['x'] = self.argx
+        output['y'] = self.argy
         output['series'] = []
         for series in self.series:
-            idfile = os.path.join(result_dir, 'benchmark_' + str(series['id']) +
-                '.json')
+            idfile = os.path.join(result_dir,
+                                  'benchmark_' + str(series['id']) + '.json')
             rows = json_from_file(idfile)['json']
             # If it is dict() it indicates a mix workload consisting of
             # two parts: 'read' and 'write'. In this case, the series
@@ -113,14 +119,17 @@ class Figure:
             # it is assumed each row has the same names of columns
             keys = rows[0].keys()
             # skip the series if it does not have required keys
-            if self.x not in keys:
-                print(SKIP_NO_AXIS_MSG.format(self.x, series['id'], str(keys)))
+            if self.argx not in keys:
+                print(SKIP_NO_AXIS_MSG.format(
+                    self.argx, series['id'], str(keys)))
                 continue
-            elif self.y not in keys:
-                print(SKIP_NO_AXIS_MSG.format(self.y, series['id'], str(keys)))
+            if self.argy not in keys:
+                print(SKIP_NO_AXIS_MSG.format(
+                    self.argy, series['id'], str(keys)))
                 continue
-            points = [[row[self.x], row[self.y]] for row in rows]
-            output['series'].append({'label': series['label'], 'points': points})
+            points = [[row[self.argx], row[self.argy]] for row in rows]
+            output['series'].append(
+                {'label': series['label'], 'points': points})
         # save the series to a file
         series_path = self._series_file(result_dir)
         if os.path.exists(series_path):
@@ -134,13 +143,13 @@ class Figure:
         self.output['done'] = True
 
     def _points_to_xy(self, points):
-        xs = [p[0] for p in points]
-        ys = [p[1] for p in points]
-        return xs, ys
+        xslist = [p[0] for p in points]
+        yslist = [p[1] for p in points]
+        return xslist, yslist
 
     def _label(self, column, with_better=False):
         """Translate the name of a column to a label with a unit"""
-        label_by_column={
+        label_by_column = {
             'threads': '# of threads',
             'iodepth': 'iodepth',
             'bs': 'block size [B]',
@@ -152,54 +161,57 @@ class Figure:
         }
         lower = 'lower is better'
         higher = 'higher is better'
-        better_by_column={
+        better_by_column = {
             'lat_avg': lower,
             'lat_pctl_99.9': lower,
             'lat_pctl_99.99': lower,
             'bw_avg': higher
         }
-        # If the column is not in the dictionary the default return value is the raw name of the column.
+        # If the column is not in the dictionary
+        # the default return value is the raw name of the column.
         output = label_by_column.get(column, column)
         if with_better:
             output += '\n(' + better_by_column.get(column, column) + ')'
         return output
 
     def png_path(self):
+        """get a path to the output PNG file"""
         output = self.file + '_' + self.key + '.png'
         return os.path.join('.', output)
 
     def to_png(self):
+        """generate an output PNG file"""
         # set output file size, padding and title
         fig = plt.figure(**Figure._figure_kwargs)
         suptitle = "\n".join(wrap(self.title, 60))
         fig.suptitle(suptitle, fontsize='medium', y=0.90)
         # get a subplot
-        ax = plt.subplot(1, 1, 1)
+        plot = plt.subplot(1, 1, 1)
         # XXX bw_avg [threads=24, iodepth=2, block size=4096B]
-        ax.title.set_text('')
+        plot.title.set_text('')
         xticks = []
         for oneseries in self.series:
             # draw series ony-by-one
-            xs, ys = self._points_to_xy(oneseries['points'])
-            ax.plot(xs, ys, marker='.', label=oneseries['label'])
+            xslist, yslist = self._points_to_xy(oneseries['points'])
+            plot.plot(xslist, yslist, marker='.', label=oneseries['label'])
             # collect all existing x values
-            xticks.extend(xs)
+            xticks.extend(xslist)
         # make values unique (set) and sort them
         xticks = sorted(list(set(xticks)))
         # set the x-axis scale
         if self.xscale == "linear":
-            ax.set_xscale(self.xscale)
+            plot.set_xscale(self.xscale)
         else:
-            ax.set_xscale(self.xscale, base=2)
-            ax.xaxis.set_major_formatter(ScalarFormatter())
+            plot.set_xscale(self.xscale, base=2)
+            plot.xaxis.set_major_formatter(ScalarFormatter())
 
-        ax.set_xticks(xticks)
-        plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
-        ax.set_xlabel(self._label(self.x))
-        ax.set_ylabel(self._label(self.y, with_better=True))
-        ax.set_ylim(bottom=0)
-        ax.legend(fontsize=6)
-        ax.grid(True)
+        plot.set_xticks(xticks)
+        plt.setp(plot.get_xticklabels(), rotation=45, ha='right')
+        plot.set_xlabel(self._label(self.argx))
+        plot.set_ylabel(self._label(self.argy, with_better=True))
+        plot.set_ylim(bottom=0)
+        plot.legend(fontsize=6)
+        plot.grid(True)
 
         os.chdir(self.result_dir)
         plt.savefig(self.png_path())
@@ -216,8 +228,10 @@ class Figure:
 
         # rows
         for oneseries in self.series:
-            # since the output is processed as markdown, special characters have to be escaped
-            html += "<tr><td>" + oneseries['label'].replace('_', '\\_') + "</td>"
+            # Since the output is processed as markdown,
+            # special characters have to be escaped.
+            html += "<tr><td>" + oneseries['label'].replace('_', '\\_') \
+                + "</td>"
             for point in oneseries['points']:
                 html += "<td>" + str(point[1]) + "</td>"
             html += "</tr>"
--- tools/perf/tests/lib/figure/test_html_data_table.py
+++ tools/perf/tests/lib/figure/test_html_data_table.py
@@ -4,11 +4,11 @@
 # Copyright 2021, Intel Corporation
 #
 
-"""test_html_data_table.py -- lib.Figure.html_data_table() tests"""
+"""test_html_data_table.py -- lib.figure.Figure.html_data_table() tests"""
 
 import pytest
 from lib.common import json_from_file
-from lib.Figure import Figure
+from lib.figure import Figure
 
 DATA = {
     'output': {
@@ -33,5 +33,5 @@ DATA = {
 HTML = "<table><thead><tr><th></th><th>0</th><th>1</th><th>2</th></tr></thead><tbody><tr><td>label\\_1</td><td>3</td><td>4</td><td>5</td></tr><tr><td>label\\_2</td><td>6</td><td>7</td><td>8</td></tr></tbody></table>"
 
 def test_html_data_table_basic():
-    """basic lib.Figure.html_data_table() test"""
+    """basic lib.figure.Figure.html_data_table() test"""
     assert Figure(DATA).html_data_table() == HTML
