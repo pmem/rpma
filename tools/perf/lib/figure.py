86f3b13620d8fa3f966ba3f0bf8345b107d7d67b
Jan M Michalski <jan.m.michalski@intel.com>
2021-08-30 14:01:40 -0400

    tools: perf: improve data tables
    - collect a common subset of x-values
    - replace missing y-values with '-'

--- tools/perf/lib/figure.py
+++ tools/perf/lib/figure.py
@@ -157,6 +157,17 @@ class Figure:
         yslist = [p[1] for p in points]
         return xslist, yslist
 
+    def _get_xcommon(self):
+        """generate an ordered list of common x-values"""
+        xlist = [p[0] for oneseries in self.series for p in oneseries['points']]
+        xlist.sort()
+        return list(set(xlist))
+
+    @staticmethod
+    def _points_to_dict(points):
+        """transform list of [x, y] into a dict() where {x: y}"""
+        return {p[0]: p[1] for p in points}
+
     def _label(self, column, with_better=False):
         """Translate the name of a column to a label with a unit"""
         label_by_column = {
@@ -232,19 +243,19 @@ class Figure:
         Create an HTML snippet string with a table containing the Figure data.
         """
         # header
-        html = "<table><thead><tr><th></th>"
-        for point in self.series[0]['points']:
-            html += "<th>" + str(point[0]) + "</th>"
-        html += "</tr></thead><tbody>"
-
+        xcommon = self._get_xcommon()
+        html = '<table><thead><tr><th></th><th>{}</th></tr></thead><tbody>'. \
+            format('</th><th>'.join([str(x) for x in xcommon]))
         # rows
         for oneseries in self.series:
             # Since the output is processed as markdown,
             # special characters have to be escaped.
             html += "<tr><td>" + oneseries['label'].replace('_', '\\_') \
                 + "</td>"
-            for point in oneseries['points']:
-                html += "<td>" + str(point[1]) + "</td>"
+            points = Figure._points_to_dict(oneseries['points'])
+            points = {k: '{0:.2f}'.format(v) for k, v in points.items()}
+            for xarg in xcommon:
+                html += '<td>{}</td>'.format(points.get(xarg, '-'))
             html += "</tr>"
 
         # end the table
