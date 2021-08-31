73e59d5627408d52090e96c3d86308a7746ef4d2
Oksana Salyk <oksana.salyk@intel.com>
2021-08-31 19:31:35 -0400

    tools: fix y-axis to 110% of max

--- tools/perf/report_figures.py
+++ tools/perf/report_figures.py
@@ -15,6 +15,8 @@ PARSER = argparse.ArgumentParser(
     description='Generate figures (EXPERIMENTAL)')
 PARSER.add_argument('--bench', type=json_from_file, required=True,
                     help='a bench.json file of a completed benchmark')
+PARSER.add_argument('--report', type=json_from_file,
+                    help='a report configuration file')
 PARSER.add_argument('--include_titles', dest='include_titles',
                     action='store_true',
                     help='include titles into the output figures')
@@ -27,7 +29,15 @@ def main():
     args = PARSER.parse_args()
     bench = Bench.carry_on(args.bench)
     bench.check_completed()
+    bw_max = None
+    if args.report:
+        bw_max = args.report['json'].get('visualization', {}).\
+            get('rnic_max_bw', None)
+    if bw_max is not None:
+        bw_max *= 1.1
     for figure in bench.figures:
+        if figure.argy == 'bw_avg':
+            figure.set_yaxis_max(bw_max)
         figure.to_png(args.include_titles)
     print('Done.')
 
