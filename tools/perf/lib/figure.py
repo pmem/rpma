15cda148fe98d4ad62585f443e874438635dd7c9
Jan M Michalski <jan.m.michalski@intel.com>
2021-09-14 14:54:27 -0400

    tools: perf: compare figure by figure (ALT)

--- tools/perf/lib/figure.py
+++ tools/perf/lib/figure.py
@@ -58,7 +58,7 @@ class Figure:
         """A comparison function"""
         if DeepDiff(self.output, other.output, exclude_paths=["root['title']"]):
             return False
-        if self.series_in != other.series_in:
+        if len(self.series_in) != len(other.series_in):
             return False
         if DeepDiff(self.series_in, other.series_in):
             return False
