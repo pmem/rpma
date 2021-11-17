39793db251cb497eb56edc00fdf0abd7cbaeeae2
Jan M Michalski <jan.m.michalski@intel.com>
2021-11-17 15:34:34 +0000

    tools: perf: BaseRunner and IbReadRunner (scaffolding)

---
+++ tools/perf/lib/benchmark/runner/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2021, Intel Corporation
+#
+
+"""base.py -- the base benchmark runner (EXPERIMENTAL)"""
+
+from .fio import FioRunner
+from .ib_read import IbReadRunner
+
+class BaseRunner:
+    """XXX"""
+
+    RUNNERS = {
+        'fio': FioRunner,
+        'ib': IbReadRunner
+    }
+
+    @classmethod
+    def run(cls, benchmark, config, result_dir):
+        """XXX"""
+        # XXX DUMP_CMDS?
+        idfile = benchmark.get_output_file(result_dir)
+        runner_cls = cls.RUNNERS.get(benchmark.oneseries['tool'], None)
+        if runner_cls is None:
+            raise NotImplementedError()
+        runner = runner_cls(benchmark, config, idfile)
+        runner.run()
---
+++ tools/perf/lib/benchmark/runner/common.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2021, Intel Corporation
+#
+
+"""common.py -- the runner's helpers (EXPERIMENTAL)"""
+
+class RemoteCmd:
+    """run a command on a remote side over SSH"""
+
+    @classmethod
+    def run_sync(cls, config, cmd):
+        """XXX"""
+        # XXX run a remote command and wait till its complete
+
+    @classmethod
+    def run_async(cls, config, cmd):
+        """XXX"""
+        # XXX run a remote command and return a control object
+        # XXX maybe it can be an instance of this class?
+    
