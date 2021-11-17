40fae89042b7ac458cadeebd1a2b4103994e57e6
Lukasz Dorau <lukasz.dorau@intel.com>
2021-11-17 12:53:55 +0100

    common: add encoding argument to all open() calls
    Add the 'encoding' argument to all open() calls to avoid the error:
    
    W1514: Using open without explicitly specifying an encoding \
           (unspecified-encoding)

--- tools/perf/lib/bench.py
+++ tools/perf/lib/bench.py
@@ -62,7 +62,7 @@ class Bench:
         }
 
         output_path = os.path.join(self.result_dir, 'bench.json')
-        with open(output_path, 'w', encoding="utf-8") as file:
+        with open(output_path, 'w', encoding='utf-8') as file:
             json.dump(output, file, indent=4)
 
     def get_config(self):
