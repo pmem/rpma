bdf4a01e1d898c5f86894ac676f2cda39294fab3
Jan M Michalski <jan.m.michalski@intel.com>
2021-11-30 00:50:12 +0000

    tools: perf: separate Figure and Benchmark oneseries
    lib.figure.base.Figure instances contain oryginal set of series.
    In order to execute, each of the series initiates
    lib.benchmark.base.Benchmark instances.
    The previous implementation did not separate well which instance
    of 'oneseries' belongs to which entity.

--- tools/perf/lib/benchmark/base.py
+++ tools/perf/lib/benchmark/base.py
@@ -8,108 +8,90 @@
 # base.py -- a single benchmark object (EXPERIMENTAL)
 #
 
-import json
 import os
+from copy import deepcopy
 
-from ..common import uniq
+from ..common import uniq, ENCODE
 from .runner import BaseRunner, Bash, Dummy
-
-ENCODE = json.JSONEncoder(indent=4).encode
 
 class Benchmark:
     """A single benchmark object"""
 
-    def __init__(self, oneseries, from_figure=False):
-        self.oneseries_from_figure = None
-        if from_figure:
-            # When oneseries belongs to a Figure:
-            # - it should not be modified directly (a copy is required) and
-            # - the id when set should be propagated to the original oneseries 
-            #   object to allow relating among a unique Benchmark
-            #   and potentially multiple series making use of it.
-            self.oneseries_from_figure = oneseries
-            oneseries = oneseries.copy()
-            # validate if it is a mixed workload
-            if 'rw' in oneseries and 'rw' in oneseries['rw']:
-                if 'rw_dir' not in oneseries:
-                    raise SyntaxError(
-                        "'rw_dir' is required for mixed workloads (['rw'] == '*rw')\n{}".format(
-                            ENCODE(oneseries)))
-            # remove unnecessary fields
-            oneseries.pop('label', None)
-            oneseries.pop('rw_dir', None)
+    def __init__(self, oneseries, figure=None, series_index=-1):
+        self.__figure = figure
+        self.__series_index = series_index
+        # remove unnecessary fields
+        oneseries.pop('label', None)
+        oneseries.pop('rw_dir', None)
         oneseries['done'] = oneseries.get('done', False)
-        self.oneseries = oneseries
-        if 'requirements' in oneseries.keys():
-            self.req = oneseries['requirements']
-            # remove the remaining duplicate
-            oneseries.pop('requirements')
-        else:
-            self.req = {}
+        self.__req = oneseries.pop('requirements', {})
+        self.__oneseries = oneseries
+
+    @classmethod
+    def uniq(cls, figures):
+        """Generate a set of unique benchmarks"""
+        output = [cls(oneseries, figure, index)
+            for figure in figures
+                for index, oneseries in enumerate(figure.series_in)
+        ]
+        return uniq(output)
 
     def __repr__(self):
         """A string representation of the object"""
-        return ENCODE(self.oneseries)
+        return ENCODE(self.__oneseries)
 
     def __eq__(self, other):
         """A comparison function"""
         # Benchmark is defined also by its requirements.
-        if self.req != other.req:
+        if self.__req != other.__req:
             return False
         # a complete list of all keys from both objects (without duplicates)
-        keys = list(set([*self.oneseries.keys(), *other.oneseries.keys()]))
+        keys = list(set([*self.__oneseries.keys(), *other.__oneseries.keys()]))
         for k in keys:
             # ignore series-specific or instance-specific keys
             if k == 'id':
                 continue
-            sv = self.oneseries.get(k, None)
-            ov = other.oneseries.get(k, None)
+            sv = self.__oneseries.get(k, None)
+            ov = other.__oneseries.get(k, None)
             if sv != ov:
                 return False
         return True
 
     def set_id(self, id):
         """Set an instance id"""
-        self.oneseries['id'] = id
-        if self.oneseries_from_figure is not None:
-            # Propagated to the original oneseries object to allow relating
-            # to the Benchmark object.
-            self.oneseries_from_figure['id'] = id
+        self.__oneseries['id'] = id
+        if self.__figure is not None:
+            self.__figure.set_series_id(self.__series_index, id)
 
     def get_id(self):
         """Get the instance id"""
-        return self.oneseries.get('id', None)
+        return self.__oneseries.get('id', None)
 
     def get_output_file(self, result_dir):
         """Get the output file path"""
         return os.path.join(result_dir,
             'benchmark_' + str(self.get_id()) + '.json')
 
-    @classmethod
-    def uniq(cls, figures):
-        """Generate a set of unique benchmarks"""
-        output = [cls(oneseries, from_figure=True)
-            for f in figures
-                for oneseries in f.get_series_in()
-        ]
-        return uniq(output)
+    @property
+    def oneseries(self):
+        return deepcopy(self.__oneseries)
 
     def get_requirements(self):
-        return self.req
+        return deepcopy(self.__req)
 
     def cache(self):
         """Cache the current state of execution"""
-        return self.oneseries
+        return self.__oneseries
 
     def is_done(self):
-        return self.oneseries['done']
+        return self.__oneseries['done']
 
     ONESERIES_REQUIRED = ['filetype', 'id', 'tool', 'mode']
     CONFIG_REQUIRED = ['server_ip']
 
     def __validate(self, config):
         for required in self.ONESERIES_REQUIRED:
-            if required not in self.oneseries:
+            if required not in self.__oneseries:
                 raise ValueError("'{}' is missing in the figure".format(required))
         for required in self.CONFIG_REQUIRED:
             if required not in config:
@@ -127,13 +109,13 @@ class Benchmark:
         self.__validate(config)
         if config.get('dummy_results', False):
             runner = Dummy
-        elif '.sh' in self.oneseries['tool']:
+        elif '.sh' in self.__oneseries['tool']:
             runner = Bash
         else:
             runner = BaseRunner
 
         runner.run(self, config, result_dir)
-        self.oneseries['done'] = True
+        self.__oneseries['done'] = True
 
     def skip(self):
-        self.oneseries['done'] = True
+        self.__oneseries['done'] = True
