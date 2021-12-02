33a900ad8b8a770202ebf7cacec70122bf99db11
Jan M Michalski <jan.m.michalski@intel.com>
2021-12-02 03:12:23 +0000

    tools: perf: document lib.benchmark.runner

--- tools/perf/lib/benchmark/runner/common.py
+++ tools/perf/lib/benchmark/runner/common.py
@@ -4,10 +4,18 @@
 # Copyright 2021, Intel Corporation
 #
 
-"""common.py -- the runner's helpers (EXPERIMENTAL)"""
+#
+# common.py
+#
 
+"""the runner's helpers (EXPERIMENTAL)"""
+
+#: an error message when an unexpected mode is detected
 UNKNOWN_MODE_MSG = "An unexpected 'mode' value: {}"
+
+#: an error message when x-axis cannot be identified
 NO_X_AXIS_MSG = \
     "SETTINGS_BY_MODE[{}] is missing a key defined as a list (x-axis)"
 
+#: a common block sizes list
 BS_VALUES = [256, 1024, 4096, 8192, 16384, 32768, 65536, 131072, 262144]
