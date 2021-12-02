7fc7a3b7ff16a2194121dd51830aac561d45336a
Jan M Michalski <jan.m.michalski@intel.com>
2021-12-02 04:14:25 +0000

    tools: perf: document lib.common

--- tools/perf/lib/common.py
+++ tools/perf/lib/common.py
@@ -5,16 +5,34 @@
 #
 
 #
-# common.py -- common definitions (EXPERIMENTAL)
+# common.py
 #
+
+"""common tools (EXPERIMENTAL)"""
 
 import json
 import os.path
 
+from typing import Union
+
+#: a JSON encoder with nice to read indent
 ENCODE = json.JSONEncoder(indent=4).encode
 
-def dir_path(string):
-    """Validate provided directory path"""
+def dir_path(string: str) -> str:
+    """validate path and return it as an absolute path
+
+    **Note**: If `string` does not exist in the filesystem it is created
+    (`os.mkdir()`).
+
+    Args:
+        string: a path to process
+
+    Returns
+        An abolute path made from `string`.
+
+    Raises:
+        NotADirectoryError: If the `string` exists but it is not a directory.
+    """
     if os.path.exists(string):
         if not os.path.isdir(string):
             raise NotADirectoryError(string)
@@ -24,8 +42,22 @@ def dir_path(string):
     # make the path absolute before giving it back
     return os.path.abspath(string)
 
-def json_from_file(string):
-    """Validate provided JSON file"""
+def json_from_file(string: str) -> dict:
+    """load a `string` JSON file
+
+    Args:
+        string: a path to a JSON file
+
+    Return:
+        An `output` dictionary composed of:
+
+        - `output['input_file']` is the path of the JSON file
+        - `output['json']` is the contents of the JSON file as a Python object
+          (`json.load()`).
+
+    Raises:
+        FileNotFoundError: If the `string` file does not exist.
+    """
     if not os.path.isfile(string):
         raise FileNotFoundError(string)
     # read the file
@@ -34,10 +66,25 @@ def json_from_file(string):
     # return the content of the file
     return {'input_file': string, 'json': data}
 
-def uniq(elems):
-    """Generate a set of unique objects"""
+def uniq(elems: list) -> dict:
+    """generate a set of unique objects
+
+    The list is browsed in order to remove duplicates. Each of the unique
+    objects has been assigned an identifier to produce a dictionary where keys
+    are identifiers and values are objects.
+
+    **Note**: Objects to be compatible with the `uniq()` function has to be
+    comparable (`__eq__()`) and has a property called `identifier` which will
+    be used in the process of assigning an identifier to the object.
+
+    Args:
+        elems: an input `list` of objects
+
+    Returns:
+        A dictionary `{identifier: object}`.
+    """
     output = {}
-    id = 0
+    identifier = 0
     for elem in elems:
         duplicate = False
         # look up for duplicates
@@ -49,16 +96,25 @@ def uniq(elems):
                 break
         # a new benchmark found
         if not duplicate:
-            elem.identifier = id
-            output[id] = elem
-            id += 1
+            elem.identifier = identifier
+            output[identifier] = elem
+            identifier += 1
     return output
 
-def escape(x):
-    """Escape markdown special characters"""
-    if isinstance(x, str):
-        return x.replace('_', '\\_')
-    elif isinstance(x, list):
-        return [escape(el) for el in x]
-    else:
-        raise ValueError('Unsupported type in escape(): ' + type(x))
+def escape(strings: Union[str, list]) -> Union[str, list]:
+    """Escape markdown special characters
+
+    Args:
+        strings: an input to process
+
+    Return:
+        A processed input.
+
+    Raises:
+        ValueError: If the input is not `str` nor `list`.
+    """
+    if isinstance(strings, str):
+        return strings.replace('_', '\\_')
+    if isinstance(strings, list):
+        return [escape(el) for el in strings]
+    raise ValueError('Unsupported type in escape(): ' + type(strings))
