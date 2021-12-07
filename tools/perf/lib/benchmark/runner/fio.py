79186d40f3154ea26c493f2f9b381b7fbd918d40
Lukasz Dorau <lukasz.dorau@intel.com>
2021-12-07 16:05:04 +0100

    common: AUTO-PUSH

--- tools/perf/lib/benchmark/runner/fio.py
+++ tools/perf/lib/benchmark/runner/fio.py
@@ -14,7 +14,8 @@ from os.path import join
 from shutil import which
 from ...common import json_from_file
 from ...remote_cmd import RemoteCmd
-from .common import UNKNOWN_MODE_MSG, NO_X_AXIS_MSG, BS_VALUES
+from .common import UNKNOWN_MODE_MSG, NO_X_AXIS_MSG, BS_VALUES, \
+                    result_append, result_is_done
 
 UNKNOWN_RW_MSG = "An unexpected 'rw' value: {}"
 UNKNOWN_FILETYPE_MSG = "An unexpected 'filetype' value: {}"
@@ -76,7 +77,8 @@ class FioRunner:
         try:
             self.__results = json_from_file(idfile)
         except FileNotFoundError:
-            self.__results = {}
+            self.__results = {'input_file': idfile, 'json': []}
+        self.__data = self.__results['json']
         self.__validate()
 
     def __server_start(self, settings):
@@ -168,16 +170,13 @@ class FioRunner:
         # in case of a mixed workload the result is a tuple
         return 0
 
-    def __result_append(self, x_value, y_value):
-        # XXX this part is probably common betwen ib_read and fio classes
-        # maybe it should fit into the common module? TBD
-        # I think it should also be step-by-step cached into a file
-        # so the execution process will be more resilient
-        pass
+    def __result_append(self, _, y_value: dict):
+        """append new result to internal __data and the '__idfile' file"""
+        result_append(self.__data, self.__idfile, y_value)
 
-    def __result_is_done(self, x_value):
-        """check if the result for a given x value is already collected"""
-        # XXX
+    def __result_is_done(self, x_value: int):
+        """check if the result for the given x value is already collected"""
+        return result_is_done(self.__data, self.__x_key, x_value)
 
     def run(self) -> None:
         """collects the `benchmark` results using `fio`
