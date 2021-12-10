3adca1d452cc29238de538624755c3a9c4706f73
Lukasz Dorau <lukasz.dorau@intel.com>
2021-12-10 10:34:43 +0100

    test: enable the sysconf mock only in the main of unit tests
    The clang ASAN sanitizer calls sysconf() during
    the initialization step before main() and it fails
    all unit tests using the mock of this syscall.
    
    This patch introduces possibility to enable chosen
    (only sysconf in this patch) unistd mocks
    only during the main() function of unit tests.
    
    Fixes: #1397

--- tests/unit/common/mocks-unistd.c
+++ tests/unit/common/mocks-unistd.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2020-2021, Intel Corporation */
 
 /*
  * mocks-unistd.c -- unistd mocks
@@ -11,12 +11,37 @@
 #include "cmocka_headers.h"
 #include "mocks-unistd.h"
 
+static int unistd_mocks_enabled;
+
+long __real_sysconf(int name);
+
+/*
+ * enable_unistd_mocks -- enable unistd mocks
+ */
+void
+enable_unistd_mocks(void)
+{
+	unistd_mocks_enabled = 1;
+}
+
+/*
+ * disable_unistd_mocks -- disable unistd mocks
+ */
+void
+disable_unistd_mocks(void)
+{
+	unistd_mocks_enabled = 0;
+}
+
 /*
  * __wrap_sysconf -- sysconf() mock
  */
 long
 __wrap_sysconf(int name)
 {
+	if (unistd_mocks_enabled == 0)
+		return __real_sysconf(name);
+
 	assert_int_equal(name, _SC_PAGESIZE);
 	int err = mock_type(int);
 	if (err) {
--- tests/unit/common/mocks-unistd.h
+++ tests/unit/common/mocks-unistd.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2020-2021, Intel Corporation */
 
 /*
  * mocks-unistd.h -- the unistd mocks' header
@@ -9,5 +9,10 @@
 #define MOCKS_UNISTD_H
 
 #define PAGESIZE 4096
+
+void enable_unistd_mocks(void);
+void disable_unistd_mocks(void);
+
+long __wrap_sysconf(int name);
 
 #endif /* MOCKS_UNISTD_H */
