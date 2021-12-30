9aa66e8208d5c8f5e5e7a38b5aa0bd6516e2d714
Tomasz Gromadzki <grom72@o2.pl>
2021-12-30 20:54:34 +0100

    tools: results handling moved to Runner class

--- tools/perf/lib/benchmark/runner/fio.py
+++ tools/perf/lib/benchmark/runner/fio.py
@@ -20,7 +20,7 @@ from ...common import json_from_file
 from ...remote_cmd import RemoteCmd
 from .common import UNKNOWN_VALUE_MSG, NO_X_AXIS_MSG, MISSING_KEY_MSG, \
                     BS_VALUES, run_pre_command, run_post_command, \
-                    result_append, result_is_done, print_start_message
+                    print_start_message
 from .runner import Runner
 class FioRunner(Runner):
     """the FIO runner
@@ -115,7 +115,7 @@ class FioRunner(Runner):
             self.__results = json_from_file(idfile)
         except FileNotFoundError:
             self.__results = {'input_file': idfile, 'json': []}
-        self.__data = self.__results['json']
+        self._data = self.__results['json']
         self.__validate()
 
     def __server_start(self, settings):
@@ -255,11 +255,11 @@ class FioRunner(Runner):
 
     def __result_append(self, _, y_value: dict):
         """append new result to internal __data and the '_idfile' file"""
-        result_append(self.__data, self._idfile, y_value)
+        self._result_append(y_value)
 
     def __result_is_done(self, x_value: int):
         """check if the result for the given x value is already collected"""
-        return result_is_done(self.__data, self.__x_key, x_value)
+        return self._result_is_done(self.__x_key, x_value)
 
     def __set_log_files_names(self):
         """set names of log files"""
