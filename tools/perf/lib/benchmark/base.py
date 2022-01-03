31779847b826dbfacd4af4d7fcdb61ffaa0bf799
osalyk <oksana.salyk@intel.com>
2022-01-03 08:25:57 -0500

    tools: improve run mode
    If bench.json exists, do not create new one, but revert state from file.
    The run option will continue interrupted benchmarks

--- tools/perf/lib/benchmark/base.py
+++ tools/perf/lib/benchmark/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2021, Intel Corporation
+# Copyright 2021-2022, Intel Corporation
 #
 
 #
@@ -165,7 +165,7 @@ class Benchmark:
                 raise ValueError(
                     "'{}' is missing in the config".format(required))
 
-    def run(self, config: dict, result_dir: str) -> None:
+    def run(self, config: dict, req: dict, result_dir: str) -> None:
         """Pick a runner and run the benchmark.
 
         Either:
@@ -184,9 +184,12 @@ class Benchmark:
         Args:
             config: the configuration of the benchmarking system
 
+            req: the dictionary of requirements
+
             result_dir: a directory where the intermediate and final products
              of the benchmarking process will be stored.
         """
+        self.__req = deepcopy(req)
         self.__validate(config)
         if config.get('dummy_results', False):
             runner = Dummy
