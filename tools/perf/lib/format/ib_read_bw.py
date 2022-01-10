1438fa067118dd007a6641b75d9d38ab3983b3a2
Lukasz Dorau <lukasz.dorau@intel.com>
2022-01-10 13:30:32 +0100

    tools: add generating null results

--- tools/perf/lib/format/ib_read_bw.py
+++ tools/perf/lib/format/ib_read_bw.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2020-2021, Intel Corporation
+# Copyright 2020-2022, Intel Corporation
 #
 
 #
@@ -79,4 +79,25 @@ class IbReadBwFormat:
             'bs': int(found.group(1)),
             'ops': int(found.group(2)),
             'bw_avg': round(float(found.group(4)), 2)
+        }
+
+    @classmethod
+    def null_results(cls, blocksize: int, threads: int, iodepth: int) -> dict:
+        """generate a null ib_read_bw output and return a row of data
+
+        Args:
+            blocksize: a value of block size
+            threads: a number of threads
+            iodepth: a value of iodepth
+
+        Returns:
+            A `dict` of results.
+        """
+
+        return {
+            'threads': int(threads),
+            'iodepth': int(iodepth),
+            'bs': int(blocksize),
+            'ops': 0,
+            'bw_avg': 0
         }
