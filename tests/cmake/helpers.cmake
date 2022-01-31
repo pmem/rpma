bd33dd6a24c5373e371d19b6807142023d2c5968
Patryk717 <patrykzbierski717@wp.pl>
2022-01-31 13:26:03 +0100

    common: remove deprecated API

--- tests/cmake/helpers.cmake
+++ tests/cmake/helpers.cmake
@@ -234,17 +234,7 @@ endfunction()
 # First argument of the command is test directory name.
 # Optional function arguments are passed as consecutive arguments to
 # the command.
-function(execute name)
-    check_target(${name})
 
-    execute_common(true ${TRACER}_${TESTCASE} ${name} ${ARGN})
-endfunction()
-
-# Executes test command ${name} under GDB.
-# First argument of the command is a gdb batch file.
-# Second argument of the command is the test command.
-# Optional function arguments are passed as consecutive arguments to
-# the command.
 function(crash_with_gdb gdb_batch_file name)
     check_target(${name})
 
