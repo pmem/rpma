a8bfc053e16a81aa05ea39045dbba7acb4367d14
Oksana Salyk <oksana.salyk@intel.com>
2022-01-31 13:50:11 -0500

    examples: add the example_12 receive_completion_queue

--- examples/run-all-on-SoftRoCE.sh
+++ examples/run-all-on-SoftRoCE.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2020-2021, Intel Corporation
+# Copyright 2020-2022, Intel Corporation
 
 #
 # run-all-on-SoftRoCE.sh - run all examples on SoftRoCE (optionally under valgrind)
@@ -107,7 +107,15 @@ function run_example() {
 		$VLD_CCMD $DIR/client $IP_ADDRESS $PORT "1234"
 		RV=$?
 		;;
+	12-receive-completion-queue)
+		SEED=7
+		ROUNDS=3
+		echo "Starting the client ..."
+		$VLD_CCMD $DIR/client $IP_ADDRESS $PORT $SEED $ROUNDS
+		RV=$?
+		;;
 	*)
+	
 		echo "Starting the client ..."
 		$VLD_CCMD $DIR/client $IP_ADDRESS $PORT
 		RV=$?
