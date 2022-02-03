60b109e19bed145613fbd7dbae711bc69a0c7d11
Oksana Salyk <oksana.salyk@intel.com>
2022-02-03 13:57:19 -0500

    examples: delete rpma_cq_get_wc() appearing before rpma_cq_wait()
    Ref: #1541

---
+++ examples/08-messages-ping-pong/messages-ping-pong-common.c
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * messages-ping-pong-common.c -- a common definitions for the 08 example
+ */
+
+#include <librpma.h>
+#include <inttypes.h>
+
+#include "messages-ping-pong-common.h"
+
+static int
+verify_wc(struct ibv_wc *wc, uint64_t *recv,
+		int *send_cmpl, int *recv_cmpl)
+{
+	if (wc->status != IBV_WC_SUCCESS) {
+		char *func = (wc->opcode == IBV_WC_SEND)? "send" : "recv";
+		(void) fprintf(stderr, "rpma_%s() failed: %s\n",
+			func, ibv_wc_status_str(wc->status));
+		return -1;
+	}
+
+	if (wc->opcode == IBV_WC_SEND) {
+		*send_cmpl = 1;
+	} else if (wc->opcode == IBV_WC_RECV) {
+		if (wc->wr_id != (uintptr_t)recv ||
+				wc->byte_len != MSG_SIZE) {
+			(void) fprintf(stderr,
+				"received completion is not as expected (0x%"
+				PRIXPTR " != 0x%" PRIXPTR " [wc.wr_id] || %"
+				PRIu32 " != %ld [wc.byte_len])\n", wc->wr_id,
+				(uintptr_t)recv, wc->byte_len, MSG_SIZE);
+			return -1;
+		}
+		*recv_cmpl = 1;
+	}
+	return 0;
+}
+
+int
+get_wc_and_validate(struct rpma_cq *cq, uint64_t *recv,
+		int *send_cmpl, int *recv_cmpl)
+{
+	struct ibv_wc wc[NUM_ENTRIES];
+	int num_entries_got;
+	int ret;
+
+	/* wait for the completion to be ready */
+	if ((ret = rpma_cq_wait(cq)))
+		return ret;
+
+	/* get two next completion (send, recv) */
+	if ((ret = rpma_cq_get_wc(cq, NUM_ENTRIES, wc, &num_entries_got))) {
+		if (ret == RPMA_E_NO_COMPLETION)
+			ret = 0;
+		return ret;
+	}
+
+	/* validate received completions */
+	for (int i = 0; i < num_entries_got; i++) {
+		ret = verify_wc(&wc[i], recv, send_cmpl, recv_cmpl);
+	}
+	return ret;
+}
