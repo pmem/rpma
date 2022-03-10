3fc2d80465b04bbbaa110491e2397147f40915b3
Lukasz Dorau <lukasz.dorau@intel.com>
2022-03-10 07:29:20 +0100

    rpma: remove rpma_write_atomic()
    rpma_write_atomic() has been replaced with rpma_atomic_write().

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Removed
+- APIs:
+  - rpma_write_atomic - replaced with rpma_atomic_write
 
 ## [0.13.0] - 2022-03-09
 ### Added
--- src/conn.c
+++ src/conn.c
@@ -361,29 +361,6 @@ rpma_write_with_imm(struct rpma_conn *conn,
 }
 
 /*
- * rpma_write_atomic -- initiate the atomic write operation
- */
-int
-rpma_write_atomic(struct rpma_conn *conn,
-	struct rpma_mr_remote *dst, size_t dst_offset,
-	const struct rpma_mr_local *src,  size_t src_offset,
-	int flags, const void *op_context)
-{
-	if (conn == NULL || dst == NULL || src == NULL || flags == 0)
-		return RPMA_E_INVAL;
-
-	if (dst_offset % RPMA_ATOMIC_WRITE_ALIGNMENT != 0)
-		return RPMA_E_INVAL;
-
-	return rpma_mr_write(conn->id->qp,
-			dst, dst_offset,
-			src, src_offset,
-			RPMA_ATOMIC_WRITE_ALIGNMENT, flags,
-			IBV_WR_RDMA_WRITE, 0,
-			op_context, true);
-}
-
-/*
  * rpma_atomic_write -- initiate the atomic 8 bytes write operation
  */
 int
