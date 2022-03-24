9832727eb5880fd2c25a8c9a79d84a0192692f64
Kacper Stefanski <kacper.stefanski@intel.com>
2022-03-25 00:11:47 +0100

    examples: add libpmem2 to example 03

---
+++ examples/03-read-to-persistent/client_pmem2_map_file.c
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * client_pmem2_map_file.c -- a function to map PMem using libpmem2
+ *
+ * Please see README.md for a detailed description of this example.
+ */
+
+#include <fcntl.h>
+#include <librpma.h>
+#include <libpmem2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "client_pmem_map_file.h"
+#include "common-conn.h"
+
+int
+client_pmem_map_file(char *path, int argc, struct example_mem *mem)
+{
+	int fd = 0;
+	struct pmem2_config *cfg = NULL;
+	struct pmem2_map *map = NULL;
+	struct pmem2_source *src = NULL;
+	pmem2_persist_fn persist;
+
+	if ((fd = open(path, O_RDWR)) < 0) {
+		(void) fprintf(stderr, "cannot open file\n");
+		return -1;
+	}
+
+	if (pmem2_source_from_fd(&src, fd) != 0) {
+		(void) fprintf(stderr,
+			"pmem2_source_from_fd() failed\n");
+		goto err_close;
+	}
+
+	if (pmem2_config_new(&cfg) != 0) {
+		(void) fprintf(stderr,
+			"pmem2_config_new() failed\n");
+		goto err_source_delete;
+	}
+
+	if (pmem2_config_set_required_store_granularity(cfg,
+			PMEM2_GRANULARITY_CACHE_LINE) != 0) {
+		(void) fprintf(stderr,
+			"pmem2_config_set_required_store_granularity() failed: %s\n",
+			pmem2_errormsg());
+		goto err_config_delete;
+	}
+
+	if (pmem2_map_new(&map, cfg, src) != 0) {
+		(void) fprintf(stderr,
+			"pmem2_map_new(%s) failed: %s\n",
+			path, pmem2_errormsg());
+		goto err_config_delete;
+	}
+	mem->map = map;
+	mem->mr_size = pmem2_map_get_size(map);
+	mem->mr_ptr = pmem2_map_get_address(map);
+
+	/*
+	 * Get rid of no longer needed config, source
+	 * and close the file
+	 */
+	pmem2_config_delete(&cfg);
+	pmem2_source_delete(&src);
+	close(fd);
+	/* Get libpmem2 persist function from pmem2_map */
+	persist = pmem2_get_persist_fn(map);
+	mem->persist = persist;
+
+	return 0;
+
+err_config_delete:
+	pmem2_config_delete(&cfg);
+err_source_delete:
+	pmem2_source_delete(&src);
+err_close:
+	close(fd);
+
+	return -1;
+}
+
+void
+client_pmem_unmap_file(struct example_mem *mem)
+{
+	(void) pmem2_map_delete(&mem->map);
+	mem->mr_ptr = NULL;
+}
