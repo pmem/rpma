c25eac7ce728bfbbedebf0193e0185122ac9a2c6
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-04 11:38:29 +0200

    rpma: add shared completion channel

--- src/cq.h
+++ src/cq.h
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2022, Intel Corporation */
 /* Copyright 2021, Fujitsu */
 
 /*
@@ -21,13 +22,32 @@ struct ibv_cq *rpma_cq_get_ibv_cq(const struct rpma_cq *cq);
 
 /*
  * ERRORS
+ * rpma_comp_channel_new() can fail with the following errors:
+ *
+ * - RPMA_E_PROVIDER - ibv_create_comp_channel(3) failed with a provider error
+ */
+int rpma_comp_channel_new(struct ibv_context *ibv_ctx,
+		struct ibv_comp_channel **channel_ptr);
+
+/*
+ * ERRORS
+ * rpma_comp_channel_new() can fail with the following errors:
+ *
+ * - RPMA_E_PROVIDER - ibv_destroy_comp_channel(3) failed with a provider error
+ */
+int rpma_comp_channel_destroy(struct ibv_comp_channel *channel);
+
+/*
+ * ERRORS
  * rpma_cq_new() can fail with the following errors:
  *
  * - RPMA_E_PROVIDER - ibv_create_comp_channel(3), ibv_create_cq(3) or
  * ibv_req_notify_cq(3) failed with a provider error
  * - RPMA_E_NOMEM - out of memory
  */
-int rpma_cq_new(struct ibv_context *ibv_ctx, int cqe, struct rpma_cq **cq_ptr);
+int rpma_cq_new(struct ibv_context *ibv_ctx, int cqe,
+		struct ibv_comp_channel *shared_channel,
+		struct rpma_cq **cq_ptr);
 
 /*
  * ERRORS
