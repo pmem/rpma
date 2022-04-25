caaa45f056e1a25183d430c9a7f71f0e77c8c7fd
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-25 11:13:09 +0200

    examples: add scaffolding of examples with SHared Completion CHannel
    Add a scaffolding of examples with SHared Completion CHannel (SHCCH):
    - example 13 is only an exact copy of example 09,
    - example 14 is only an exact copy of example 12,
    - example 15 is only an exact copy of example 06
    just for purpose of scaffolding.

---
+++ examples/14-receive-completion-queue-SHCCH/receive-completion-queue-common.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * receive-completion-queue-common.c -- a common declarations for the 12 example
+ */
+
+#include <librpma.h>
+#include <inttypes.h>
+
+#include "receive-completion-queue-common.h"
+
+int
+get_wc_and_validate(struct rpma_cq *cq, enum ibv_wc_opcode opcode,
+		char *func_name)
+{
+	struct ibv_wc wc;
+	int ret;
+
+	if ((ret = rpma_cq_wait(cq)))
+			return ret;
+
+	if ((ret = rpma_cq_get_wc(cq, 1, &wc, NULL)))
+			return ret;
+
+	if (wc.status != IBV_WC_SUCCESS) {
+		(void) fprintf(stderr, "%s failed: %s\n",
+			func_name, ibv_wc_status_str(wc.status));
+		return -1;
+	}
+
+	if (wc.opcode != opcode) {
+		(void) fprintf(stderr, "unexpected wc.opcode value "
+			"(0x%" PRIXPTR " != 0x%" PRIXPTR ")\n",
+			(uintptr_t)wc.opcode, (uintptr_t)opcode);
+		return -1;
+	}
+	return 0;
+}
