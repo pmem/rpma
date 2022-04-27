dadc174ed450f626b4e64979cb4813175b791a4f
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-27 15:51:46 +0200

    rpma: ack the connection request event in rpma_conn_req_from_cm_event
    ACK the connection request event and free it
    in rpma_conn_req_from_cm_event() and do not pass it
    to the connection request. Use is_passive instead of edata
    to distinguish between active and passive side.

--- src/conn_req.c
+++ src/conn_req.c
@@ -24,8 +24,8 @@
 #endif
 
 struct rpma_conn_req {
-	/* RDMA_CM_EVENT_CONNECT_REQUEST event (if applicable) */
-	struct rdma_cm_event *edata;
+	/* it is the passive side */
+	int is_passive;
 	/* CM ID of the connection request */
 	struct rdma_cm_id *id;
 	/* main CQ */
@@ -146,7 +146,7 @@ rpma_conn_req_from_id(struct rpma_peer *peer, struct rdma_cm_id *id,
 	}
 #undef GID_STR_LEN
 
-	(*req_ptr)->edata = NULL;
+	(*req_ptr)->is_passive = 0;
 	(*req_ptr)->id = id;
 	(*req_ptr)->cq = cq;
 	(*req_ptr)->rcq = rcq;
@@ -191,15 +191,7 @@ rpma_conn_req_accept(struct rpma_conn_req *req,
 	if (rdma_accept(req->id, conn_param)) {
 		RPMA_LOG_ERROR_WITH_ERRNO(errno, "rdma_accept()");
 		ret = RPMA_E_PROVIDER;
-		(void) rdma_ack_cm_event(req->edata);
 		goto err_conn_req_delete;
-	}
-
-	/* ACK the connection request event */
-	if (rdma_ack_cm_event(req->edata)) {
-		RPMA_LOG_ERROR_WITH_ERRNO(errno, "rdma_ack_cm_event()");
-		ret = RPMA_E_PROVIDER;
-		goto err_conn_disconnect;
 	}
 
 	struct rpma_conn *conn = NULL;
@@ -288,13 +280,6 @@ rpma_conn_req_reject(struct rpma_conn_req *req)
 		}
 	}
 
-	if (rdma_ack_cm_event(req->edata)) {
-		if (!ret) {
-			RPMA_LOG_ERROR_WITH_ERRNO(errno, "rdma_ack_cm_event()");
-			ret = RPMA_E_PROVIDER;
-		}
-	}
-
 	return ret;
 }
 
@@ -337,26 +322,45 @@ rpma_conn_req_from_cm_event(struct rpma_peer *peer,
 		struct rdma_cm_event *edata, const struct rpma_conn_cfg *cfg,
 		struct rpma_conn_req **req_ptr)
 {
-	if (peer == NULL || edata == NULL || req_ptr == NULL)
+	int ret;
+
+	if (edata == NULL)
 		return RPMA_E_INVAL;
 
-	if (edata->event != RDMA_CM_EVENT_CONNECT_REQUEST)
-		return RPMA_E_INVAL;
+	if (peer == NULL || req_ptr == NULL || edata->event != RDMA_CM_EVENT_CONNECT_REQUEST) {
+		ret = RPMA_E_INVAL;
+		goto err_ack_cm_event;
+	}
 
 	struct rpma_conn_req *req = NULL;
-	int ret = rpma_conn_req_from_id(peer, edata->id, cfg, &req);
+	ret = rpma_conn_req_from_id(peer, edata->id, cfg, &req);
 	if (ret)
-		return ret;
+		goto err_ack_cm_event;
 
 	ret = rpma_private_data_store(edata, &req->data);
-	if (ret) {
-		(void) rpma_conn_req_delete(&req);
-		return ret;
+	if (ret)
+		goto err_conn_req_delete;
+
+	/* ACK the connection request event */
+	if (rdma_ack_cm_event(edata)) {
+		RPMA_LOG_ERROR_WITH_ERRNO(errno, "rdma_ack_cm_event()");
+		ret = RPMA_E_PROVIDER;
+		goto err_conn_req_delete;
 	}
-	req->edata = edata;
+
+	req->is_passive = 1;
 	*req_ptr = req;
 
 	return 0;
+
+err_conn_req_delete:
+	(void) rpma_conn_req_delete(&req);
+
+err_ack_cm_event:
+	if (rdma_ack_cm_event(edata))
+		RPMA_LOG_ERROR_WITH_ERRNO(errno, "rdma_ack_cm_event()");
+
+	return ret;
 }
 
 /* public librpma API */
@@ -459,7 +463,7 @@ rpma_conn_req_connect(struct rpma_conn_req **req_ptr,
 
 	int ret = 0;
 
-	if (req->edata)
+	if (req->is_passive)
 		ret = rpma_conn_req_accept(req, &conn_param, conn_ptr);
 	else
 		ret = rpma_conn_req_connect_active(req, &conn_param, conn_ptr);
@@ -489,7 +493,7 @@ rpma_conn_req_delete(struct rpma_conn_req **req_ptr)
 
 	int ret = 0;
 
-	if (req->edata)
+	if (req->is_passive)
 		ret = rpma_conn_req_reject(req);
 	else
 		ret = rpma_conn_req_destroy(req);
--- tests/unit/conn_req/conn_req-connect.c
+++ tests/unit/conn_req/conn_req-connect.c
@@ -133,8 +133,6 @@ connect_via_accept__accept_ERRNO(void **cstate_ptr)
 	/* configure mocks */
 	expect_value(rdma_accept, id, &cstate->id);
 	will_return(rdma_accept, MOCK_ERRNO);
-	expect_value(rdma_ack_cm_event, event, &cstate->event);
-	will_return(rdma_ack_cm_event, MOCK_OK);
 	expect_value(rdma_destroy_qp, id, &cstate->id);
 	expect_value(rpma_cq_delete, *cq_ptr, MOCK_GET_RCQ(cstate));
 	will_return(rpma_cq_delete, MOCK_OK);
@@ -169,89 +167,6 @@ connect_via_accept__accept_ERRNO_subsequent_ERRNO2(void **cstate_ptr)
 	/* configure mocks */
 	expect_value(rdma_accept, id, &cstate->id);
 	will_return(rdma_accept, MOCK_ERRNO); /* first error */
-	expect_value(rdma_ack_cm_event, event, &cstate->event);
-	will_return(rdma_ack_cm_event, MOCK_ERRNO2); /* second error */
-	expect_value(rdma_destroy_qp, id, &cstate->id);
-	expect_value(rpma_cq_delete, *cq_ptr, MOCK_GET_RCQ(cstate));
-	if (cstate->get_cqe.rcq_size) {
-		will_return(rpma_cq_delete, RPMA_E_PROVIDER);
-		will_return(rpma_cq_delete, MOCK_ERRNO2); /* third error */
-	} else {
-		/* rcq == NULL cannot fail */
-		will_return(rpma_cq_delete, MOCK_OK);
-	}
-	expect_value(rpma_cq_delete, *cq_ptr, MOCK_RPMA_CQ);
-	will_return(rpma_cq_delete, RPMA_E_PROVIDER);
-	will_return(rpma_cq_delete, MOCK_ERRNO2); /* third or fourth error */
-	if (cstate->get_cqe.shared)
-		will_return(ibv_destroy_comp_channel, MOCK_OK);
-
-	/* run test */
-	struct rpma_conn *conn = NULL;
-	int ret = rpma_conn_req_connect(&cstate->req, NULL, &conn);
-
-	/* verify the results */
-	assert_int_equal(ret, RPMA_E_PROVIDER);
-	assert_null(cstate->req);
-	assert_null(conn);
-}
-
-/*
- * connect_via_accept__ack_ERRNO -- rdma_ack_cm_event() fails with MOCK_ERRNO
- */
-static void
-connect_via_accept__ack_ERRNO(void **cstate_ptr)
-{
-	/* WA for cmocka/issues#47 */
-	struct conn_req_test_state *cstate = *cstate_ptr;
-	assert_int_equal(setup__conn_req_from_cm_event((void **)&cstate), 0);
-	assert_non_null(cstate);
-
-	/* configure mocks */
-	expect_value(rdma_accept, id, &cstate->id);
-	will_return(rdma_accept, MOCK_OK);
-	expect_value(rdma_ack_cm_event, event, &cstate->event);
-	will_return(rdma_ack_cm_event, MOCK_ERRNO);
-	expect_value(rdma_disconnect, id, &cstate->id);
-	will_return(rdma_disconnect, MOCK_OK);
-	expect_value(rdma_destroy_qp, id, &cstate->id);
-	expect_value(rpma_cq_delete, *cq_ptr, MOCK_GET_RCQ(cstate));
-	will_return(rpma_cq_delete, MOCK_OK);
-	expect_value(rpma_cq_delete, *cq_ptr, MOCK_RPMA_CQ);
-	will_return(rpma_cq_delete, MOCK_OK);
-	if (cstate->get_cqe.shared)
-		will_return(ibv_destroy_comp_channel, MOCK_OK);
-
-	/* run test */
-	struct rpma_conn *conn = NULL;
-	int ret = rpma_conn_req_connect(&cstate->req, NULL, &conn);
-
-	/* verify the results */
-	assert_int_equal(ret, RPMA_E_PROVIDER);
-	assert_null(cstate->req);
-	assert_null(conn);
-}
-
-/*
- * connect_via_accept__ack_ERRNO_subsequent_ERRNO2 -- rdma_ack_cm_event()
- * fails with MOCK_ERRNO whereas subsequent (rdma_disconnect(),
- * rpma_cq_delete(&req->rcq), rpma_cq_delete(&req->cq)) fail with MOCK_ERRNO2
- */
-static void
-connect_via_accept__ack_ERRNO_subsequent_ERRNO2(void **cstate_ptr)
-{
-	/* WA for cmocka/issues#47 */
-	struct conn_req_test_state *cstate = *cstate_ptr;
-	assert_int_equal(setup__conn_req_from_cm_event((void **)&cstate), 0);
-	assert_non_null(cstate);
-
-	/* configure mocks */
-	expect_value(rdma_accept, id, &cstate->id);
-	will_return(rdma_accept, MOCK_OK);
-	expect_value(rdma_ack_cm_event, event, &cstate->event);
-	will_return(rdma_ack_cm_event, MOCK_ERRNO); /* first error */
-	expect_value(rdma_disconnect, id, &cstate->id);
-	will_return(rdma_disconnect, MOCK_ERRNO2); /* second error */
 	expect_value(rdma_destroy_qp, id, &cstate->id);
 	expect_value(rpma_cq_delete, *cq_ptr, MOCK_GET_RCQ(cstate));
 	if (cstate->get_cqe.rcq_size) {
@@ -292,8 +207,6 @@ connect_via_accept__conn_new_ERRNO(void **cstate_ptr)
 	/* configure mocks */
 	expect_value(rdma_accept, id, &cstate->id);
 	will_return(rdma_accept, MOCK_OK);
-	expect_value(rdma_ack_cm_event, event, &cstate->event);
-	will_return(rdma_ack_cm_event, MOCK_OK);
 	expect_value(rpma_conn_new, id, &cstate->id);
 	expect_value(rpma_conn_new, rcq, MOCK_GET_RCQ(cstate));
 	expect_value(rpma_conn_new, channel, MOCK_GET_CHANNEL(cstate));
@@ -336,8 +249,6 @@ connect_via_accept__conn_new_ERRNO_subsequent_ERRNO2(void **cstate_ptr)
 	/* configure mocks */
 	expect_value(rdma_accept, id, &cstate->id);
 	will_return(rdma_accept, MOCK_OK);
-	expect_value(rdma_ack_cm_event, event, &cstate->event);
-	will_return(rdma_ack_cm_event, MOCK_OK);
 	expect_value(rpma_conn_new, id, &cstate->id);
 	expect_value(rpma_conn_new, rcq, MOCK_GET_RCQ(cstate));
 	expect_value(rpma_conn_new, channel, MOCK_GET_CHANNEL(cstate));
@@ -386,8 +297,6 @@ connect_via_accept__success_incoming(void **cstate_ptr)
 	/* configure mocks */
 	expect_value(rdma_accept, id, &cstate->id);
 	will_return(rdma_accept, MOCK_OK);
-	expect_value(rdma_ack_cm_event, event, &cstate->event);
-	will_return(rdma_ack_cm_event, MOCK_OK);
 	expect_value(rpma_conn_new, id, &cstate->id);
 	expect_value(rpma_conn_new, rcq, MOCK_GET_RCQ(cstate));
 	expect_value(rpma_conn_new, channel, MOCK_GET_CHANNEL(cstate));
@@ -613,9 +522,6 @@ static const struct CMUnitTest test_connect[] = {
 	CONN_REQ_TEST_WITH_AND_WITHOUT_RCQ(connect_via_accept__accept_ERRNO),
 	CONN_REQ_TEST_WITH_AND_WITHOUT_RCQ(
 		connect_via_accept__accept_ERRNO_subsequent_ERRNO2),
-	CONN_REQ_TEST_WITH_AND_WITHOUT_RCQ(connect_via_accept__ack_ERRNO),
-	CONN_REQ_TEST_WITH_AND_WITHOUT_RCQ(
-		connect_via_accept__ack_ERRNO_subsequent_ERRNO2),
 	CONN_REQ_TEST_WITH_AND_WITHOUT_RCQ(connect_via_accept__conn_new_ERRNO),
 	CONN_REQ_TEST_WITH_AND_WITHOUT_RCQ(
 		connect_via_accept__conn_new_ERRNO_subsequent_ERRNO2),
