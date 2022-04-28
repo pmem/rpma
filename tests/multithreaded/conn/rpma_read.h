13e1853200f5161239babd202ea890bba9f755e3
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-28 22:58:34 +0200

    test: add rpma_read MT test

---
+++ tests/multithreaded/conn/rpma_read.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2021-2022, Intel Corporation */
+
+/*
+ * rpma_read.h -- common definition for rpma_read multithreaded test
+ */
+
+#ifndef MTT_RPMA_READ
+#define MTT_RPMA_READ
+
+#define HELLO_STR "Hello client!"
+#define MAX_STR_LEN (strlen(HELLO_STR) + 1)
+
+#endif /* MTT_RPMA_READ */
