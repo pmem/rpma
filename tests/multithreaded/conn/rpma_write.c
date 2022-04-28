7ad1cfae9e62393329694891d96a7faf45a27fc7
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-28 23:06:04 +0200

    test: add rpma_write MT test

---
+++ tests/multithreaded/conn/rpma_write.c
@@ -0,0 +1,259 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * rpma_write.c -- rpma_write multithreaded test
+ */
+
+#include <stdlib.h>
+#include <librpma.h>
+
+#include "mtt.h"
+#include "mtt_connect.h"
+#include "rpma_write.h"
+
+/* the client's part */
+
+struct prestate {
+	char *addr;
+	unsigned port;
+	struct rpma_peer *peer;
+};
+
+struct thread_state {
+	struct rpma_conn *conn;
+	struct rpma_cq *cq;
+
+	void *local_ptr;
+	struct rpma_mr_local *mr_local_ptr;
+	struct rpma_mr_remote *mr_remote_ptr;
+	size_t local_mr_size;
+	size_t mr_remote_size;
+
+	/* the expected value of the private data */
+	struct rpma_conn_private_data pdata;
+};
+
+static void
+prestate_init(void *prestate, struct mtt_result *tr)
+{
+	struct prestate *pr = (struct prestate *)prestate;
+
+	(void) mtt_client_peer_new(tr, pr->addr, &pr->peer);
+}
+
+static void
+thread_seq_init(unsigned id, void *prestate, void **state_ptr, struct mtt_result *tr)
+{
+	struct prestate *pr = (struct prestate *)prestate;
+
+	/* allocate a thread state */
+	struct thread_state *ts = mtt_malloc_aligned(sizeof(struct thread_state), tr);
+	/* save the thread state */
+	*state_ptr = ts;
+	if (ts == NULL)
+		return;
+
+	/* allocate a memory for local MR */
+	ts->local_mr_size = MAX_STR_LEN;
+	ts->local_ptr = mtt_malloc_aligned(ts->local_mr_size, tr);
+	if (ts->local_ptr == NULL)
+		goto err_free;
+
+	/* fill the source memory with a content */
+	memcpy(ts->local_ptr, HELLO_STR, ts->local_mr_size);
+
+	int ret = mtt_client_connect(tr, pr->addr, pr->port, pr->peer, &ts->conn, &ts->pdata);
+	if (ret)
+		goto err_free;
+
+	/* register the memory */
+	ret = rpma_mr_reg(pr->peer, ts->local_ptr, ts->local_mr_size, RPMA_MR_USAGE_WRITE_SRC,
+				&ts->mr_local_ptr);
+	if (ret) {
+		MTT_RPMA_ERR(tr, "rpma_mr_reg", ret);
+		goto err_conn_disconnect;
+	};
+
+	/*
+	 * Create a remote memory registration structure from the received
+	 * descriptor.
+	 */
+	struct common_data *dst_data = ts->pdata.ptr;
+	ret = rpma_mr_remote_from_descriptor(&dst_data->descriptors[0], dst_data->mr_desc_size,
+			&ts->mr_remote_ptr);
+	if (ret) {
+		MTT_RPMA_ERR(tr, "rpma_mr_remote_from_descriptor", ret);
+		goto err_mr_dereg;
+	};
+
+	ret = rpma_mr_remote_get_size(ts->mr_remote_ptr, &ts->mr_remote_size);
+	if (ret) {
+		MTT_RPMA_ERR(tr, "rpma_mr_remote_get_size", ret);
+		goto err_mr_remote_delete;
+	};
+
+	/* get the connection's main CQ */
+	ret = rpma_conn_get_cq(ts->conn, &ts->cq);
+	if (ret)
+		goto err_mr_remote_delete;
+
+	return;
+
+err_mr_remote_delete:
+	/* delete the remote memory region's structure */
+	(void) rpma_mr_remote_delete(&ts->mr_remote_ptr);
+
+err_mr_dereg:
+	/* deregister the memory region */
+	(void) rpma_mr_dereg(&ts->mr_local_ptr);
+
+err_conn_disconnect:
+	mtt_client_err_disconnect(&ts->conn);
+
+err_free:
+	free(ts->local_ptr);
+	free(ts);
+}
+
+/*
+ * thread -- write data
+ */
+static void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *result)
+{
+	struct thread_state *ts = (struct thread_state *)state;
+	struct ibv_wc wc;
+
+	/* post an RDMA write operation */
+	int ret = rpma_write(ts->conn, ts->mr_remote_ptr, 0, ts->mr_local_ptr, 0,
+			ts->mr_remote_size, RPMA_F_COMPLETION_ALWAYS, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_write() failed", ret);
+		return;
+	}
+
+	/* wait for the completion to be ready */
+	ret = rpma_cq_wait(ts->cq);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_wait() failed", ret);
+		return;
+	}
+
+	/* wait for a completion of the RDMA read */
+	ret = rpma_cq_get_wc(ts->cq, 1, &wc, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_get_wc() failed", ret);
+		return;
+	}
+
+	if (wc.status != IBV_WC_SUCCESS) {
+		MTT_ERR_MSG(result, "rpma_write() failed", -1);
+		return;
+	}
+
+	if (wc.opcode != IBV_WC_RDMA_WRITE) {
+		MTT_ERR_MSG(result, "unexpected wc.opcode value", -1);
+		return;
+	}
+}
+
+/*
+ * thread_seq_fini -- deregister and free the memory region, disconnect and delete the peer object
+ */
+static void
+thread_seq_fini(unsigned id, void *prestate, void **state_ptr, struct mtt_result *tr)
+{
+	struct thread_state *ts = (struct thread_state *)(*state_ptr);
+	enum rpma_conn_event conn_event = RPMA_CONN_UNDEFINED;
+	int ret;
+
+	if ((ret = rpma_conn_disconnect(ts->conn))) {
+		MTT_RPMA_ERR(tr, "rpma_conn_disconnect", ret);
+	} else {
+		/* wait for the connection to be closed */
+		if ((ret = rpma_conn_next_event(ts->conn, &conn_event)))
+			MTT_RPMA_ERR(tr, "rpma_conn_next_event", ret);
+		else if (conn_event != RPMA_CONN_CLOSED)
+			MTT_ERR_MSG(tr, "rpma_conn_next_event returned an unexpected event", -1);
+	}
+
+	if ((ret = rpma_conn_delete(&ts->conn)))
+		MTT_RPMA_ERR(tr, "rpma_conn_delete", ret);
+
+	/* delete the remote memory region's structure */
+	if ((ret = rpma_mr_remote_delete(&ts->mr_remote_ptr)))
+		MTT_RPMA_ERR(tr, "rpma_mr_remote_delete", ret);
+
+	/* deregister the memory region */
+	if ((ret = rpma_mr_dereg(&ts->mr_local_ptr)))
+		MTT_RPMA_ERR(tr, "rpma_mr_dereg", ret);
+
+	free(ts->local_ptr);
+	free(ts);
+}
+
+/*
+ * prestate_fini -- deregister and free the memory region, disconnect and delete the peer object
+ */
+static void
+prestate_fini(void *prestate, struct mtt_result *tr)
+{
+	struct prestate *pr = (struct prestate *)prestate;
+	int ret;
+
+	if ((ret = rpma_peer_delete(&pr->peer)))
+		MTT_RPMA_ERR(tr, "rpma_peer_delete", ret);
+}
+
+/* the server's part */
+
+struct server_prestate {
+	char *addr;
+	unsigned port;
+
+	/* the expected value of the private data */
+	struct rpma_conn_private_data pdata;
+};
+
+/*
+ * server_main -- the main function of the server
+ */
+int server_main(char *addr, unsigned port);
+
+/*
+ * server_func -- the server function of this test
+ */
+int
+server_func(void *prestate)
+{
+	struct server_prestate *pst = (struct server_prestate *)prestate;
+	return server_main(pst->addr, pst->port);
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct mtt_args args = {0};
+
+	if (mtt_parse_args(argc, argv, &args))
+		return -1;
+
+	struct prestate client_prestate = {args.addr, args.port};
+	struct server_prestate server_prestate = {args.addr, args.port};
+
+	struct mtt_test test = {
+			&client_prestate,
+			prestate_init,
+			thread_seq_init,
+			NULL,
+			thread,
+			NULL,
+			thread_seq_fini,
+			prestate_fini,
+			server_func,
+			&server_prestate
+	};
+
+	return mtt_run(&test, args.threads_num);
+}
---
+++ tests/multithreaded/conn/rpma_write.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2021-2022, Intel Corporation */
+
+/*
+ * rpma_write.h -- common definition for rpma_write multithreaded test
+ */
+
+#ifndef MTT_RPMA_WRITE
+#define MTT_RPMA_WRITE
+
+#define HELLO_STR "Hello client!"
+#define MAX_STR_LEN (strlen(HELLO_STR) + 1)
+
+#endif /* MTT_RPMA_WRITE */
