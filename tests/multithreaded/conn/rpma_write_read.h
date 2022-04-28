b92af9a3690c6327a5f1c2bcce0343178db91272
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-28 23:29:05 +0200

    test: add rpma_write with rpma_read verification MT test

---
+++ tests/multithreaded/conn/rpma_write_read.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2021-2022, Intel Corporation */
+
+/*
+ * rpma_write_read.h -- common definition for rpma_write with rpma_read verification MT test
+ */
+
+#ifndef MTT_RPMA_WRITE_READ
+#define MTT_RPMA_WRITE_READ
+
+#define HELLO_STR "Hello client!"
+#define MAX_STR_LEN (strlen(HELLO_STR) + 1)
+
+#endif /* MTT_RPMA_WRITE_READ */
