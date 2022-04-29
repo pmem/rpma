1418e67ffea06be432ad7de728e694799d2d4a6d
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-29 09:45:51 +0200

    test: add one rpma_read and two rpma_write MT tests

--- tests/multithreaded/run_group.cmake
+++ tests/multithreaded/run_group.cmake
@@ -1,13 +1,15 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2021, Intel Corporation
+# Copyright 2021-2022, Intel Corporation
 #
 
 include(${SRC_DIR}/../../cmake/helpers.cmake)
 
 setup()
 
-set(THREADS 32)
+# XXX change ulimits to enable 32 threads
+# set(THREADS 32)
+set(THREADS 7)
 
 if("$ENV{RPMA_SOFT_ROCE_IP}" STREQUAL "")
 	set(SOFT_ROCE_IP "127.0.0.1")
