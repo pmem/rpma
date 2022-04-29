9b7937568e8802369a1eb3cd6c5ec17c50a07b15
Lukasz Dorau <lukasz.dorau@intel.com>
2022-04-29 11:01:55 +0200

    test: add one rpma_read and two rpma_write MT tests

---
+++ tests/multithreaded/conn/rpma_write.c
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * rpma_write.c -- rpma_write multithreaded test
+ */
+
+#include <librpma.h>
+#include "mtt.h"
+#include "rpma_common.h"
+
+/*
+ * thread -- main function of rpma_write multithreaded test
+ */
+void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *result)
+{
+	struct thread_state *ts = (struct thread_state *)state;
+	struct ibv_wc wc;
+
+	/* post an RDMA write operation */
+	int ret = rpma_write(ts->conn, ts->mr_remote_ptr, 0, ts->mr_local_ptr, 0,
+			ts->mr_remote_size, RPMA_F_COMPLETION_ALWAYS, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_write() failed", ret);
+		return;
+	}
+
+	/* wait for the completion to be ready */
+	ret = rpma_cq_wait(ts->cq);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_wait() failed", ret);
+		return;
+	}
+
+	/* wait for a completion of the RDMA read */
+	ret = rpma_cq_get_wc(ts->cq, 1, &wc, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_get_wc() failed", ret);
+		return;
+	}
+
+	if (wc.status != IBV_WC_SUCCESS) {
+		MTT_ERR_MSG(result, "rpma_write() failed", -1);
+		return;
+	}
+
+	if (wc.opcode != IBV_WC_RDMA_WRITE) {
+		MTT_ERR_MSG(result, "unexpected wc.opcode value", -1);
+		return;
+	}
+}
