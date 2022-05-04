2d5d84b123a4cc5ef2b59370732c8555f70ecfdb
Lukasz Dorau <lukasz.dorau@intel.com>
2022-05-04 15:48:28 +0200

    test: add one rpma_read and two rpma_write MT tests

---
+++ tests/multithreaded/conn/server_rpma_empty.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020-2022, Intel Corporation */
+
+/*
+ * server_rpma_empty.c -- empty implementation of test-specific functions for common server
+ *                        of multi-connection MT tests
+ */
+
+#include "server_rpma_common.h"
+
+/*
+ * client_completion_event_handle -- empty implementation of callback on completion is ready
+ */
+void
+client_completion_event_handle(struct custom_event *ce)
+{
+}
+
+/*
+ * client_is_ready_handle -- empty implementation of callback on connection is established
+ */
+void
+client_is_ready_handle(struct client_res *clnt)
+{
+}
