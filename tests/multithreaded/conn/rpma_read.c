7f399b43c43d47d89bc484b7e9bffb094e2ef299
Lukasz Dorau <lukasz.dorau@intel.com>
2022-05-04 12:10:55 +0200

    test: add one rpma_read and two rpma_write MT tests

---
+++ tests/multithreaded/conn/rpma_read.c
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * rpma_read.c -- rpma_read multithreaded test
+ */
+
+#include <librpma.h>
+#include "mtt.h"
+#include "rpma_common.h"
+
+/*
+ * thread -- main function of rpma_read multithreaded test (read and verify the data)
+ */
+void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *result)
+{
+	struct thread_state *ts = (struct thread_state *)state;
+
+	/* zero the destination memory */
+	memset(ts->local_ptr, 0, ts->mr_local_size);
+
+	/* post an RDMA read operation */
+	int ret = rpma_read(ts->conn, ts->mr_local_ptr, 0, ts->mr_remote_ptr, 0, ts->mr_remote_size,
+			RPMA_F_COMPLETION_ALWAYS, (void *)WR_ID_READ);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_read() failed", ret);
+		return;
+	}
+
+	ret = wait_and_validate_completion(ts->cq, IBV_WC_RDMA_READ, WR_ID_READ, result);
+	if (ret)
+		return;
+
+	if (memcmp(ts->local_ptr, STRING_TO_READ, LEN_STRING_TO_READ) != 0)
+		MTT_ERR_MSG(result, "read string mismatch", -1);
+}
---
+++ tests/multithreaded/conn/rpma_write_read.c
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * rpma_write_read.c -- rpma_write with rpma_read verification multithreaded test
+ */
+
+#include <librpma.h>
+#include "mtt.h"
+#include "rpma_common.h"
+
+/*
+ * thread -- main function of rpma_write with rpma_read verification MT test
+ *           (write and verify the data using rpma_read)
+ */
+void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *result)
+{
+	struct thread_state *ts = (struct thread_state *)state;
+
+	/* post an RDMA write operation */
+	int ret = rpma_write(ts->conn, ts->mr_remote_ptr, 0, ts->mr_local_ptr, 0,
+				ts->mr_remote_size, RPMA_F_COMPLETION_ALWAYS, (void *)WR_ID_WRITE);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_write() failed", ret);
+		return;
+	}
+
+	ret = wait_and_validate_completion(ts->cq, IBV_WC_RDMA_WRITE, WR_ID_WRITE, result);
+	if (ret)
+		return;
+
+	/* zero the local memory as the destination for reading */
+	memset(ts->local_ptr, 0, ts->mr_local_size);
+
+	/* post an RDMA read operation */
+	ret = rpma_read(ts->conn, ts->mr_local_ptr, 0, ts->mr_remote_ptr, 0,
+				ts->mr_remote_size, RPMA_F_COMPLETION_ALWAYS, (void *)WR_ID_READ);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_read() failed", ret);
+		return;
+	}
+
+	ret = wait_and_validate_completion(ts->cq, IBV_WC_RDMA_READ, WR_ID_READ, result);
+	if (ret)
+		return;
+
+	if (memcmp(ts->local_ptr, STRING_TO_WRITE, LEN_STRING_TO_WRITE) != 0)
+		MTT_ERR_MSG(result, "write string mismatch", -1);
+}
