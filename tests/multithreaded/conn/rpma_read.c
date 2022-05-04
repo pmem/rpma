1f73c6873c3410497d94d818245efc44d767e3cc
Lukasz Dorau <lukasz.dorau@intel.com>
2022-05-04 10:05:31 +0200

    test: add one rpma_read and two rpma_write MT tests

---
+++ tests/multithreaded/conn/rpma_read.c
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * rpma_read.c -- rpma_read multithreaded test
+ */
+
+#include <librpma.h>
+#include "mtt.h"
+#include "rpma_common.h"
+
+/*
+ * thread -- main function of rpma_read multithreaded test (read and verify the data)
+ */
+void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *result)
+{
+	struct thread_state *ts = (struct thread_state *)state;
+	struct ibv_wc wc;
+
+	/* zero the destination memory */
+	memset(ts->local_ptr, 0, ts->mr_local_size);
+
+	/* post an RDMA read operation */
+	int ret = rpma_read(ts->conn, ts->mr_local_ptr, 0, ts->mr_remote_ptr, 0, ts->mr_remote_size,
+			RPMA_F_COMPLETION_ALWAYS, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_read() failed", ret);
+		return;
+	}
+
+	/* wait for the completion to be ready */
+	ret = rpma_cq_wait(ts->cq);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_wait() failed", ret);
+		return;
+	}
+
+	/* get a completion of the RDMA read */
+	ret = rpma_cq_get_wc(ts->cq, 1, &wc, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_get_wc() failed", ret);
+		return;
+	}
+
+	if (wc.status != IBV_WC_SUCCESS) {
+		MTT_ERR_MSG(result, "completion status is different from IBV_WC_SUCCESS", -1);
+		return;
+	}
+
+	if (wc.opcode != IBV_WC_RDMA_READ) {
+		MTT_ERR_MSG(result, "unexpected wc.opcode value", -1);
+		return;
+	}
+
+	if (memcmp(ts->local_ptr, STRING_TO_READ, LEN_STRING_TO_READ) != 0)
+		MTT_ERR_MSG(result, "read string mismatch", -1);
+}
---
+++ tests/multithreaded/conn/rpma_write_read.c
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * rpma_write_read.c -- rpma_write with rpma_read verification multithreaded test
+ */
+
+#include <librpma.h>
+#include "mtt.h"
+#include "rpma_common.h"
+
+/*
+ * thread -- main function of rpma_write with rpma_read verification MT test
+ *           (write and verify the data using rpma_read)
+ */
+void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *result)
+{
+	struct thread_state *ts = (struct thread_state *)state;
+	struct ibv_wc wc;
+
+	/* post an RDMA write operation */
+	int ret = rpma_write(ts->conn, ts->mr_remote_ptr, 0, ts->mr_local_ptr, 0,
+				ts->mr_remote_size, RPMA_F_COMPLETION_ALWAYS, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_write() failed", ret);
+		return;
+	}
+
+	/* wait for the completion to be ready */
+	ret = rpma_cq_wait(ts->cq);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_wait() failed", ret);
+		return;
+	}
+
+	/* get a completion of the RDMA write */
+	ret = rpma_cq_get_wc(ts->cq, 1, &wc, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_get_wc() failed", ret);
+		return;
+	}
+
+	if (wc.status != IBV_WC_SUCCESS) {
+		MTT_ERR_MSG(result, "completion status is different from IBV_WC_SUCCESS", -1);
+		return;
+	}
+
+	if (wc.opcode != IBV_WC_RDMA_WRITE) {
+		MTT_ERR_MSG(result, "unexpected wc.opcode value", -1);
+		return;
+	}
+
+	/* zero the local memory as the destination for reading */
+	memset(ts->local_ptr, 0, ts->mr_local_size);
+
+	/* post an RDMA read operation */
+	ret = rpma_read(ts->conn, ts->mr_local_ptr, 0, ts->mr_remote_ptr, 0,
+				ts->mr_remote_size, RPMA_F_COMPLETION_ALWAYS, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_read() failed", ret);
+		return;
+	}
+
+	/* wait for the completion to be ready */
+	ret = rpma_cq_wait(ts->cq);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_wait() failed", ret);
+		return;
+	}
+
+	/* get a completion of the RDMA read */
+	ret = rpma_cq_get_wc(ts->cq, 1, &wc, NULL);
+	if (ret) {
+		MTT_ERR_MSG(result, "rpma_cq_get_wc() failed", ret);
+		return;
+	}
+
+	if (wc.status != IBV_WC_SUCCESS) {
+		MTT_ERR_MSG(result, "completion status is different from IBV_WC_SUCCESS", -1);
+		return;
+	}
+
+	if (wc.opcode != IBV_WC_RDMA_READ) {
+		MTT_ERR_MSG(result, "unexpected wc.opcode value", -1);
+		return;
+	}
+
+	if (memcmp(ts->local_ptr, STRING_TO_WRITE, LEN_STRING_TO_WRITE) != 0)
+		MTT_ERR_MSG(result, "write string mismatch", -1);
+}
