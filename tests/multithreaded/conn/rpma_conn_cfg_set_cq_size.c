ed93bd07a47177389abc8779bd731d438c21640e
Tomasz Gromadzki <grom72@o2.pl>
2022-05-26 12:37:28 +0200

    rpma: make rpma_conn_cfg_set_cq_size thread-safe

--- tests/multithreaded/conn/rpma_conn_cfg_set_cq_size.c
+++ tests/multithreaded/conn/rpma_conn_cfg_set_cq_size.c
@@ -11,6 +11,8 @@
 #include "mtt.h"
 #include "rpma_conn_cfg_common.h"
 
+struct rpma_conn_cfg_common_prestate prestate = {NULL};
+
 /*
  * thread -- set connection establishment cq size and check if its value is
  * as expected
@@ -18,24 +20,23 @@
 static void
 thread(unsigned id, void *prestate, void *state, struct mtt_result *tr)
 {
-	struct rpma_conn_cfg_common_state *st =
-		(struct rpma_conn_cfg_common_state *)state;
-	uint32_t cq_size;
+	struct rpma_conn_cfg_common_prestate *pr = (struct rpma_conn_cfg_common_prestate *)prestate;
+
+	uint32_t cq_size = 0;
 	int ret;
 
-	if ((ret = rpma_conn_cfg_set_cq_size(st->cfg_ptr,
-				RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
+	if ((ret = rpma_conn_cfg_set_cq_size(pr->cfg_ptr, RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
 		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_cq_size", ret);
 		return;
 	}
 
-	if ((ret = rpma_conn_cfg_get_cq_size(st->cfg_ptr, &cq_size))) {
+	if ((ret = rpma_conn_cfg_get_cq_size(pr->cfg_ptr, &cq_size))) {
 		MTT_RPMA_ERR(tr, "rpma_conn_cfg_get_cq_size", ret);
 		return;
 	}
 
 	if (cq_size != RPMA_CONN_CFG_COMMON_Q_SIZE_EXP)
-		MTT_ERR(tr, "cq_size != Q_SIZE_EXP", EINVAL);
+		MTT_ERR_MSG(tr, "Invalid cq_size: %d instead of %d", -1, cq_size, id);
 }
 
 int
@@ -47,14 +48,14 @@ main(int argc, char *argv[])
 		return -1;
 
 	struct mtt_test test = {
-			NULL,
-			NULL,
+			&prestate,
+			rpma_conn_cfg_common_prestate_init,
 			NULL,
 			rpma_conn_cfg_common_init,
 			thread,
 			rpma_conn_cfg_common_fini,
 			NULL,
-			NULL
+			rpma_conn_cfg_common_prestate_fini
 	};
 
 	return mtt_run(&test, args.threads_num);
