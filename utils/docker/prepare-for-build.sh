8130cb16690c67d84c2d370a48ca6a19211816dd
Lukasz Dorau <lukasz.dorau@intel.com>
2022-05-30 09:17:16 +0200

    common: add comment to prepare-for-build.sh
    Add a comment to prepare-for-build.sh missed in 69ac575.
    
    Ref: 69ac575

--- utils/docker/prepare-for-build.sh
+++ utils/docker/prepare-for-build.sh
@@ -16,9 +16,9 @@ function sudo_password() {
 
 # this should be run only on CIs
 if [ "$CI_RUN" == "YES" ]; then
-	set -x
 	echo WORKDIR=$WORKDIR
 	sudo_password chown -R $(id -u).$(id -g) $WORKDIR
+	# fix for: https://github.com/actions/checkout/issues/766 (git CVE-2022-24765)
 	git config --global --add safe.directory "$WORKDIR"
 	sudo_password git config --global --add safe.directory "$WORKDIR"
 fi
