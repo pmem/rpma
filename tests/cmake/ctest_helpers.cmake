54d5f10410e9018f24cfc269125e0f2060fcb9d9
Lukasz Dorau <lukasz.dorau@intel.com>
2022-06-24 08:44:01 +0200

    test: add a notice when TESTS_USE_VALGRIND is OFF

--- tests/cmake/ctest_helpers.cmake
+++ tests/cmake/ctest_helpers.cmake
@@ -53,7 +53,10 @@ function(find_packages)
 	endif()
 
 	if(TESTS_USE_VALGRIND AND NOT VALGRIND_FOUND)
-		message(WARNING "Valgrind not found. Valgrind tests will not be performed.")
+		message(WARNING "Valgrind not found. Valgrind tests will not be run.")
+	endif()
+	if(VALGRIND_FOUND AND NOT TESTS_USE_VALGRIND)
+		message(STATUS "NOTICE: valgrind tests (drd/helgrind/memcheck) will not be run (TESTS_USE_VALGRIND is OFF).")
 	endif()
 endfunction()
 
