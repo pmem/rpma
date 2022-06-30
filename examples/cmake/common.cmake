4a18687de9a483dd100586e5ce653ec3a09e9f5d
Kacper Stefanski <kacper.stefanski@intel.com>
2022-06-30 23:54:00 +0200

    examples: add common-pmem_map_file and signature_check to example09

--- examples/cmake/common.cmake
+++ examples/cmake/common.cmake
@@ -6,8 +6,19 @@
 cmake_minimum_required(VERSION 3.3)
 
 function(add_example_with_pmem name)
+	set(options USE_LIBPROTOBUFC)
 	set(srcs ${ARGN})
 	set(srcs ${srcs} ../common/common-hello.c)
+
+	cmake_parse_arguments(EXAMPLE
+		"${options}"
+		${ARGN})
+
+	if (EXAMPLE_USE_LIBPROTOBUFC AND NOT LIBPROTOBUFC_FOUND)
+		message(STATUS "${target} skipped - no libprotobuf-c found")
+		return()
+	endif()
+
 	if (LIBPMEM2_FOUND)
 		set(srcs ${srcs} ../common/common-pmem2_map_file.c ../common/common-map_file_with_signature_check.c)
 	elseif (LIBPMEM_FOUND)
@@ -32,5 +43,11 @@ function(add_example_with_pmem name)
 		target_link_libraries(${name} ${LIBPMEM_LIBRARIES})
 		target_compile_definitions(${name}
 			PRIVATE USE_LIBPMEM)
+	endif()
+
+	if(EXAMPLE_USE_LIBPROTOBUFC)
+		target_include_directories(${name}
+			PRIVATE ${LIBPROTOBUFC_INCLUDE_DIRS})
+		target_link_libraries(${name} ${LIBPROTOBUFC_LIBRARIES})
 	endif()
 endfunction()
