e2329c47e5e191c7c5cdc13649ecc66f0dc98652
Kacper Stefanski <kacper.stefanski@intel.com>
2022-07-08 10:35:09 +0200

    rpma: add missing mt test for compl_channel and rcq_size

--- tests/multithreaded/conn_cfg/rpma_conn_cfg_common.c
+++ tests/multithreaded/conn_cfg/rpma_conn_cfg_common.c
@@ -21,33 +21,42 @@ rpma_conn_cfg_common_prestate_init(void *prestate, struct mtt_result *tr)
 	struct rpma_conn_cfg_common_prestate *pr =
 		(struct rpma_conn_cfg_common_prestate *)prestate;
 	int ret;
+	bool shared = true;
 
 	if ((ret = rpma_conn_cfg_new(&pr->cfg_ptr))) {
 		MTT_RPMA_ERR(tr, "rpma_conn_cfg_new", ret);
 		return;
 	}
 
-	if ((ret = rpma_conn_cfg_set_cq_size(pr->cfg_ptr,
-				RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
+	if ((ret = rpma_conn_cfg_set_compl_channel(pr->cfg_ptr, shared))) {
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_compl_channel", ret);
+		return;
+	}
+
+	if ((ret = rpma_conn_cfg_set_cq_size(pr->cfg_ptr, RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
 		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_cq_size", ret);
 		return;
 	}
 
-	if ((ret = rpma_conn_cfg_set_sq_size(pr->cfg_ptr,
-				RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
+	if ((ret = rpma_conn_cfg_set_sq_size(pr->cfg_ptr, RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
 		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_sq_size", ret);
 		return;
 	}
 
-	if ((ret = rpma_conn_cfg_set_rq_size(pr->cfg_ptr,
-				RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
+	if ((ret = rpma_conn_cfg_set_rcq_size(pr->cfg_ptr, RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
+		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_rcq_size", ret);
+		return;
+	}
+
+	if ((ret = rpma_conn_cfg_set_rq_size(pr->cfg_ptr, RPMA_CONN_CFG_COMMON_Q_SIZE_EXP))) {
 		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_rq_size", ret);
 		return;
 	}
 
-	if ((ret = rpma_conn_cfg_set_timeout(pr->cfg_ptr,
-				RPMA_CONN_CFG_COMMON_TIMEOUT_MS_EXP)))
+	if ((ret = rpma_conn_cfg_set_timeout(pr->cfg_ptr, RPMA_CONN_CFG_COMMON_TIMEOUT_MS_EXP))) {
 		MTT_RPMA_ERR(tr, "rpma_conn_cfg_set_timeout", ret);
+		return;
+	}
 }
 
 /*
