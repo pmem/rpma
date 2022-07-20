f6b6fcd6cd0c0d60db9a5c12ac9847246045e930
Kacper Stefanski <kacper.stefanski@intel.com>
2022-07-20 14:07:17 +0200

    rpma: add the rpma_log_set_function mtt test.

---
+++ tests/multithreaded/log/rpma_log_set_function.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/*
+ * rpma_log_set_function.c -- rpma_log_set_function multithreaded test
+ */
+
+#include <librpma.h>
+
+#include "mtt.h"
+
+/*
+ * thread -- set log function
+ */
+void
+thread(unsigned id, void *prestate, void *state, struct mtt_result *tr)
+{
+	int ret;
+
+	if ((ret = rpma_log_set_function(RPMA_LOG_USE_DEFAULT_FUNCTION))) {
+		MTT_RPMA_ERR(tr, "rpma_log_set_function", ret);
+		return;
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct mtt_args args = {0};
+
+	if (mtt_parse_args(argc, argv, &args))
+		return -1;
+
+	struct mtt_test test = {
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			thread,
+			NULL,
+			NULL,
+			NULL
+	};
+
+	return mtt_run(&test, args.threads_num);
+}
