fb995bb9052dde040ca79c44a767f013ad642156
Lukasz Dorau <lukasz.dorau@intel.com>
2022-08-10 15:26:27 +0200

    examples: rename 06s-multiple-connections to 06scch-multiple-connections

--- examples/run-all-examples.sh
+++ examples/run-all-examples.sh
@@ -270,7 +270,7 @@ function run_example() {
 
 	RV=0
 	case $EXAMPLE in
-	06-multiple-connections|06s-multiple-connections)
+	06-multiple-connections|06scch-multiple-connections)
 		[ "$MODE" == "integration-tests" ] && SEEDS="8" || SEEDS="8 9 11 12"
 		for SEED in $SEEDS; do
 			start_client $VLD_CCMD $DIR/client $IP_ADDRESS $PORT $SEED
