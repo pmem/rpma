56ae91774816fb37a7abc20bc7c98300ee1471ce
Oksana Salyk <oksana.salyk@intel.com>
2022-09-05 12:56:53 +0000

    examples: change the write size from KILOBYTE to HELLO_STR_SIZE

--- examples/09scch-flush-to-persistent-GPSPM/server.c
+++ examples/09scch-flush-to-persistent-GPSPM/server.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "common-conn.h"
+#include "common-hello.h"
 #include "common-map_file_with_signature_check.h"
 #include "common-pmem_map_file.h"
 #include "gpspm/flush-to-persistent-GPSPM.h"
@@ -62,7 +63,7 @@ main(int argc, char *argv[])
 	if (argc >= 4) {
 		pmem_path = argv[3];
 
-		ret = common_pmem_map_file_with_signature_check(pmem_path, KILOBYTE, &mem);
+		ret = common_pmem_map_file_with_signature_check(pmem_path, HELLO_STR_SIZE, &mem);
 		if (ret)
 			goto err_free;
 	}
@@ -71,11 +72,11 @@ main(int argc, char *argv[])
 	/* if no pmem support or it is not provided */
 	if (mem.mr_ptr == NULL) {
 		(void) fprintf(stderr, NO_PMEM_MSG);
-		mem.mr_ptr = malloc_aligned(KILOBYTE);
+		mem.mr_ptr = malloc_aligned(HELLO_STR_SIZE);
 		if (mem.mr_ptr == NULL)
 			return -1;
 
-		mem.mr_size = KILOBYTE;
+		mem.mr_size = HELLO_STR_SIZE;
 	}
 
 	/* allocate messaging buffer */
