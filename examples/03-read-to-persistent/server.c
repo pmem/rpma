006433b85b0162871fccd303ff652acbec4bfb5a
Lukasz Dorau <lukasz.dorau@intel.com>
2022-10-31 15:05:43 +0100

    examples: do not overwrite the previous errors
    Do not overwrite the previous errors (ret = -1)
    set when (wc.status != IBV_WC_SUCCESS) or
    (wc.opcode != IBV_WC_RDMA_READ).

--- examples/03-read-to-persistent/server.c
+++ examples/03-read-to-persistent/server.c
@@ -182,7 +182,7 @@ err_disconnect:
 	 * Wait for RPMA_CONN_CLOSED, disconnect and delete the connection
 	 * structure.
 	 */
-	ret = common_disconnect_and_wait_for_conn_close(&conn);
+	ret |= common_disconnect_and_wait_for_conn_close(&conn);
 
 err_mr_dereg:
 	/* deregister the memory region */
