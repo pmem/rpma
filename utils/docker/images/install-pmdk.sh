b00699e13afac788afbc63efb9828f76717752eb
Lukasz Dorau <lukasz.dorau@intel.com>
2023-03-06 13:56:11 +0100

    common: install a newer PMDK with required fixes
    Install a newer PMDK with fixes required for Rocky Linux 8 and 9.
    
    Ref: https://github.com/pmem/pmdk/issues/5540
    Ref: https://github.com/pmem/rpma/pull/2109

--- utils/docker/images/install-pmdk.sh
+++ utils/docker/images/install-pmdk.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 #
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2020-2022, Intel Corporation
+# Copyright 2020-2023, Intel Corporation
 #
 
 #
 # install-pmdk.sh - installs PMDK libraries
 #
 
-# PMDK version: 1.12.0
-PMDK_VERSION=1.12.0
+# PMDK version: 8074b19b1d9b40bcaaed7e0dc0622dccf8007f2f (1.12.1-119-g8074b19b1)
+# with required fixes, see:
+# https://github.com/pmem/pmdk/issues/5540
+PMDK_VERSION=8074b19b1d9b40bcaaed7e0dc0622dccf8007f2f
 
 WORKDIR=$(pwd)
 
 #
 # Install PMDK libraries from a release package with already generated documentation.
 #
-wget https://github.com/pmem/pmdk/releases/download/${PMDK_VERSION}/pmdk-${PMDK_VERSION}.tar.gz
-tar -xzf pmdk-${PMDK_VERSION}.tar.gz
+wget https://github.com/pmem/pmdk/archive/${PMDK_VERSION}.zip
+gzip -d pmdk-${PMDK_VERSION}.tar.gz
 cd pmdk-${PMDK_VERSION}
 make -j$(nproc) NDCTL_ENABLE=n
 sudo make -j$(nproc) install prefix=/usr NDCTL_ENABLE=n
